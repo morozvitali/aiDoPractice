2b8a56357dc9ffb4640f21d636e6d04d0ba5a434
morozvitali <morozvitali99@gmail.com>
2025-03-10 21:54:26 +0200

    hasDigit

---
+++ src/regex/task1/Regex1.java
@@ -0,0 +1,40 @@
+package regex.task1;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex1 {
+
+    public boolean hasDigit1 (String str) {
+        return str.replaceAll("\\D","").length()>0;
+    }
+
+    public boolean hasDigit2 (String str) {
+        return str.matches("\".*\\d.*\"");
+    }
+
+    public boolean hasDigit3(String str) {
+        for (char c : str.toCharArray()) {
+            if (Character.isDigit(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean hasDigit4 (String str) {
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(str);
+        return matcher.find();
+    }
+
+    public static void main(String[] args) {
+        Regex1 regex = new Regex1();
+        System.out.println("---using-----replaceAll---method------");
+        System.out.println(regex.hasDigit2("hello123"));
+        System.out.println(regex.hasDigit2("world"));
+        System.out.println(regex.hasDigit2("abc5xyz"));
+        System.out.println(regex.hasDigit2("!@#$%^"));
+
+    }
+}
