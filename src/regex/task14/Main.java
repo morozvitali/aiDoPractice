74e56c9b555acf5bf50a2a00871a05258c83a855
morozvitali <morozvitali99@gmail.com>
2025-03-17 15:12:47 +0200

    Patcher & Matcher task

---
+++ src/regex/task14/Main.java
@@ -0,0 +1,24 @@
+package regex.task14;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+String string = "Contact me at test@example.com or at info@regex.com.";
+        System.out.println(main.extract(string).toString());
+    }
+
+    public static List <String> extract (String str) {
+        List<String> emails = new ArrayList<>();
+        Pattern regex = Pattern.compile("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}");
+        Matcher matcher = regex.matcher(str);
+        while (matcher.find()) {
+            emails.add(matcher.group());
+        }
+        return emails;
+    }
+}
---
+++ src/regex/task14/readme.md
@@ -0,0 +1,10 @@
+🔹 Завдання 3: Знайти всі email-адреси в тексті
+📌 Умова: Напиши метод extractEmails(String text),
+який шукає та повертає всі email-адреси в тексті.
+
+Приклад роботи:
+
+extractEmails("Contact me at test@example.com or at info@regex.com.")  
+// 🔥 ["test@example.com", "info@regex.com"]
+💡 Підказка:
+Використай Pattern і Matcher (find()).
