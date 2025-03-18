31a0d034de3308bdff12e05dd5f4fcb42f7f1ddc
morozvitali <morozvitali99@gmail.com>
2025-03-18 15:15:02 +0200

    comit task 15

---
+++ src/regex/task15_/Main.java
@@ -0,0 +1,19 @@
+package regex.task15_;
+
+public class Main {
+
+    public String [] splitWords (String text) {
+        return text.split("[\\W]+");
+//        splitWords("Hello, world! How are you?")
+// 🔥 ["Hello", "world", "How", "are", "you"]
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        String str = "Hello, world! How are you?";
+
+        for (String word : m.splitWords(str)) {
+            System.out.println(word);
+        }
+    }
+}
