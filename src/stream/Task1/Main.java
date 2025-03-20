a59da1df4a2fec61c1d3b274e89350bc0a46ee5c
morozvitali <morozvitali99@gmail.com>
2025-03-20 11:18:55 +0200

    comit task Stream-1

---
+++ src/stream/Task1/Main.java
@@ -0,0 +1,22 @@
+package stream.Task1;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numbers = {3, 6, 1, 9, 4, 8, 2};
+        int[] result = filtering(numbers);
+        System.out.println(Arrays.toString(result)); // [4, 16, 36, 64]
+    }
+
+    public static int [] filtering(int[] arr) {
+        return (int[]) Arrays.stream(arr)
+                .filter(x -> x % 2 == 0)
+                .map(x -> x * x)
+                .boxed()
+                .sorted((a, b) -> b - a)
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+}
