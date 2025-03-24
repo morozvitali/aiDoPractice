854718f589d8fc91fb1a73f62e8c5bcd45035c47
morozvitali <morozvitali99@gmail.com>
2025-03-24 15:32:48 +0200

    comit task reduce training7

--- src/stream/Task7/Main.java
+++ src/stream/Task7/Main.java
@@ -6,14 +6,22 @@ public class Main {
     public static void main(String[] args) {
 
         String[] words = {"apple", "banana", "book", "car", "blue"};
+        int[] numbers = {3, 10, 14, 9, 20, 25, 8};
         Main main = new Main();
-        main.printer(words);
+        main.wordPrinter(words);
+        main.numberPrinter(numbers);
     }
 
-    public void printer (String [] array) {
+    public void wordPrinter (String [] array) {
         Arrays.stream(array)
                 .filter(a -> a
                 .toLowerCase()
                 .charAt(0) == 'b').forEach(System.out::println);
+    }
+    
+    public void numberPrinter (int [] array) {
+        Arrays.stream(array)
+                .filter(a-> a % 2 == 0 && a > 10)
+                .forEach(System.out::println);
     }
 }
