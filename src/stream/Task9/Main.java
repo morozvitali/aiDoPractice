30f27f3ba87969ba802e88b5a42c4fdff18559d1
morozvitali <morozvitali99@gmail.com>
2025-03-24 16:07:41 +0200

    comit task reduce training9

---
+++ src/stream/Task9/Main.java
@@ -0,0 +1,28 @@
+package stream.Task9;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        String[] words = {"java", "python", "go"};
+        int[] numbers = {2, 3, 4};
+        String[] words2 = {"apple", "banana", "kiwi"};
+
+        main.myLength(words);
+        main.mySqrt(numbers);
+        main.myUpperCase(words2);
+    }
+
+    public void myLength (String [] array) {
+        Arrays.stream(array).map(a->a.length()).forEach(System.out::println);
+    }
+
+    public void mySqrt (int [] array) {
+        Arrays.stream(array).map(a->a*a).forEach(System.out::println);
+    }
+
+    public void myUpperCase (String [] array) {
+        Arrays.stream(array).map(a -> a.toUpperCase()).forEach(System.out::println);
+    }
+}
