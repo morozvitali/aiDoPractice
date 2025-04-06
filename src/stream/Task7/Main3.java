eac31ce2576892f74847cc83ef294a8a862f06eb
morozvitali <morozvitali99@gmail.com>
2025-04-06 14:57:27 +0300

    stream lesson 7

---
+++ src/stream/Task7/Main3.java
@@ -0,0 +1,21 @@
+package stream.Task7;
+
+import java.util.Arrays;
+
+public class Main3 {
+
+    public void filtering () {
+
+        Arrays.stream(new String [] {"apple", "banana", "book", "car", "blue"})
+                .filter(a->a.startsWith("b"))
+                .forEach(System.out::println);
+    }
+
+
+    public void filtering2 () {
+
+        Arrays.stream(new int [] {3, 10, 14, 9, 20, 25, 8})
+                .filter(a-> a>10 && a%2 ==0)
+                .forEach(System.out::print);
+    }
+}
