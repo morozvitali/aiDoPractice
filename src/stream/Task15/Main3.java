ebb78ab721be17cc6559bd0456dd41283fe427e3
morozvitali99 <morozvitali99@gmail.com>
2025-04-07 13:17:33 +0300

    comit Streams task15

---
+++ src/stream/Task15/Main3.java
@@ -0,0 +1,34 @@
+package stream.Task15;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main3 {
+    public Map<Integer, List<String>> myGrouper1() {
+
+        return Arrays.stream(new String[]{"apple", "dog", "banana", "cat", "kiwi", "plum", "watermelon"})
+                .collect(Collectors.groupingBy(a -> a.length()));
+    }
+
+    public Map<Character, List<String>> myGrouper2() {
+        List<String> words = List.of("Apple", "apricot", "Banana", "blue", "Cherry", "cranberry");
+        return words.stream().map(a -> a.toLowerCase())
+                .collect(Collectors.groupingBy(a -> a.charAt(0)));
+    }
+
+    public Map<Character, Long> myGrouper3() {
+        return Arrays.stream(new String[]{"apple", "banana", "apricot", "blue", "berry", "cherry"})
+                .collect(Collectors.groupingBy(a -> a.charAt(0), Collectors.counting()));
+    }
+
+    public Map<String, Boolean> myGrouper4() {
+        return Arrays.stream(new String[]{"sky", "apple", "dry", "orange", "sun"})
+                        .collect(Collectors.toMap(
+                                a->a,
+                                a->a.chars().anyMatch(c->"aeiou".indexOf(c)>=0)));
+
+    }
+}
+
