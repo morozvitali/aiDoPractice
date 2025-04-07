8dd2cc13a59a9f0eb0288d5ad7071ae7a68429bc
morozvitali99 <morozvitali99@gmail.com>
2025-04-07 11:38:35 +0300

    comit Streams task12

---
+++ src/stream/Task12/Main3.java
@@ -0,0 +1,18 @@
+package stream.Task12;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main3 {
+
+    public Map<Character, Long> myMaper1 (List<String> list) {
+        return list.stream()
+                .collect(Collectors.groupingBy(a->a.charAt(0), Collectors.counting()));
+    }
+
+    public Map<String, String> myMaper2 (List<String> list) {
+        return list.stream()
+                .collect(Collectors.toMap(w->w, w->w.toUpperCase()));
+    }
+}
