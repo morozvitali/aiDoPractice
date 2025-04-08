3523c41edd0964559884af55798238316bd1a6c2
morozvitali99 <morozvitali99@gmail.com>
2025-04-08 12:06:34 +0300

    comit Streams task20

---
+++ src/stream/Task20_collectingAndThen/MyCollectingAndThen.java
@@ -0,0 +1,18 @@
+package stream.Task20_collectingAndThen;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MyCollectingAndThen {
+
+
+    public List <Integer> myCollectingAndThen () {
+        List<Integer> numbers = List.of(10, 70, 20, 90, 55, 30);
+
+            return numbers.stream().filter(a->a>50)
+                    .sorted(Comparator.reverseOrder())
+                    .collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
+    }
+}
