1cc9bc8c88c38c014949b7dca698bfba6ab6b19a
morozvitali99 <morozvitali99@gmail.com>
2025-04-18 17:41:33 +0300

    comit Stream Api 18 re

---
+++ src/stream/task18_flatMap/Main4.java
@@ -0,0 +1,21 @@
+package stream.task18_flatMap;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main4 {
+    public List<Integer> myFlatMap () {
+
+        List<List<Integer>> weeklyData = List.of(
+                List.of(40, 55, 60),
+                List.of(30, 70),
+                List.of(65, 10, 90)
+        );
+
+        return weeklyData.stream().flatMap(list->list.stream())
+                .filter(a -> a>50)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+    }
+}
