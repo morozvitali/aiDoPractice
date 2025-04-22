94a53c5270c982adaa678e70be5a1065e0cebdd4
morozvitali99 <morozvitali99@gmail.com>
2025-04-22 15:39:31 +0300

    comit List My New Linked List [addFirst] method realisation

--- src/collection/list/linkedlistrealisation/linked2/MyLinkedList.java
+++ src/collection/list/linkedlistrealisation/linked2/MyLinkedList.java
@@ -1,6 +1,7 @@
 package collection.list.linkedlistrealisation.linked2;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class MyLinkedList <E> implements Iterable <E> {
 
@@ -41,6 +42,16 @@ public class MyLinkedList <E> implements Iterable <E> {
             size++; // збільшимо кількість елементів після операції додавання
     }
 
+    public E removeFirst() {
+        if (head == null) {  // якщо лист пустий то видаляти перший елемент неможна
+            throw new NoSuchElementException("Список порожній"); // повернемо помилку
+        }
+
+        E value = head.value; // зберігаємо посилання на елемент який будемо видаляти
+        head = head.next; // значенню голови (першого елемента) присвоюємо значенню яке у нього у next
+        size--; // якщо все добре зменшуємо лист
+        return value; // повертаємо те що видалили (посилання на елемент який тепер не в листі)
+    }
 
 
 
