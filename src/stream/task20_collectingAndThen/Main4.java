7ecb703f27e9afdfb08c21bd0027953269568c3c
morozvitali99 <morozvitali99@gmail.com>
2025-04-22 12:14:43 +0300

    comit Stream Api 20 collectingAndThen

---
+++ src/stream/task20_collectingAndThen/Main4.java
@@ -0,0 +1,25 @@
+package stream.task20_collectingAndThen;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main4 {
+
+    public List<Integer> myCollecting1() {
+        List<Integer> numbers = List.of(10, 70, 20, 90, 55, 30);
+        return numbers.stream().filter(a -> a > 50).sorted(Comparator.reverseOrder())
+                .collect(Collectors
+                        .collectingAndThen(Collectors.toList(), a -> Collections.unmodifiableList(a)));
+    }
+
+    public List<String> myCollecting2() {
+        List<String> numbers = List.of("Polly", "it's", "a", "bird", "Polly", "it's", "a", "bird");
+        return numbers.stream()
+                .filter(a->a.length()>4)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), a->Collections.unmodifiableList(a)));
+    }
+}
+
+
