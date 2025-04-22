a6300fef092c3a0daccb589cc8eeb852b3ae95bd
morozvitali99 <morozvitali99@gmail.com>
2025-04-22 15:30:47 +0300

    comit List My New Linked List [add] method realisation

--- src/collection/list/linkedlistrealisation/linked2/MyLinkedList.java
+++ src/collection/list/linkedlistrealisation/linked2/MyLinkedList.java
@@ -9,6 +9,8 @@ public class MyLinkedList <E> implements Iterable <E> {
         return null;
     }
 
+    /* ----------------M-Y--------N-O-D-E----------------- */
+
     private static class Node <E> {
         E value;
         Node <E> next;
@@ -19,7 +21,28 @@ public class MyLinkedList <E> implements Iterable <E> {
     }
 
     private Node <E> head;
-    private int size =0;
+    private int size = 0;
+
+/* ----------------A-D-D--------M-E-T-H-O-D----------------- */
+
+    public void add (E value) {
+
+        Node <E> newNode = new Node <> (value); //додали новий нод, саме зараз він має значення яке передаємо [[value E]] і next = null
+        if (head == null) {   // перевірка, чи не пустий ліст
+            head = newNode;     //якщо пустий то це перший елемент і нод має те саме значення  [[value E]] і next = null
+        } else { // інакше ліст не порожній
+            Node <E> current = head;    // Створюємо тимчасову змінну яка нам допомогатиме перебирати вузли і шукати саме потрібний
+            while (current.next != null) {  // а для цього перебираємо всі вузли до самого останнього у котрого значення next == null
+                current = current.next; // за цім шматочком коду ми знаходимо наступний обьєкт, він працює як i++
+                // і таким чином ми отримуємо посилання на наступний нод
+            }
+            current.next = newNode; // це самий останній нод в якому next == null і цьому ноду ми створюємо посилання на наш newNode
+        }
+            size++; // збільшимо кількість елементів після операції додавання
+    }
+
+
+
 
 
 }
