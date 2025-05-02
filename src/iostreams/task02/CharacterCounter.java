7038d9f3d1b29df4dde9b26d29ed58b3676fb62c
morozvitali99 <morozvitali99@gmail.com>
2025-05-02 12:33:22 +0300

    commit task [io] task2

--- src/iostreams/task02/CharacterCounter.java
+++ src/iostreams/task02/CharacterCounter.java
@@ -1,4 +1,33 @@
 package iostreams.task02;
 
+import java.io.*;
+
 public class CharacterCounter {
+
+    public void longLine() {
+        String filePath = "iotask02.txt";
+        String longestLine = "";
+        int maxLenght = 0;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.length() > maxLenght) {
+                    maxLenght = line.length();
+                    longestLine = line;
+                }
+            }
+            System.out.println("longest line in file " + filePath + " is: " + longestLine);
+            System.out.println("max lenght in this line is " + maxLenght);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("start main function ***");
+        CharacterCounter cc = new CharacterCounter();
+        cc.longLine();
+    }
 }
