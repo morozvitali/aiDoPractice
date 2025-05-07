6faa87b4a10130af900a0580e843edc97570f894
morozvitali99 <morozvitali99@gmail.com>
2025-05-07 13:09:10 +0300

    commit regex5 [train] -->

--- src/regex/task5/Regex3.java
+++ src/regex/task5/Regex3.java
@@ -1,21 +1,26 @@
 package regex.task5;
 
-public class Regex2 {
+public class Regex3 {
 
-    public boolean isValidIPv4(String ip) {
-        String[] numbers = ip.split("\\.");
-        if (numbers.length != 4) return false;
+    public boolean isValidIPv4(String str) {
+        String[] array = str.split("\\.");
 
-        for (String notnumber : numbers) {
-            if (!notnumber.matches("([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])")) {
+        if (array.length != 4) {
+            return false;
+        }
+
+        for (String s : array) {
+            if (!s.matches("[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]")) {
                 return false;
             }
         }
         return true;
     }
 
+
+
     public static void main(String[] args) {
-        Regex2 reg = new Regex2();
+        Regex3 reg = new Regex3();
         System.out.println(reg.isValidIPv4("192.168.1.1"));
         System.out.println(reg.isValidIPv4("0.0.0.0"));
         System.out.println(reg.isValidIPv4("256.100.100.100"));
