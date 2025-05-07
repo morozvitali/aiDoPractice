4a1eaef7f8a75ee1cf4719a5da66a1a4ac7b0756
morozvitali99 <morozvitali99@gmail.com>
2025-05-07 11:49:13 +0300

    commit regex2 [train] -->

--- src/regex/task2/Regex3.java
+++ src/regex/task2/Regex3.java
@@ -1,19 +1,18 @@
 package regex.task2;
 
-public class Regex2 {
+public class Regex3 {
 
-        public boolean isValidPhoneNumber (String s) {
-            return s.matches("\\+380[0-9]{2}[0-9]{7}");
-        }
+    public boolean isValidPhoneNumber(String phone) {
+        return phone.matches("\\+380[\\d]{2}[\\d]{7}");
+    }
+
 
     public static void main(String[] args) {
-        Regex2 is = new Regex2();
+        Regex3 is = new Regex3();
         System.out.println(is.isValidPhoneNumber("+380501234567"));
         System.out.println(is.isValidPhoneNumber("380501234567"));
         System.out.println(is.isValidPhoneNumber("+380991234"));
         System.out.println(is.isValidPhoneNumber("+380AA1234567"));
         System.out.println(is.isValidPhoneNumber("+38 0501234567"));
     }
-
-
 }
