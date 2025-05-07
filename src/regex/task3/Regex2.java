d8692ec0ebae59baf98cb4a725905891145ec33e
morozvitali99 <morozvitali99@gmail.com>
2025-05-07 12:03:08 +0300

    commit regex3 [train] -->

--- src/regex/task3/Regex2.java
+++ src/regex/task3/Regex2.java
@@ -1,13 +1,14 @@
 package regex.task3;
 
-public class Regex3 {
-    public boolean isValidEmail(String email) {
-        return email.matches("^[0-9a-zA-Z][0-9a-zA-Z._-]+[0-9a-zA-Z_]@[0-9a-zA-Z][0-9a-zA-Z.]+[0-9a-zA-Z].[a-zA-Z]{2,6}$");
+public class Regex2 {
+    public boolean isValidEmail (String email) {
+        return email.matches("^[a-zA-Z0-9][a-zA-Z0-9_-]+[a-zA-Z0-9]@[a-zA-Z0-9]+\\.[a-zA-Z]{2,6}");
+
+
     }
 
     public static void main(String[] args) {
-
-        Regex3 validator = new Regex3();
+        Regex2 validator = new Regex2();
         System.out.println(validator.isValidEmail("user@example.com"));
         System.out.println(validator.isValidEmail("john.doe-123@gmail.com"));
         System.out.println(validator.isValidEmail("jane@sub.domain.org"));
@@ -17,4 +18,6 @@ public class Regex3 {
         System.out.println(validator.isValidEmail("user@example.com."));
         System.out.println(validator.isValidEmail("user@domain.c"));
     }
+
+
 }
