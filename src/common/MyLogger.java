7c2094c7ba2257814fbbf68a979ad0f4d4dc1ab0
morozvitali99 <morozvitali99@gmail.com>
2025-05-13 15:20:07 +0300

    commit Stream [common task #Logger]

---
+++ src/common/MyLogger.java
@@ -0,0 +1,29 @@
+package common;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class MyLogger {
+
+    public void log(String message, String level) {
+
+        LocalDateTime now = LocalDateTime.now(); //додали час на зараз
+        String formattedDateTime = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")); // із строчки часу зробили шаблонну строчку для виводу
+        String logLine = String.format("[%s] [%s] [%s]", formattedDateTime, level.toUpperCase(), message);  // сформували строчку
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("log.txt", true))) {
+            writer.write(logLine);
+            writer.newLine();
+        } catch (IOException e) {
+            System.out.println("Помилка запису у файл " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        MyLogger myLogger = new MyLogger();
+        myLogger.log("Testing Of tje Method", "All system error");
+    }
+}
