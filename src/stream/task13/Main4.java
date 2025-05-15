76e50818fdad24ad022095a39ff56ba26b0aebb0
morozvitali99 <morozvitali99@gmail.com>
2025-05-15 12:43:15 +0300

    commit Stream [practice] map filter collect 13.5

--- src/stream/task13/Main4.java
+++ src/stream/task13/Main4.java
@@ -1,6 +1,9 @@
 package stream.task13;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main4 {
     public long mapping1 () {
@@ -12,6 +15,21 @@ public class Main4 {
         return Arrays.stream(words).filter(w->w.startsWith("s")).reduce((a,b) -> a.length() < b.length() ? a : b).orElse("");
     }
 
+    public int mapping3 () {
+        int[] nums = {1, 2, 3, 4, 5};
+        return Arrays.stream(nums)
+                .filter(a-> a%2 != 0)
+                .map(a->a*a)
+                .reduce(0, (a,b) -> a +b);
+    }
 
+    public List<String> mapping4 () {
+        String[] words = {"java", "code", "fun", "javafx", "loop"};
+        return Arrays.stream(words).filter(w->w.length() == 4).collect(Collectors.toList());
+    }
 
+    public Map<String, Long> mapping5 () {
+        String[] words = {"apple", "banana", "cherry"};
+        return Arrays.stream(words).collect(Collectors.toMap(a->a, a->a.chars().filter(c->"aeiou".indexOf(c) >=0).count()));
+    }
 }
