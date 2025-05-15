9891e88aa0ede296a7a54d0bffdc93813fb7374a
morozvitali99 <morozvitali99@gmail.com>
2025-05-15 13:47:15 +0300

    commit Stream [practice] stream 14.5

--- src/stream/task14/Main4.java
+++ src/stream/task14/Main4.java
@@ -2,6 +2,7 @@ package stream.task14;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main4 {
@@ -20,6 +21,13 @@ public class Main4 {
         return words.stream().filter(a->a.length()%2 == 0).map(a->a.toUpperCase()).collect(Collectors.toList());
     }
 
-    public
+    public Map <Character, List<String>> filtering4 () {
+        String[] words = {"apple", "ant", "banana", "blue", "berry", "dog", "dolphin"};
+        return Arrays.stream(words).filter(a->a.length()>4).collect(Collectors.groupingBy(a->a.charAt(0)));
+    }
 
+    public Map <String, Integer> filtering5 () {
+        String[] words = {"sky", "apple", "moon", "dry", "banana"};
+        return Arrays.stream(words).filter(a->a.chars().filter(c->"Aeiou".indexOf(c) >= 0).count() > 1).collect(Collectors.toMap(a-> a, a->a.length()));
+    }
 }
