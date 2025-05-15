7211a17b4ca0bcb97b2ec1d217d03b3c0a0d09da
morozvitali99 <morozvitali99@gmail.com>
2025-05-15 12:15:58 +0300

    commit Stream [practice] map filter collect 12

---
+++ src/stream/task12/Main4.java
@@ -0,0 +1,14 @@
+package stream.task12;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main4 {
+    public Map<Character, Long> countWordsByFirstLetter1 (String[] array) {
+        return Arrays.stream(array).collect(Collectors.groupingBy(a->a.charAt(0), Collectors.counting()));
+    }
+    public Map<Character, String> countWordsByFirstLetter2 (String[] array) {
+        return Arrays.stream(array).collect(Collectors.toMap(a->a.charAt(0), a->a.toUpperCase()));
+        }
+}
