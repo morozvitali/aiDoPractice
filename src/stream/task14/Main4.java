527863c482452864f8e483cb426a679eb504b80c
morozvitali99 <morozvitali99@gmail.com>
2025-05-15 13:05:05 +0300

    commit Stream [practice] stream 14.1

--- src/stream/task14/Main4.java
+++ src/stream/task14/Main4.java
@@ -1,6 +1,14 @@
 package stream.task14;
 
+import java.util.Arrays;
+
 public class Main4 {
+    public String filtering () {
+        String[] words = {"java", "stream", "code", "developer", "sun"};
+        return Arrays.stream(words).filter(a->a.contains("e")).reduce((a,b)-> a.length() >= b.length() ? a : b).orElse("");
+    }
+
+
 
 
 }
