c9a35de6ef426edef380351dd268430d3827cb3c
morozvitali99 <morozvitali99@gmail.com>
2025-05-20 17:56:14 +0300

    commit [practice] Stream 18.1

---
+++ src/stream/task18_flatMap/Main5.java
@@ -0,0 +1,20 @@
+package stream.task18_flatMap;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main5 {
+
+    public List<Integer> mapper() {
+        List<List<Integer>> weeklyData = List.of(
+                List.of(40, 55, 60),
+                List.of(30, 70),
+                List.of(65, 10, 90)
+        );
+
+        return weeklyData.stream().flatMap(list->list.stream())
+                .filter(a -> a > 50)
+                .collect(Collectors.toList());
+    }
+}
