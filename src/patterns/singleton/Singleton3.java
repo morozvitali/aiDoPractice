7a3f442495f6b5110783b42578c3f94d0a2f5cb2
morozvitali99 <morozvitali99@gmail.com>
2025-05-21 15:53:54 +0300

    commit [practice] pattern Singleton realisation v3

---
+++ src/patterns/singleton/Singleton3.java
@@ -0,0 +1,22 @@
+package patterns.singleton;
+
+public class Singleton3 {
+    private static volatile Singleton3 instance;
+    /*
+    Double-checked locking
+     */
+
+    private Singleton3() {
+    }
+
+    public static Singleton3 getInstance() {
+        if (instance == null) { // перга перевірка
+            synchronized (Singleton3.class) {
+                if (instance == null) { // друга перевірка
+                    instance = new Singleton3();
+                }
+            }
+        }
+        return instance;
+    }
+}
