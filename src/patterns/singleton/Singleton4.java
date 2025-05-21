376a4faf60e69c9cf739ccac11e7a9604467fa5e
morozvitali99 <morozvitali99@gmail.com>
2025-05-21 15:58:03 +0300

    commit [practice] pattern Singleton realisation v4

---
+++ src/patterns/singleton/Singleton4.java
@@ -0,0 +1,15 @@
+package patterns.singleton;
+
+public class Singleton4 {
+        /*
+        using of static block
+         */
+    private static final Singleton4 instance = new Singleton4();
+
+    private Singleton4() {
+    }
+
+    public static Singleton4 getInstance() {
+        return instance;
+    }
+}
