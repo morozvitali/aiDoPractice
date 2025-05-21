52ddbd52ab9a615c8806bab6546bc4689e3a6ab4
morozvitali99 <morozvitali99@gmail.com>
2025-05-21 15:45:20 +0300

    commit [practice] pattern Singleton realisation v2

--- src/patterns/singleton/Singleton1.java
+++ src/patterns/singleton/Singleton1.java
@@ -4,12 +4,12 @@ package patterns.singleton;
 ледача ініціалізація, непотокобезпечна
  */
 
-public class Singleton {
-    private static Singleton instance;
-    private Singleton () {}
-    public static Singleton getInstance() {
+public class Singleton1 {
+    private static Singleton1 instance;
+    private Singleton1() {}
+    public static Singleton1 getInstance() {
         if (instance == null) {
-            instance = new Singleton();
+            instance = new Singleton1();
         }
         return instance;
     }
