c5224df04be00fc50721dc99ebd2d09d2bc4771f
morozvitali <morozvitali99@gmail.com>
2025-05-27 06:34:30 +0000

    Singleton pattern [realisation] v3.4

---
+++ src/patterns/singleton/realisation2/Singleton4.java
@@ -0,0 +1,14 @@
+package patterns.singleton.realisation2;
+
+public class Singleton4 {
+        /*
+        using of static block
+         */
+    private static final Singleton4 instance = new Singleton4();
+    private Singleton4() {
+    }
+
+    public static Singleton4 getInstance() {return instance;}
+
+
+}
