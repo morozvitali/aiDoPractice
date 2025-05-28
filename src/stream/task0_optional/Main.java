ac9542e663643338bbfa50fdcb3b99c1597c7494
morozvitali <morozvitali99@gmail.com>
2025-05-28 15:52:37 +0300

    Streams [optional] #1.1

--- src/stream/task0_optional/Main.java
+++ src/stream/task0_optional/Main.java
@@ -1,12 +1,17 @@
 package stream.task0_optional;
 
 import java.util.List;
+import java.util.Optional;
 
 public class Main {
 
     public Integer myOptional () {
         List<Integer> numbers = List.of(3, 8, 10, 15, 4);
         return numbers.stream().filter(a->a%5 ==0).findFirst().orElse(-1);
+    }
+
+    public Integer myOptional1 (Optional<Integer> value) {
+        return value.map(a->a*2).orElse(-1);
     }
 
 
