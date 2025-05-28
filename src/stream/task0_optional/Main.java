1b0b8d94e9b8f9abd28c37de3460eb159ab3e107
morozvitali <morozvitali99@gmail.com>
2025-05-28 16:33:32 +0300

    Streams [optional] #1.5

--- src/stream/task0_optional/Main.java
+++ src/stream/task0_optional/Main.java
@@ -6,24 +6,24 @@ import java.util.stream.Stream;
 
 public class Main {
 
-    public Integer myOptional () {
+    public Integer myOptional() {
         List<Integer> numbers = List.of(3, 8, 10, 15, 4);
-        return numbers.stream().filter(a->a%5 ==0).findFirst().orElse(-1);
+        return numbers.stream().filter(a -> a % 5 == 0).findFirst().orElse(-1);
     }
 
-    public Integer myOptional1 (Optional<Integer> value) {
-        return value.map(a->a*2).orElse(-1);
+    public Integer myOptional1(Optional<Integer> value) {
+        return value.map(a -> a * 2).orElse(-1);
     }
 
-    public String myOptional2 (Optional<String> value) {
-        return value.filter(s->s.length() > 5).orElse("short");
+    public String myOptional2(Optional<String> value) {
+        return value.filter(s -> s.length() > 5).orElse("short");
     }
 
-    public String myOptional3 (Optional <String> value) {
-        return value.map(a->"Hello, " + a + "!").orElse("Hello, guest!");
+    public String myOptional3(Optional<String> value) {
+        return value.map(a -> "Hello, " + a + "!").orElse("Hello, guest!");
     }
 
-    public void myOptional4 () {
+    public void myOptional4() {
 
         List<Integer> nums = List.of(1, 2, 3, 4, 5);
         nums.stream()
@@ -34,4 +34,8 @@ public class Main {
                 );
     }
 
+    public String myOptional5() {
+        Optional<String> email = Optional.of("ADMIN@SITE.COM");
+        return email.map(a -> a.toLowerCase()).filter(a -> a.contains(".com")).orElseThrow(() -> new IllegalArgumentException("Invalid email"));
+    }
 }
