5f9fae54064e4ef7801ed6a84affeeb70ecbf2f5
morozvitali99 <morozvitali99@gmail.com>
2025-05-29 13:54:04 +0300

    commit [pattern protype] realisation2

---
+++ src/patterns/prototype4/realisation2_3/Prototype.java
@@ -0,0 +1,5 @@
+package patterns.prototype4.realisation2_3;
+
+public interface Prototype extends Cloneable {
+    Prototype clone();
+}
--- src/patterns/prototype4/realisatrion2_3/Prototype.java
+++
@@ -1,4 +0,0 @@
-package patterns.prototype4.realisatrion2_3;
-
-public interface Prototype {
-}
