1cfbed9c2d366848e01070a5ff79fc126e50c41e
morozvitali99 <morozvitali99@gmail.com>
2025-05-29 14:21:26 +0300

    commit [Streams] practice task3.3

--- src/stream/task3/Main5.java
+++ src/stream/task3/Main5.java
@@ -2,7 +2,6 @@ package stream.task3;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main5 {
     public List<String> myFiltering () {
@@ -11,5 +10,24 @@ public class Main5 {
         return Arrays.stream(array).map(a->a.toLowerCase())
                 .filter(s->"aeiou".indexOf(s.charAt(0))>=0)
                 .toList();
+    }
+
+    public Long myFiltering2 () {
+        String sentence = "Programming in Java is so enjoyable and educational";
+        String [] array= sentence.split(" ");
+        return Arrays.stream(array).filter(a->counter(a)>2).count();
+    }
+
+    public Long counter (String string) {
+        return string.chars().filter(c->"aeiou".indexOf(c) >=0).count();
+    }
+
+    public void myFiltering3 () {
+        String sentence = "Programming in Java is so enjoyable and educational";
+        String[] array = sentence.split(" ");
+        Arrays.stream(array)
+        .map(String::toUpperCase)
+                .filter(a->a.length()>=3 && a.length() <=5)
+                .forEach(System.out::println);
     }
 }
