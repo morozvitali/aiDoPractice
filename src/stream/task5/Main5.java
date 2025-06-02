752d4b68165f3a330d9bbfccb9283c82f8472848
morozvitali99 <morozvitali99@gmail.com>
2025-06-02 17:35:15 +0300

    commit [Streams] practice task 5.3

---
+++ src/stream/task5/Main5.java
@@ -0,0 +1,32 @@
+package stream.task5;
+
+import java.util.Arrays;
+
+public class Main5 {
+    public int practice1() {
+        int[] numbers = {15, 3, 8, 22, 1, 10};
+        return Arrays.stream(numbers)
+                .reduce((a, b) -> a < b ? a : b).orElse(0);
+    }
+
+    public String practice2() {
+        String[] words = {"Java", "Python", "C++"};
+        return Arrays.stream(words)
+                .reduce((a,b) -> a + " " + b).orElse("");
+    }
+
+    public int practice3 () {
+        int[] numbers = {123, 91, 77, 456, 18};
+        return Arrays.stream(numbers)
+                .reduce(0, (a,b) -> calc(a) > calc(b) ? a : b);
+    }
+
+    public int calc (int value) {
+        int sum = 0;
+        while (true) {
+            sum = sum + value % 10;
+            value = value / 10;
+        }
+    }
+
+}
