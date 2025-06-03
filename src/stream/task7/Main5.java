98e46381af69ea2c3b89a08f5701b8f4bb18b864
morozvitali <morozvitali99@gmail.com>
2025-06-03 16:30:51 +0300

    Practice [Streams]  #7.9

--- src/stream/task7/Main5.java
+++ src/stream/task7/Main5.java
@@ -1,6 +1,7 @@
 package stream.task7;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Main5 {
     public void practice1 () {
@@ -16,6 +17,25 @@ public class Main5 {
         Arrays.stream(numbers).filter(a-> a%2 ==0).forEach(System.out::println);
     }
 
+    public boolean practice6 () {
+        List<Integer> list = List.of(3, -5, 7, 10);
+        return list.stream()
+                .anyMatch(a->a<0);
+    }
 
+    public boolean practice7 () {
+        List<Integer> list = List.of(2, 5, 10, 20);
+        return list.stream().allMatch(a->a>=0);
+    }
+
+    public boolean practice8() {
+        List<Integer> list = List.of(1, 2, 3, 0);
+        return list.stream().noneMatch(a->a==0);
+    }
+
+    public boolean practice9() {
+        List<Integer> list = List.of(1, 2, 3, 0);
+        return list.stream().anyMatch(a->a%7==0);
+    }
 
 }
