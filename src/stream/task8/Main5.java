f633d4e87d9ba285a3a03f03ac7915b96a429c7b
morozvitali <morozvitali99@gmail.com>
2025-06-05 14:47:43 +0300

    Practice [Streams]  #8.8

--- src/stream/task8/Main5.java
+++ src/stream/task8/Main5.java
@@ -1,6 +1,10 @@
 package stream.task8;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main5 {
 
@@ -21,16 +25,30 @@ public class Main5 {
 
     public int practice3 () {
         int[] numbers = {10, 40, 55, 49, 60, 30};
-        Arrays.stream(numbers).filter(a->a<50).reduce((a,b)->a>=b?a:b).orElse(-1);
+        return Arrays.stream(numbers).reduce((a,b)->a>=b?a:b).orElse(-100);
     }
 
+    public int practice4 () {
+        int[] numbers = {10, 40, 55, 49, 60, 30};
+        return Arrays.stream(numbers).filter(a->a<50).reduce((a,b)->a>=b?a:b).orElse(-1);
+    }
 
+    public int practice5 () {
+        int[] numbers = {3, 5, 2, 6};
+        return Arrays.stream(numbers).filter(a->a%2==0).map(a->a*a).max().orElse(-1);
+    }
 
     public int practice6 () {
         int[] numbers = {2, 3, 4, 5, 6, 7};
         return Arrays.stream(numbers).filter(a->a%2==0).reduce((a,b)->a*b).orElse(0);
     }
 
+    public int practice7 () {
+        return IntStream.rangeClosed(1,5).map(a->a*a).sum();
+    }
 
+    public List<Integer> practice8 () {
+        return IntStream.rangeClosed(10,50).filter(a->a%7==0).boxed().collect(Collectors.toList());
+    }
 
 }
