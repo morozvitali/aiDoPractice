4291b98bcf267f3041eeda2c509adbce7a61eda3
morozvitali99 <morozvitali99@gmail.com>
2025-06-10 14:49:01 +0300

    commit [Streams] practice task 10.6

--- src/stream/task10_map_filter_collect/Main5.java
+++ src/stream/task10_map_filter_collect/Main5.java
@@ -27,4 +27,11 @@ public class Main5 {
         String[] words = {"java", "code", "home", "sun", "snow"};
         Arrays.stream(words).filter(a->a.length() == 4).map(w ->w.substring (0,1).toUpperCase() + w.substring(1).toLowerCase()).forEach(System.out::println);
     }
+
+    public void practice6 () {
+        int[] numbers = {2, 2, 4, 6, 3, 4};
+        Arrays.stream(numbers).filter(a->a%2==0).map(a->a*a).distinct().forEach(System.out::println);
+    }
+
+
 }
