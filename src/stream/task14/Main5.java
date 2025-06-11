8883189080d0af72fc1b723b0f4b443f8447dacc
morozvitali <morozvitali99@gmail.com>
2025-06-11 12:00:31 +0300

    Practice [Streams]  #14.3

--- src/stream/task14/Main5.java
+++ src/stream/task14/Main5.java
@@ -1,6 +1,7 @@
 package stream.task14;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Main5 {
     public String practice1() {
@@ -8,8 +9,13 @@ public class Main5 {
         return Arrays.stream(words).filter(w -> w.indexOf('e') >= 0).reduce((a, b) -> a.length() <= b.length() ? a : b).orElse("");
     }
 
-    public long practice2 () {
-        return Arrays.stream(new int[]{2, 4, 2, 6, 8, 4, 10, 10, 12}).filter(a->a%2 == 0).distinct().count();
+    public long practice2() {
+        return Arrays.stream(new int[]{2, 4, 2, 6, 8, 4, 10, 10, 12}).filter(a -> a % 2 == 0).distinct().count();
+    }
+
+    public List<String> practice3() {
+        List<String> words = List.of("APPLE", "Banana", "CHERRY", "kiwi", "PLUM");
+        return words.stream().filter(a -> a.length() % 2 == 0).map(a -> a.toLowerCase()).toList();
     }
 
 
