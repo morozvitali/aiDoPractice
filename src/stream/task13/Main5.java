4044c50fc730fdd81ff695e362c8fb4971e5b588
morozvitali <morozvitali99@gmail.com>
2025-06-11 11:35:35 +0300

    Practice [Streams]  #13.4

--- src/stream/task13/Main5.java
+++ src/stream/task13/Main5.java
@@ -1,6 +1,7 @@
 package stream.task13;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main5 {
@@ -15,6 +16,14 @@ public class Main5 {
             return Arrays.stream(words).map(a->a.toLowerCase()).filter(a->a.charAt(0)=='s').reduce((a,b)-> a.length() <= b.length() ? a : b).orElse("");
     }
 
+    public int practice3 () {
+        int[] nums = {1, 2, 3, 4, 5};
+        return Arrays.stream(nums).filter(a->a%2 !=0).map(a->a*a).sum();
+    }
 
+    public List<String> practice4 () {
+        String[] words = {"java", "code", "fun", "javafx", "loop"};
+        return Arrays.stream(words).filter(a->a.length() == 4).map(a->a.toUpperCase()).collect(Collectors.toList());
+    }\
 
 }
