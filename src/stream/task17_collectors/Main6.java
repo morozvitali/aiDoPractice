56ef025cbc562d3c807c3f74cc59a1749a06bf89
morozvitali <morozvitali99@gmail.com>
2025-06-16 10:05:29 +0300

    Practice [Streams]  #17.11 collectors

--- src/stream/task17_collectors/Main6.java
+++ src/stream/task17_collectors/Main6.java
@@ -64,5 +64,33 @@ public class Main6 {
         return numbers.stream().collect(Collectors.collectingAndThen(Collectors.counting(), count -> "Count " + count));
     }
 
+    public Map <Character, String> practice11 () {
+        List<String> words = List.of("apple", "ant", "banana", "bat", "car");
+        return words.stream()
+                .collect(Collectors.groupingBy(a->a.charAt(0),
+                        Collectors.joining(", ")));
+    }
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
