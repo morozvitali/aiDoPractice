7acc6657ee0a38a0fa8de436f951bb45e44ef69a
morozvitali <morozvitali99@gmail.com>
2025-06-16 08:33:29 +0300

    Practice [Streams]  #17 collectors

---
+++ src/stream/task17_collectors/Main6.java
@@ -0,0 +1,35 @@
+package stream.task17_collectors;
+
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main6 {
+    public String practice1 () {
+        List<String> names = List.of("Alice", "Bob", "Charlie");
+        return names.stream().collect(Collectors.joining(", "));
+    }
+
+    public void practice2 () {
+        List<Integer> numbers = List.of(4, 8, 15, 16, 23, 42);
+        IntSummaryStatistics stats = numbers.stream().collect(Collectors.summarizingInt(value -> value));
+        Integer count = Math.toIntExact(stats.getCount());
+        Integer sum = Math.toIntExact(stats.getSum());
+        Integer min = Math.toIntExact(stats.getMin());
+        Integer max = Math.toIntExact(stats.getMax());
+        Integer average = Math.toIntExact((long) stats.getAverage());
+    }
+
+    public Map<Integer, List <String>> practice3 () {
+        List<String> words = List.of("hi", "book", "sun", "day", "Java", "sky");
+        return words.stream().collect(Collectors.groupingBy(a->a.length()));
+    }
+
+    public Map <Integer, Long> practice4 () {
+        List<String> words = List.of("hi", "book", "sun", "day", "Java", "sky");
+        return words.stream().collect(Collectors.groupingBy(a->a.length(), Collectors.counting()));
+    }
+
+
+}
