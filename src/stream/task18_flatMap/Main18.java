8a2451fc35f9b9cd5aae08baf08d45c47151474a
morozvitali <morozvitali99@gmail.com>
2025-06-18 10:38:37 +0300

    Practice [Stream] flatmap 18.2 task

--- src/stream/task18_flatMap/Main18.java
+++ src/stream/task18_flatMap/Main18.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main18 {
-    public List<Integer> practice1_0 () {
+    public List<Integer> practice1 () {
         List<List<Integer>> weeklyData = List.of(
                 List.of(40, 55, 60),
                 List.of(30, 70),
@@ -25,6 +25,26 @@ public class Main18 {
     public int practice1_2 () {
         int[] numbers = {-3, 5, 2, -1, 4};
         return Arrays.stream(numbers).filter(a->a<0).reduce((a,b)->a*b).orElse(1);
+    }
+
+    public int practice1_3 () {
+        int[] numbers = {3, 9, 8, 12, 7};
+        return Arrays.stream(numbers).filter(a->a%3==0).reduce((a,b)-> a>b ?a : b).orElse(-1);
+    }
+
+    public int practice1_4 () {
+        int[] numbers = {2, 3, 4};
+        return Arrays.stream(numbers).filter(a->a%2==0).map(n->n*n).reduce((a,b) -> a+b).orElse(-1);
+    }
+
+    public int practice2 () {
+        int[][] data = {
+                {3, 11},
+                {6, 7},
+                {10}
+        };
+
+        return Arrays.stream(data).flatMapToInt(a-> Arrays.stream(a)).reduce((a,b)->a<b?a:b).orElse(-1);
     }
 
 
