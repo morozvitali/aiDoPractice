c47fefbc2a758a70bfa510fd6015b299b62dfa8d
morozvitali99 <morozvitali99@gmail.com>
2025-06-24 10:24:41 +0300

    commit [Streams] practice 14.2

--- src/stream/task14_summarystatistics/Main1.java
+++ src/stream/task14_summarystatistics/Main1.java
@@ -1,14 +1,23 @@
 package stream.task14_summarystatistics;
 
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.Map;
+import java.util.Optional;
 
 public class Main1 {
-    public void practice1 () {
+    public Optional<Map.Entry<String, Integer>> practice1 () {
         String[] words = {"java", "stream", "code", "developer", "Engineer"};
 
-        Arrays.stream(words).map(a->a.toLowerCase()).filter(w->w.contains("e"))
+        return Arrays.stream(words).map(a->a.toLowerCase()).filter(w->w.contains("e"))
                 .map(w-> Map.entry(w, w.length()))
                 .reduce((a,b) -> a.getValue() >= b.getValue() ? a : b);
+    }
+    
+    public Long practice2 () {
+        int[] numbers = {2, 4, 2, 6, 8, 4, 10, 10, 12};
+        IntSummaryStatistics stats = Arrays.stream(numbers).distinct().filter(a->a%2==0)
+                .summaryStatistics();
+        return stats.getCount();
     }
 }
