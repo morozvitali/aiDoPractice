f48b232e9914aee570d249f0c077521dacf32b2f
morozvitali <morozvitali99@gmail.com>
2025-06-25 12:26:29 +0300

    Practice [Streams] task 5.2 limit maptoint

--- src/stream/task5_skip_limit/Main1.java
+++ src/stream/task5_skip_limit/Main1.java
@@ -23,5 +23,9 @@ public class Main1 {
         list.stream().skip(list.size()-2).forEach(System.out::println);
     }
 
+    public int practice5 () {
+        List<Integer> list = List.of(3, 2, 4, 6, 1, 8, 10, 12);
+        return list.stream().limit(5).mapToInt(a-> (int)a).sum();
+    }
 
 }
