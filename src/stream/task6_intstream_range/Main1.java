99c7488ed19c5a5939e7694f0a2531a923942906
morozvitali <morozvitali99@gmail.com>
2025-06-25 12:42:25 +0300

    Practice [Streams] task 6.2 range closed

--- src/stream/task6_intstream_range/Main1.java
+++ src/stream/task6_intstream_range/Main1.java
@@ -1,11 +1,16 @@
 package stream.task6_intstream_range;
 
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Main1 {
-    public int practice () {
-        return IntStream.range(1,99).sum();
+    public int practice1 () {
+        return IntStream.range(1, 100).sum();
     }
-
+    public long practice2 () {
+        return IntStream.rangeClosed(10, 20)
+                .filter(a -> a % 2 == 0)
+                .count();
+    }
 
 }
