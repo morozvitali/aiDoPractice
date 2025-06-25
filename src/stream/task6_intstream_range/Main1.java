b55fca6c28a92c6a83a529ef86ecf2818b3469a8
morozvitali <morozvitali99@gmail.com>
2025-06-25 12:36:58 +0300

    Practice [Streams] task 6.1 intstreamrange

---
+++ src/stream/task6_intstream_range/Main1.java
@@ -0,0 +1,11 @@
+package stream.task6_intstream_range;
+
+import java.util.stream.IntStream;
+
+public class Main1 {
+    public int practice () {
+        return IntStream.range(1,99).sum();
+    }
+
+
+}
