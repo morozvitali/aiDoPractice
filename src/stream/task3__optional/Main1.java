c66f36a539e19927d3decb206a999fa6e158c83c
morozvitali <morozvitali99@gmail.com>
2025-06-26 11:14:40 +0300

    Practice [Streams] task 3.2 Optional -> map and orElse

--- src/stream/task3__optional/Main1.java
+++ src/stream/task3__optional/Main1.java
@@ -1,6 +1,7 @@
 package stream.task3__optional;
 
 import java.util.List;
+import java.util.Optional;
 
 public class Main1 {
     public int practice1 () {
@@ -8,5 +9,8 @@ public class Main1 {
         return numbers.stream().filter(a->a%5 == 0).findFirst().orElse(-1);
     }
 
-
+    public void practice2 () {
+        Optional<Integer> value = Optional.of(7);
+        value.map(x->2*x).orElse(-1);
+    }
 }
