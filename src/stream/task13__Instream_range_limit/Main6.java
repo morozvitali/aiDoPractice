fa335c0e49bf4f8ca44e56ee9d47c8d6e4158403
morozvitali <morozvitali99@gmail.com>
2025-06-27 11:32:44 +0300

    Practice [Streams] -reformatted -updated -forsed 13.9 IntStream iterate limit

--- src/stream/task13__Instream_range_limit/Main6.java
+++ src/stream/task13__Instream_range_limit/Main6.java
@@ -41,4 +41,15 @@ public class Main6 {
         IntStream.iterate(1, i ->i+3).filter(a -> a%2 == 0)
                 .limit(10).forEach(System.out::println);
     }
+
+    public void practice9 () {
+        IntStream.iterate(1, i->i+1).
+                map(a->a*a)
+                .filter(a->a%5 != 0)
+                .limit(10)
+                .forEach(System.out::println);
+    }
+
+
+
 }
