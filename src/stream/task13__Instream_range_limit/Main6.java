887334770987ba62864065b943230e1025f51fb3
morozvitali <morozvitali99@gmail.com>
2025-06-27 11:19:25 +0300

    Practice [Streams] -reformatted -updated -forsed 13.2 IntStream iterate limit

--- src/stream/task13__Instream_range_limit/Main6.java
+++ src/stream/task13__Instream_range_limit/Main6.java
@@ -4,15 +4,15 @@ import java.util.stream.IntStream;
 
 public class Main6 {
 
-    public static void practice2 () {
-        IntStream.iterate(2, i -> i+2).limit(10).forEach(System.out::println);
+    public static void practice2() {
+        IntStream.iterate(2, i -> i + 2).limit(10).forEach(System.out::println);
     }
 
     public static void main(String[] args) {
         Main6.practice2();
     }
 
-
-
-
+    public void practice3() {
+        IntStream.iterate(1, i -> i+2).limit(10).forEach(System.out::println);
+    }
 }
