cdbe90eba91d0082f3291666064b8f6036e25fa4
morozvitali <morozvitali99@gmail.com>
2025-07-01 10:08:12 +0300

    Practice [Streams] Task 1.2 mapToInt -> sum

--- src/stream/task1__sorting/Main6.java
+++ src/stream/task1__sorting/Main6.java
@@ -10,5 +10,12 @@ public class Main6 {
         return numbers.stream().filter(a -> a % 2 == 0).map(a -> a * a).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
     }
 
+    public int practice2 () {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7);
+        return numbers.stream().filter(a->a%2!=0).map(a->a*a).mapToInt(a->(int)a).sum();
+    }
+
+
+
 
 }
