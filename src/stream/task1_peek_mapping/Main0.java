9437ef494c1f6d0a2d3f00846b18a4cc4b516f4b
morozvitali <morozvitali99@gmail.com>
2025-07-01 12:17:37 +0300

    Practice [Streams] Task 1.2 peek mapping

--- src/stream/task1_peek_mapping/Main0.java
+++ src/stream/task1_peek_mapping/Main0.java
@@ -1,6 +1,7 @@
 package stream.task1_peek_mapping;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main0 {
@@ -15,4 +16,16 @@ public class Main0 {
                 .collect(Collectors.toList());
     }
 
+    public void practice2 () {
+        List<String> emails = List.of("ivan@ukr.net", "olga@gmail.com", "petro@ukr.net");
+        Map<Integer, List<String>> byLoginLength =
+                emails.stream()
+                        .collect(Collectors.groupingBy(
+                                email -> email.substring(0, email.indexOf("@")).length(),
+                                Collectors.mapping(
+                                        email -> email.substring(email.indexOf("@") + 1),
+                                        Collectors.toList()
+                                )
+                        ));
+    }
 }
