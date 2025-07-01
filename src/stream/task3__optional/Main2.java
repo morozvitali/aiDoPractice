e75bb856cc37a78b36abc6bc7f71593ecdb05f69
morozvitali <morozvitali99@gmail.com>
2025-07-01 10:36:06 +0300

    Practice [Streams] Task 3.2 Optional

--- src/stream/task3__optional/Main2.java
+++ src/stream/task3__optional/Main2.java
@@ -1,6 +1,7 @@
 package stream.task3__optional;
 
 import java.util.List;
+import java.util.Optional;
 
 public class Main2 {
 
@@ -8,4 +9,10 @@ public class Main2 {
         List<Integer> numbers = List.of(3, 8, 10, 15, 4);
         return numbers.stream().filter(a->a%5==0).findFirst().orElse(-1);
     }
+
+    public int practice2 () {
+        Optional<Integer> value = Optional.of(7);
+        return value.map(a->a*a).orElse(-1);
+    }
+
 }
