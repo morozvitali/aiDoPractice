e4393ddf1e19f750d1c2a625a179976258d74a8a
morozvitali99 <morozvitali99@gmail.com>
2025-07-03 12:25:14 +0300

    commit [Streams] task 5.4 limit skip

--- src/stream/task5_skip_limit/Main6.java
+++ src/stream/task5_skip_limit/Main6.java
@@ -18,4 +18,10 @@ public class Main6 {
         List<Integer> list = List.of(100, 200, 300, 400, 500);
     list.stream().skip(2).limit(3).forEach(System.out::println);
     }
+
+    public void practice4 () {
+        List<Integer> list = List.of(1, 2, 3, 4, 5);
+        list.stream().skip(list.size()-2).forEach(System.out::println);
+    }
+
 }
