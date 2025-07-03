0049fb8d0755d903dae632cca4e04b850968fda2
morozvitali99 <morozvitali99@gmail.com>
2025-07-03 14:47:13 +0300

    commit [Streams] task 8.3 Int Stream

--- src/stream/task8star_IntStream/Main6.java
+++ src/stream/task8star_IntStream/Main6.java
@@ -3,23 +3,29 @@ package stream.task8star_IntStream;
 import java.util.Arrays;
 
 public class Main6 {
-    public int practice0 () {
+    public int practice0() {
         int[] numbers = {2, 8, 15, 21, 4, 9};
-        return Arrays.stream(numbers).filter(a->a%2==1)
-                .reduce((a,b)->a>b?a:b).orElse(-1);
+        return Arrays.stream(numbers).filter(a -> a % 2 == 1)
+                .reduce((a, b) -> a > b ? a : b).orElse(-1);
     }
 
-    public void practice1 () {
+    public void practice1() {
         int[] numbers = {-5, 3, 0, -2, 10, 1};
         Arrays.stream(numbers)
-                .filter(a->a>0)
-                .reduce((a,b)-> a<b?a:b)
+                .filter(a -> a > 0)
+                .reduce((a, b) -> a < b ? a : b)
                 .orElse(-1);
     }
 
-    public void practice2 () {
+    public int practice2() {
         int[] numbers = {4, 9, 6, 14, 21, 5};
-        Arrays.stream(numbers).filter(a->a%3==0).reduce((a,b)->a>b?a:b).orElse(-1);
+        return Arrays.stream(numbers).filter(a -> a % 3 == 0).reduce((a, b) -> a > b ? a : b).orElse(-1);
     }
 
+    public int practice3 () {
+        int[] numbers = {7, 13, 4, 6, 9};
+        return Arrays.stream(numbers)
+                .filter(a->a%2==0)
+                .reduce ((a,b)-> a>b? a:b).orElse(-100);
+    }
 }
