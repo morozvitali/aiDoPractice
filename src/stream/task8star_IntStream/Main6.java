7901c2171aef27e149e61b2bed4b074ebc8e4f96
morozvitali99 <morozvitali99@gmail.com>
2025-07-03 14:39:02 +0300

    commit [Streams] task 8.1 Int Stream

--- src/stream/task8star_IntStream/Main6.java
+++ src/stream/task8star_IntStream/Main6.java
@@ -3,13 +3,19 @@ package stream.task8star_IntStream;
 import java.util.Arrays;
 
 public class Main6 {
-    public void practice1 () {
+    public int practice0 () {
         int[] numbers = {2, 8, 15, 21, 4, 9};
-        Arrays.stream(numbers).filter(a->a%2==1)
+        return Arrays.stream(numbers).filter(a->a%2==1)
                 .reduce((a,b)->a>b?a:b).orElse(-1);
     }
 
-
+    public void practice1 () {
+        int[] numbers = {-5, 3, 0, -2, 10, 1};
+        Arrays.stream(numbers)
+                .filter(a->a>0)
+                .reduce((a,b)-> a<b?a:b)
+                .orElse(-1);
+    }
 
 
 
