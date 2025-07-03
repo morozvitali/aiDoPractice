ff4faaf9f5f3fc9bb4282569dbcca10b8c590b53
morozvitali99 <morozvitali99@gmail.com>
2025-07-03 14:18:04 +0300

    commit [Streams] task 7.1  Int Stream range closed

---
+++ src/stream/task7_anymatch/Main6.java
@@ -0,0 +1,12 @@
+package stream.task7_anymatch;
+
+import java.util.List;
+
+public class Main6 {
+    public boolean practice1 () {
+        List<Integer> list = List.of(3, -5, 7, 10);
+        return list.stream().anyMatch(n-> n<0);
+    }
+
+
+}
