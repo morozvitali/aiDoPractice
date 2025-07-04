5fe92262fc6f5bb73d694419dd91182fa45bf8dc
morozvitali <morozvitali99@gmail.com>
2025-07-04 10:34:46 +0300

    Practice [Streams] Task 13.2 IntStream Iterate

--- src/stream/task13__Instream_iterator/Main0.java
+++ src/stream/task13__Instream_iterator/Main0.java
@@ -3,7 +3,12 @@ package stream.task13__Instream_iterator;
 import java.util.stream.IntStream;
 
 public class Main0 {
-    public void practice1 () {
-        IntStream.iterate(10, i->i-1).limit(10);
+    public void practice1() {
+        IntStream.iterate(10, i -> i - 1).limit(10).forEach(System.out::println);
     }
+
+    public void practice2 () {
+        IntStream.iterate(2, i->i+2).limit(10).forEach(System.out::println);
+    }
+
 }
