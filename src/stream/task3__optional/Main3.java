fd1084bde9e1f5a3f04d1465654f7e77710221c0
morozvitali99 <morozvitali99@gmail.com>
2025-07-07 14:57:16 +0300

    commit [Streams] 3.2 Optional

--- src/stream/task3__optional/Main3.java
+++ src/stream/task3__optional/Main3.java
@@ -1,6 +1,7 @@
 package stream.task3__optional;
 
 import java.util.List;
+import java.util.Optional;
 
 public class Main3 {
     public int practice1() {
@@ -8,5 +9,9 @@ public class Main3 {
         return numbers.stream().filter(a->a%5==0).findFirst().orElse(-1);
     }
 
+    public void practice2 () {
+        Optional<Integer> value = Optional.of(7);
+        value.map(a->a*2).orElse(-1);
+    }
 
 }
