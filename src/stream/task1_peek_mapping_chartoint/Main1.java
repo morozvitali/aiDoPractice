64e36bc46f9431c906aa029c1dcdd164c10b769c
morozvitali99 <morozvitali99@gmail.com>
2025-07-07 12:38:00 +0300

    commit [Streams] 1.4 char To Int

--- src/stream/task1_peek_mapping_chartoint/Main1.java
+++ src/stream/task1_peek_mapping_chartoint/Main1.java
@@ -5,14 +5,16 @@ public class Main1 {
         return String.valueOf(n).chars().map(c -> c - '0').map(d -> d * d).sum();
     }
 
-    public boolean practice2 (int n) {
-        return String.valueOf(n).chars().map(c->c-'0').allMatch(d->d%2==0);
+    public boolean practice2(int n) {
+        return String.valueOf(n).chars().map(c -> c - '0').allMatch(d -> d % 2 == 0);
     }
 
-    public int practice3 (int n) {
-        return String.valueOf(n).chars().map(c->c-'0')
+    public int practice3(int n) {
+        return String.valueOf(n).chars().map(c -> c - '0')
                 .max().orElse(-1);
     }
 
-
+    public long practice4(int n) {
+        return String.valueOf(n).chars().map(c -> c - '0').filter(a -> a > 5).count();
+    }
 }
