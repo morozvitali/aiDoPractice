3ba71f5a86f72b488d7d6c3ad5553c8e143f9a66
morozvitali99 <morozvitali99@gmail.com>
2025-07-08 10:19:36 +0300

    commit [Streams] 6.6 IntStream range

--- src/stream/task6_intstream_range/Main0.java
+++ src/stream/task6_intstream_range/Main0.java
@@ -1,5 +1,6 @@
 package stream.task6_intstream_range;
 
+import java.math.BigInteger;
 import java.util.stream.IntStream;
 
 public class Main0 {
@@ -21,6 +22,12 @@ public class Main0 {
 
     public void practice5 () {
         IntStream.rangeClosed(1,30).filter(a->a%4==0).forEach(System.out::println);
+    }
+
+    public void practice6 () {
+        IntStream.rangeClosed(1,31).filter(a->a%2==1)
+                .mapToObj(a->BigInteger.valueOf(a))
+                .reduce(BigInteger.ONE, (a,b)->a.multiply(b));
     }
 
 
