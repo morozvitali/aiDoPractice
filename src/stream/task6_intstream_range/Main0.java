bc3611b5850d1e04db4dbbc70047eab2daf08a26
morozvitali99 <morozvitali99@gmail.com>
2025-07-08 10:05:30 +0300

    commit [Streams] 6.3 IntStream range

--- src/stream/task6_intstream_range/Main0.java
+++ src/stream/task6_intstream_range/Main0.java
@@ -11,6 +11,8 @@ public class Main0 {
         return IntStream.rangeClosed(10,20).filter(a->a%2==0).count();
     }
 
-
+    public int practice3 () {
+        return IntStream.rangeClosed(21,40).filter(a->a%2==1).min().orElse(-1);
+    }
 
 }
