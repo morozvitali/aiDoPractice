9af7000614f835a543c194a8d2883b35df6b6c5f
morozvitali99 <morozvitali99@gmail.com>
2025-07-09 09:45:07 +0300

    commit [Streams] 11.1 joining

--- src/stream/task11__mapping/Main0.java
+++ src/stream/task11__mapping/Main0.java
@@ -1,7 +1,14 @@
 package stream.task11__mapping;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Main0 {
     public void practice1 () {
-
+        Arrays.stream("Hey fellow warriors"
+                .split(" "))
+                .map(s->s.length() < 5 ? s : new StringBuilder(s)
+                        .reverse())
+                .collect(Collectors.joining(" "));
     }
 }
