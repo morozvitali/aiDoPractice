5ff5cfd03bd8f7546a7730c6dc31995a1a224267
morozvitali99 <morozvitali99@gmail.com>
2025-07-09 10:27:54 +0300

    commit [Streams] 12.1

---
+++ src/stream/task12_plus/Main0.java
@@ -0,0 +1,14 @@
+package stream.task12_plus;
+
+import java.util.stream.Collectors;
+
+public class Main0 {
+    public int practice1 (int n) {
+        return Integer.parseInt(String.valueOf(n)
+                .chars()
+                .map(ch->Character.getNumericValue(ch))
+                .map(a->a*a)
+                .mapToObj(a->String.valueOf(a))
+                .collect(Collectors.joining()));
+    }
+}
