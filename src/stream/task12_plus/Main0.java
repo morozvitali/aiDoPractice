28543db0035c7193949734c81d99ef220d46fd4f
morozvitali99 <morozvitali99@gmail.com>
2025-07-09 10:44:49 +0300

    commit [Streams] 12.5

--- src/stream/task12_plus/Main0.java
+++ src/stream/task12_plus/Main0.java
@@ -3,22 +3,56 @@ package stream.task12_plus;
 import java.util.stream.Collectors;
 
 public class Main0 {
-    public int practice1 (int n) {
+    public int practice1(int n) {
         return Integer.parseInt(String.valueOf(n)
                 .chars()
-                .map(ch->Character.getNumericValue(ch))
-                .map(a->a*a)
-                .mapToObj(a->String.valueOf(a))
+                .map(ch -> Character.getNumericValue(ch))
+                .map(a -> a * a)
+                .mapToObj(a -> String.valueOf(a))
                 .collect(Collectors.joining()));
     }
 
-    public int practice2 (int number) {
+    public int practice2(int number) {
         return Integer.parseInt(String.valueOf(number)
                 .chars()
-                .map(ch->Character.getNumericValue(ch))
-                .map(a->a*a)
-                .mapToObj(a->String.valueOf(a))
+                .map(ch -> Character.getNumericValue(ch))
+                .map(a -> a * a)
+                .mapToObj(a -> String.valueOf(a))
                 .collect(Collectors.joining()));
     }
+
+    public int practice3(int number) {
+        return Integer.parseInt(
+                String.valueOf(number)
+                        .chars()
+                        .map(ch -> Character.getNumericValue(ch))
+                        .map(a -> a * a * a)
+                        .mapToObj(a -> String.valueOf(a))
+                        .collect(Collectors.joining())
+        );
+    }
+
+    public int practice4 (int number) {
+        return Integer.parseInt(
+                String.valueOf(number)
+                        .chars()
+                        .map(ch->Character.getNumericValue(ch))
+                        .filter(a->a%2==0)
+                        .mapToObj(a->String.valueOf(a))
+                        .collect(Collectors.joining())
+        );
+    }
+
+    public int practice5 (int number) {
+        return Integer.parseInt(
+                String.valueOf(number)
+                        .chars()
+                        .map(ch->Character.getNumericValue(ch))
+                        .mapToObj(a->"*".repeat(a))
+                        .collect(Collectors.joining())
+        );
+    }
+
+
 
 }
