72454bec3b5093f67c691d18eb6d7110af85fcb2
morozvitali99 <morozvitali99@gmail.com>
2025-07-09 10:33:03 +0300

    commit [Streams] 12.2

--- src/stream/task12_plus/Main0.java
+++ src/stream/task12_plus/Main0.java
@@ -11,4 +11,14 @@ public class Main0 {
                 .mapToObj(a->String.valueOf(a))
                 .collect(Collectors.joining()));
     }
+
+    public int practice2 (int number) {
+        return Integer.parseInt(String.valueOf(number)
+                .chars()
+                .map(ch->Character.getNumericValue(ch))
+                .map(a->a*a)
+                .mapToObj(a->String.valueOf(a))
+                .collect(Collectors.joining()));
+    }
+
 }
