1fd02c3adf05494e9e578a3cc97454b2080d2e99
morozvitali99 <morozvitali99@gmail.com>
2025-07-09 11:46:37 +0300

    commit [Streams] 13.2

---
+++ src/stream/task13__Instream_iterator/Main7.java
@@ -0,0 +1,15 @@
+package stream.task13__Instream_iterator;
+
+import java.util.stream.IntStream;
+
+public class Main7 {
+    public void practice1 () {
+        IntStream.iterate(10, i->i-1).limit(10).forEach(System.out::println);
+    }
+
+    public void practice2 () {
+        IntStream.iterate(2, i->i+2).limit(10).forEach(System.out::println);
+    }
+
+
+}
