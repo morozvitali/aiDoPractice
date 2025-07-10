c7113cd1b528a83849c33cce4b539ac4eb62d643
morozvitali99 <morozvitali99@gmail.com>
2025-07-10 13:48:01 +0300

    commit [Streams] 16.1

---
+++ src/stream/task16_grouping2/Main1.java
@@ -0,0 +1,19 @@
+package stream.task16_grouping2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main1 {
+    public Map<Integer, List<String>> practice1 () {
+        String[] words = {"apple", "banana",
+                "dog", "kiwi", "plum"};
+        return Arrays.stream(words)
+                .map(a -> a.toUpperCase())
+                .collect(Collectors.groupingBy(String::length));
+    }
+
+
+
+}
