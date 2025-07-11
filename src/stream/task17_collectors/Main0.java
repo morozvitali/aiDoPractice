0669f22d8ae67640512b3a7a4ba18dac4857d289
morozvitali99 <morozvitali99@gmail.com>
2025-07-11 15:19:47 +0300

    commit [Streams] 17.10

--- src/stream/task17_collectors/Main0.java
+++ src/stream/task17_collectors/Main0.java
@@ -78,7 +78,10 @@ public class Main0 {
                 Collectors.toSet()));
     }
 
-
+    public String practice10 () {
+        List<Integer> numbers = List.of(4, 8, 15, 16, 23, 42);
+        return numbers.stream().collect(Collectors.collectingAndThen(Collectors.counting(),count -> "Count " + count));
+    }
 
 
 
