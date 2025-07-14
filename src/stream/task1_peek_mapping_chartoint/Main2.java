3532df7b525a2f0f926fc8477ecfac55b9d4b76d
morozvitali99 <morozvitali99@gmail.com>
2025-07-14 17:12:54 +0300

    commit [Streams] 1.3 peek mapping

--- src/stream/task1_peek_mapping_chartoint/Main2.java
+++ src/stream/task1_peek_mapping_chartoint/Main2.java
@@ -11,5 +11,10 @@ public class Main2 {
         return String.valueOf(n).chars().allMatch(a-> a%2 == 0);
     }
 
+    public int practice3 (int n) {
+        return String.valueOf(n).chars().max().orElse(-1);
+    }
+
+
 
 }
