68c801922a4edc791989cde78376909987ab85fa
morozvitali99 <morozvitali99@gmail.com>
2025-07-15 12:33:27 +0300

    commit [Streams] 3.4 optional

--- src/stream/task3__optional/Main0.java
+++ src/stream/task3__optional/Main0.java
@@ -4,20 +4,27 @@ import java.util.List;
 import java.util.Optional;
 
 public class Main0 {
-public int practice1 () {
-    List<Integer> numbers = List.of(3, 8, 10, 15, 4);
-    return numbers.stream().filter(a->a%5==0).findFirst().orElse(-1);
-}
+    public int practice1() {
+        List<Integer> numbers = List.of(3, 8, 10, 15, 4);
+        return numbers.stream().filter(a -> a % 5 == 0).findFirst().orElse(-1);
+    }
 
-public int practice2 () {
-    Optional<Integer> value = Optional.of(7);
+    public int practice2() {
+        Optional<Integer> value = Optional.of(7);
 
-    return value.map(x->x*2).orElse(-1);
-}
+        return value.map(x -> x * 2).orElse(-1);
+    }
 
-public String practice3 () {
-    Optional <String> value = Optional.of("HelloWorld");
-    return value.filter(a->a.length() > 5).orElse("short");
- }
+    public String practice3() {
+        Optional<String> value = Optional.of("HelloWorld");
+        return value.filter(a -> a.length() > 5).orElse("short");
+    }
+
+    public String practice4 () {
+        Optional <String> name = Optional.of("Vitali");
+        return name.map(n-> "Hello, " + n + "!").orElse("Hello, guest");
+    }
+
+
 
 }
