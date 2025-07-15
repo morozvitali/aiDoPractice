0cce804022e7a177eee2e7daa3bce88fa4ff869d
morozvitali99 <morozvitali99@gmail.com>
2025-07-15 16:18:00 +0300

    commit [Streams] 11.2 optional

--- src/stream/task11__mapping/Main1.java
+++ src/stream/task11__mapping/Main1.java
@@ -5,10 +5,18 @@ import java.util.stream.Collectors;
 
 public class Main1 {
     public String practice1 () {
-        return Arrays.stream("Hey fellow warriors".split(" ")).map(a->a.length() < 5 ? a : new StringBuilder(a).reverse().toString())
+        return Arrays.stream("Hey fellow warriors".split(" "))
+                .map(a->a.length() < 5 ? a : new StringBuilder(a).reverse().toString())
                 .collect(Collectors.joining(" "));
     }
 
-
+    public String practice2 () {
+        return Arrays
+                .stream("Java is amazing language for backend"
+                        .split(" "))
+                .map(a->a.length() <5 ? a : "[...]")
+                .collect(Collectors
+                        .joining(" "));
+    }
 
 }
