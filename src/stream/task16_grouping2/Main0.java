12f987aca9dd3c8a920aa11470c59952040a0411
morozvitali99 <morozvitali99@gmail.com>
2025-07-17 17:06:59 +0300

    commit [Streams] 16.3

--- src/stream/task16_grouping2/Main0.java
+++ src/stream/task16_grouping2/Main0.java
@@ -6,29 +6,45 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main0 {
-    public void practice1 () {
+    public void practice1() {
         String[] words = {"apple", "banana",
                 "dog", "kiwi", "plum"};
-        Arrays.stream(words).map(a->a.toUpperCase())
-                .collect(Collectors.groupingBy(w->w.length()));
+        Arrays.stream(words).map(a -> a.toUpperCase())
+                .collect(Collectors.groupingBy(w -> w.length()));
     }
 
-    public void practice2 () {
+    public void practice2() {
         String[] words = {"apple", "banana",
                 "dog", "kiwi", "plum"};
 
-        Map<Integer, List<String>> map =  Arrays.stream(words)
-                .filter(w->w.chars()
-                        .map(c->(char)c)
-                        .filter(c-> "aeiou".indexOf(c)>=0).count()>0).collect(Collectors.groupingBy(a->a.length()));
+        Map<Integer, List<String>> map = Arrays.stream(words)
+                .filter(w -> w.chars()
+                        .map(c -> (char) c)
+                        .filter(c -> "aeiou"
+                                .indexOf(c) >= 0)
+                        .count() > 0)
+                .collect(Collectors
+                        .groupingBy(a -> a
+                                .length()));
 
         for (var e : map.entrySet()) {
             System.out.println(e.getKey() + "->" + e.getValue());
         }
-
-
-
     }
+
+    public void practice3() {
+        String[] words = {"apple", "sky",
+                "banana", "dry", "orange"};
+
+        Arrays.stream(words)
+                .collect(Collectors
+                        .partitioningBy(a->a
+                                .chars().map(c->(char)c)
+                                .filter(ch->"aeiou"
+                                        .indexOf(ch)>=0)
+                                .count() > 0));
+    }
+
 
 
 }
