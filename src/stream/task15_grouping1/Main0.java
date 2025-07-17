87cad9fc21de04bb38282cf6731bc7efcf60a1bf
morozvitali99 <morozvitali99@gmail.com>
2025-07-17 11:09:30 +0300

    commit [Streams] 15.1

---
+++ src/stream/task15_grouping1/Main0.java
@@ -0,0 +1,23 @@
+package stream.task15_grouping1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main0 {
+    public Map<Integer, List<String>> practice1 () {
+        String[] words = {"apple", "dog", "banana",
+                "cat", "kiwi", "plum", "watermelon"};
+        return Arrays.stream(words).collect(Collectors.groupingBy(String::length));
+    }
+
+    public Map <Character, List <String>> practice2 () {
+        List<String> words = List.of("Apple", "apricot",
+                "Banana", "blue", "Cherry", "cranberry");
+        return words.stream().map(a->a.toLowerCase()).collect(Collectors.groupingBy(w->w.charAt(0)));
+    }
+
+
+
+}
