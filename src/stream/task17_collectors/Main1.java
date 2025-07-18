1328520371e8d2fda2cb3a008e991e7f98f12d5f
morozvitali99 <morozvitali99@gmail.com>
2025-07-18 14:38:48 +0300

    commit [Streams] 17.2

--- src/stream/task17_collectors/Main1.java
+++ src/stream/task17_collectors/Main1.java
@@ -1,14 +1,27 @@
 package stream.task17_collectors;
 
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main1 {
-    public void practice1 () {
+    public String practice1 () {
         List<String> names = List.of("Alice", "Bob", "Charlie");
-    names.stream().collect(Collectors.joining(", "));
-
-
-
+    return names.stream().collect(Collectors.joining(", "));
     }
+
+    public void practice2 () {
+        List<Integer> numbers = List.of(4, 8, 15, 16, 23, 42);
+        IntSummaryStatistics stats = numbers.stream()
+                .collect(Collectors
+                        .summarizingInt(value -> value));
+
+        System.out.println(stats.getAverage());
+        System.out.println(stats.getSum());
+        System.out.println(stats.getMin());
+        System.out.println(stats.getMax());
+    }
+
+
+
 }
