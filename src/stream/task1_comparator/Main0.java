517ead755787640e260a06ba7a94b4f51b0dca40
itdep <morozvitali99@gmail.com>
2025-07-24 16:14:30 +0300

    [Stream] practice task comparator 3

--- src/stream/task1_comparator/Main0.java
+++ src/stream/task1_comparator/Main0.java
@@ -1,6 +1,7 @@
 package stream.task1_comparator;
 
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
@@ -22,6 +23,12 @@ public class Main0 {
                 .max(Comparator.comparing(Map.Entry::getValue))
                 .map(Map.Entry::getKey)
                 .orElse("Empty");
+    }
+
+    public void practice3 () {
+        List.of(111, 123, 444, 1212).stream().map(n -> Map.entry(n, (int) String.valueOf(n).chars().distinct().count()))
+                .max(Map.Entry.comparingByValue())
+                .get().getKey();
     }
 
 
