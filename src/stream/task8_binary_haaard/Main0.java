486b02ea0bcd0f2d756d7ce0e9f8c1031e7cc3bb
itdep <morozvitali99@gmail.com>
2025-07-28 15:36:08 +0300

    [Stream] practice task 8 binary 2

--- src/stream/task8_binary_haaard/Main0.java
+++ src/stream/task8_binary_haaard/Main0.java
@@ -1,12 +1,15 @@
 package stream.task8_binary_haaard;
 
 public class Main0 {
-    public int practice (int number) {
+    public int practice1 (int number) {
         String binary = Integer.toBinaryString(number);
         long count =binary.chars().filter(ch -> ch == '1').count();
         return (int) count;
     }
 
+    public int practice2 (int n) {
+        return (int)Integer.toBinaryString(n).chars().filter(a-> a=='1').count();
+    }
 
 
 
