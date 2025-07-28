97553a58013b0787f7445b64657be590344a00bc
itdep <morozvitali99@gmail.com>
2025-07-28 15:40:10 +0300

    [Stream] practice task 8 binary 3

--- src/stream/task8_binary_haaard/Main0.java
+++ src/stream/task8_binary_haaard/Main0.java
@@ -11,6 +11,15 @@ public class Main0 {
         return (int)Integer.toBinaryString(n).chars().filter(a-> a=='1').count();
     }
 
+    public void practice3 (int n) {
+        int count  =0;
+        while (n!=0) {
+            if ((n&1) == 1) {
+                count++;
+            }
+            n = n>>1;
+        }
+    }
 
 
 }
