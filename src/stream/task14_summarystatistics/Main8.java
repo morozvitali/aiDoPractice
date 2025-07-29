5a574abe83fb6e2c58db23f4deb6df3808372a02
itdep <morozvitali99@gmail.com>
2025-07-29 16:12:08 +0300

    [Stream] practice task 14 summary 3

--- src/stream/task14_summarystatistics/Main8.java
+++ src/stream/task14_summarystatistics/Main8.java
@@ -3,6 +3,9 @@ package stream.task14_summarystatistics;
 import java.util.Arrays;
 import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Main8 {
     public void practice1 () {
@@ -19,5 +22,14 @@ public class Main8 {
                 "Banana", "CHERRY", "kiwi", "PLUM");
     words.stream().filter(w->w.length() % 2 == 0).map(w->w.toLowerCase()).mapToInt(a->a.length()).sum();
     }
+
+    public Map<String, Long> practice3 () {
+        String[] words = {"sky", "apple",
+                "moon", "dry", "banana"};
+        return Arrays.stream(words).filter(a->a.length() > 3 && a.chars().anyMatch(c->"AEIOU".indexOf(c) >= 0)).collect(Collectors.toMap(Function.identity(), w->w.chars().filter(ch-> "aeiou".indexOf(ch) >=0).count()));
+    }
+
+
+
 
 }
