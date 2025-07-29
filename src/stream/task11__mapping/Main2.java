083b3865fad3b833dc74cc3f3bf35fca2729f0eb
itdep <morozvitali99@gmail.com>
2025-07-29 12:25:49 +0300

    [Stream] practice task 11 diff 3

--- src/stream/task11__mapping/Main2.java
+++ src/stream/task11__mapping/Main2.java
@@ -11,4 +11,17 @@ public class Main2 {
     public void practice2 () {
         Arrays.stream("Java is amazing language for backend".split(" ")).map(a->a.length() < 5 ? a : "[...]").collect(Collectors.joining());
     }
+
+    public void practice3 () {
+        String [] str = "I love my keyboard very much".split(" ");
+
+        String temp = str[0];
+        str[0] = str[str.length -1];
+        str[str.length -1] = temp;
+        String.join(" ", str);
+    }
+
+
+
+
 }
