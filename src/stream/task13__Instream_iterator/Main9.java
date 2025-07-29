f15741ebc8b36e6dfd3b8ffeb7ae142c416b33f3
itdep <morozvitali99@gmail.com>
2025-07-29 14:56:39 +0300

    [Stream] practice task 13 plus 3

--- src/stream/task13__Instream_iterator/Main9.java
+++ src/stream/task13__Instream_iterator/Main9.java
@@ -12,4 +12,10 @@ public void practice2 () {
         IntStream.iterate(2, i -> i+2).limit(10).forEach(System.out::println);
 }
 
+public void practice3 () {
+        IntStream.iterate(1, i-> i+2).limit(10).forEach(System.out::println);
+}
+
+
+
 }
