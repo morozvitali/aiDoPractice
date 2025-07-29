8ac5a4bacc8faa20a27370167f24ac2d427cee96
itdep <morozvitali99@gmail.com>
2025-07-29 13:54:51 +0300

    [Stream] practice task 12 plus 3

--- src/stream/task12_plus/Main2.java
+++ src/stream/task12_plus/Main2.java
@@ -11,5 +11,7 @@ public class Main2 {
         return Integer.parseInt(String.valueOf(number).chars().map(Character::getNumericValue).map(value-> value * value * value).mapToObj(String::valueOf).collect(Collectors.joining()));
     }
 
-
+    public int practice3 (int number) {
+        return Integer.parseInt(String.valueOf(number).chars().filter(a->a%2==0).mapToObj(String::valueOf).collect(Collectors.joining()));
+    }
 }
