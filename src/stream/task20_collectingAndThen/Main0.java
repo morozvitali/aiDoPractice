7aa9fc93c853da5a5d4c5e5dc79a013a358f06b3
itdep <morozvitali99@gmail.com>
2025-07-31 15:24:26 +0300

    [Stream] task 20 /collecting and then/ [✅Completed 1 of 11 overall]

--- src/stream/task20_collectingAndThen/Main0.java
+++ src/stream/task20_collectingAndThen/Main0.java
@@ -6,11 +6,17 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main0 {
-    public void practice1() {
+    public void practice0() {
         List<Integer> numbers = List.of(10, 70, 20, 90, 55, 30);
         numbers.stream().filter(a-> a>50).sorted(Comparator.reverseOrder()).collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
     }
 
+    public void practice1 () {
+        List<String> data = List.of("apple", "car",
+                "banana", "hi", "cherry");
+        data.stream().filter(a->a.length() >4).collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
 
+
+    }
 
 }
