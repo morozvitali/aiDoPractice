eb6a7efeea669157048f5b2f3cbf0174370e89c3
itdep <morozvitali99@gmail.com>
2025-07-31 15:19:17 +0300

    [Stream] task 20 collecting and then [ADDED 11 TASKS]

--- src/stream/task20_collectingAndThen/Main0.java
+++ src/stream/task20_collectingAndThen/Main0.java
@@ -10,4 +10,7 @@ public class Main0 {
         List<Integer> numbers = List.of(10, 70, 20, 90, 55, 30);
         numbers.stream().filter(a-> a>50).sorted(Comparator.reverseOrder()).collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
     }
+
+
+
 }
