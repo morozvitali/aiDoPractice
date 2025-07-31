a26c79df4308eabc2286aafc5c0923ee9a1b1f0b
itdep <morozvitali99@gmail.com>
2025-07-31 14:41:15 +0300

    [Stream] practice task 19 sorting 3

--- src/stream/task19_sorted/Main0.java
+++ src/stream/task19_sorted/Main0.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main0 {
-    public void practice1 () {
+    public void practice0 () {
         List<String> words = List.of("sun", "banana", "kiwi",
                 "cherry", "apple", "tea", "pear");
         words.stream().filter(a->a.length() >3)
@@ -13,10 +13,20 @@ public class Main0 {
                 .thenComparing(Comparator.naturalOrder()));
     }
 
-    public void practice2 () {
+    public void practice1 () {
         List<String> words = List.of("tea", "lemon", "fig", "grape", "banana");
         words.stream().filter(a->a.length() > 3).sorted(Comparator.comparing(String::length)).collect(Collectors.toList());
     }
 
+    public void practice2 () {
+        List<String> words = List.of("Zebra", "apple",
+                "Lemon", "banana");
+        words.stream().map(String::toLowerCase).sorted(Comparator.naturalOrder());
 
+    }
+
+    public void practice3 () {
+        List<Integer> numbers = List.of(3, 7, 1, 9, 2);
+        List <Integer> list = numbers.stream().sorted(Comparator.reverseOrder()).toList();
+    }
 }
