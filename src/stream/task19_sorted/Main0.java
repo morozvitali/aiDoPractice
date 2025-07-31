6f2f1e06d6a5d0eb0dfbb862b75e87dbb2334ba9
itdep <morozvitali99@gmail.com>
2025-07-31 14:36:59 +0300

    [Stream] practice task 19 sorting 2

--- src/stream/task19_sorted/Main0.java
+++ src/stream/task19_sorted/Main0.java
@@ -2,6 +2,7 @@ package stream.task19_sorted;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main0 {
     public void practice1 () {
@@ -12,6 +13,10 @@ public class Main0 {
                 .thenComparing(Comparator.naturalOrder()));
     }
 
+    public void practice2 () {
+        List<String> words = List.of("tea", "lemon", "fig", "grape", "banana");
+        words.stream().filter(a->a.length() > 3).sorted(Comparator.comparing(String::length)).collect(Collectors.toList());
+    }
 
 
 }
