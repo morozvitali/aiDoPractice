0bf6035bc4834f2d535c9ab9b099884036937235
itdep <morozvitali99@gmail.com>
2025-07-31 13:35:38 +0300

    [Stream] practice task 18 flatmap 1

--- src/stream/task18_flatMap/Main0.java
+++ src/stream/task18_flatMap/Main0.java
@@ -1,23 +1,36 @@
 package stream.task18_flatMap;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main0 {
+    public void practice0() {
+        List<List<Integer>> weeklyData = List.of(
+                List.of(40, 55, 60),
+                List.of(30, 70),
+                List.of(65, 10, 90)
+        );
+
+        weeklyData.stream().flatMap(list -> list.stream())
+                .filter(value -> value > 50)
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.toList());
+    }
+
     public void practice1 () {
-    List<List<Integer>> weeklyData = List.of(
-            List.of(40, 55, 60),
-            List.of(30, 70),
-            List.of(65, 10, 90)
-    );
+        int[][] data = {
+                {3, 11},
+                {6, 7},
+                {10}
+        };
 
-    weeklyData.stream().flatMap(list->list.stream())
-            .filter(value->value > 50)
-            .sorted(Comparator.naturalOrder())
-            .collect(Collectors.toList())
-
-
+        Arrays.stream(data).flatMapToInt(arr -> Arrays.stream(arr))
+                .filter(a->a%2==0)
+                .min()
+                .orElse(-1);
+    }
 
 
 }
