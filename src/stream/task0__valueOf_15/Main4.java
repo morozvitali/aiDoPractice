7b223aa4ca2ab25fc8779a432196ce1b6fd6f823
itdep <morozvitali99@gmail.com>
2025-08-01 14:45:49 +0300

    [📋Stream 01] [✅3 of 15 overall]

--- src/stream/task0__valueOf_15/Main4.java
+++ src/stream/task0__valueOf_15/Main4.java
@@ -1,12 +1,34 @@
 package stream.task0__valueOf_15;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main4 {
+
     public List <String> practice1 () {
         List<Integer> codes = List.of(97, 98, 99, 100, 101);
         return codes.stream().map(c->String.valueOf((char)(int)c)).collect(Collectors.toList());
     }
+
+public void practice2 () {
+    String mixed = "a1b2c3x9z7";
+    mixed.chars()
+            .filter(value->Character
+                    .isDigit(value))
+            .mapToObj(c->Character
+            .getNumericValue(c))
+            .sorted(Comparator.naturalOrder())
+            .collect(Collectors.toList());
+}
+
+    public void practice3 () {
+        String s = "abracadabra";
+        s.chars()
+                .distinct()
+                .mapToObj(c->String.valueOf((char)c))
+                .collect(Collectors.toList());
+    }
+
 
 }
