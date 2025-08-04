1bb364a87c46a8aafb3915a7d8738f2cfab4197f
itdep <morozvitali99@gmail.com>
2025-08-04 10:11:30 +0300

    [📋Stream 01] valueOf [✅1 of 15]

---
+++ src/stream/task0__valueOf_15/Main5.java
@@ -0,0 +1,12 @@
+package stream.task0__valueOf_15;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main5 {
+    public void practice1 () {
+        List<Integer> codes = List.of(97, 98, 99, 100, 101);
+        codes.stream().map(c->String.valueOf((char)(int)c))
+                .collect(Collectors.toList());
+    }
+}
