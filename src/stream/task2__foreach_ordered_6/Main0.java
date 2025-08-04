003d9ce62c05fd746b262dbc0f9cdbbf529d5c2c
itdep <morozvitali99@gmail.com>
2025-08-04 16:18:00 +0300

    [📋Stream 01] foreach [✅5 of 6]

--- src/stream/task2__foreach_ordered_6/Main0.java
+++ src/stream/task2__foreach_ordered_6/Main0.java
@@ -1,6 +1,7 @@
 package stream.task2__foreach_ordered_6;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Main0 {
     public void practice1 () {
@@ -17,5 +18,23 @@ public class Main0 {
         List<String> list = List.of("A", "B", "C", "D");
         list.parallelStream().forEachOrdered(System.out::println);
     }
+
+    public void practice4 () {
+        List<Integer> list = List.of(1, 2, 3, 4);
+        list.parallelStream().peek(System.out::println).map(a->a*a).toList();
+    }
+
+    public void practice5 () {
+        IntStream.rangeClosed(1, 5).parallel().forEachOrdered(i -> {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+            }
+            System.out.println(i + " ");
+        });
+    }
+
+
+
 
 }
