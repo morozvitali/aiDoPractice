4b99ed244c5b3990edaafebcc39fc2ab1fb7d05e
morozvitali <morozvitali99@gmail.com>
2025-08-08 14:06:28 +0300

    Stream task17 [03 of 18]

--- src/stream/task17_collectors_18/Min6.java
+++ src/stream/task17_collectors_18/Min6.java
@@ -2,6 +2,7 @@ package stream.task17_collectors_18;
 
 import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Min6 {
@@ -15,11 +16,10 @@ public class Min6 {
         return numbers.stream().collect(Collectors.summarizingInt(value->value));
     }
 
-    public void practice3 () {
+    public Map<Integer, List <String>> practice3 () {
         List<String> words = List.of("hi", "book",
                 "sun", "day", "Java", "sky");
-
-
+        return words.stream().collect(Collectors.groupingBy(String::length));
     }
 
 
