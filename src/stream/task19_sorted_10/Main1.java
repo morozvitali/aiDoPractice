fb970b9e3f710b8fd4d0726c57017f0500626c89
morozvitali <morozvitali99@gmail.com>
2025-08-11 14:21:37 +0300

    Stream task19 [01 of 10]

--- src/stream/task19_sorted_10/Main1.java
+++ src/stream/task19_sorted_10/Main1.java
@@ -11,6 +11,13 @@ public class Main1 {
                 .sorted(Comparator.comparing(String::length).thenComparing(Comparator.naturalOrder()).thenComparing(Comparator.reverseOrder())).toList();
     }
 
+    public void practice1 () {
+        List<String> words = List.of("tea", "lemon", "fig", "grape", "banana");
+        words.stream().filter(w->w.length() >3).sorted(Comparator.comparing(String::length).thenComparing(Comparator.naturalOrder()));
+    }
+
+
+
 
 
 }
