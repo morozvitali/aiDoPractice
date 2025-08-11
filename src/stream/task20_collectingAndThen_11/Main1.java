1acc661a626133d464c179a0c3528ef870ca7053
morozvitali <morozvitali99@gmail.com>
2025-08-11 15:05:24 +0300

    Stream task120 [02 of 11]

--- src/stream/task20_collectingAndThen_11/Main1.java
+++ src/stream/task20_collectingAndThen_11/Main1.java
@@ -5,17 +5,17 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main1 {
-    public List <String> practice1 () {
+    public List<String> practice1() {
         List<String> data = List.of("apple", "car",
                 "banana", "hi", "cherry");
-        return data.stream().filter(w->w.length()>4).collect(Collectors.collectingAndThen( Collectors.toList(), a-> Collections.unmodifiableList(a)));
+        return data.stream().filter(w -> w.length() > 4).collect(Collectors.collectingAndThen(Collectors.toList(), a -> Collections.unmodifiableList(a)));
     }
 
-    public void practice2 () {
+    public int practice2() {
         List<Integer> data = List.of(3, 9, 2, 4, 6, 7, 12);
-
-
-
+        return data.stream().collect(Collectors.collectingAndThen(Collectors.toList(), List::size));
     }
+
+
 
 }
