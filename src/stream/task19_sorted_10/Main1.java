a8eb5d257c41b42336b74ad879a3132aee11ea47
morozvitali <morozvitali99@gmail.com>
2025-08-11 14:25:12 +0300

    Stream task19 [02 of 10]

--- src/stream/task19_sorted_10/Main1.java
+++ src/stream/task19_sorted_10/Main1.java
@@ -2,6 +2,7 @@ package stream.task19_sorted_10;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main1 {
     public void practice0 () {
@@ -16,7 +17,10 @@ public class Main1 {
         words.stream().filter(w->w.length() >3).sorted(Comparator.comparing(String::length).thenComparing(Comparator.naturalOrder()));
     }
 
-
+    public List <String> practice2 () {
+        List<String> words = List.of("Zebra", "apple", "Lemon", "banana");
+        return words.stream().map(a->a.toLowerCase()).sorted(Comparator.naturalOrder()).collect(Collectors.toList());
+    }
 
 
 
