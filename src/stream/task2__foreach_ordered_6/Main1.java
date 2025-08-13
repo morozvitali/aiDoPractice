29b97864c3f1a159c5c50d8a01b4f6839015a7a7
morozvitali <morozvitali99@gmail.com>
2025-08-13 12:40:10 +0300

    Stream task02 [06 of 06]

--- src/stream/task2__foreach_ordered_6/Main1.java
+++ src/stream/task2__foreach_ordered_6/Main1.java
@@ -1,6 +1,7 @@
 package stream.task2__foreach_ordered_6;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Main1 {
     public void practice1 () {
@@ -25,4 +26,21 @@ public class Main1 {
                 .forEachOrdered(a-> System.out.println("logging " + a));
     }
 
+    public void practice5 () {
+        IntStream.rangeClosed(1,5).parallel()
+                .forEachOrdered(i-> {
+                    try{
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {}
+                    System.out.println(i + " ");
+                });
+    }
+
+    public void practice6 () {
+        List<String> list = List.of("A", "B", "C", "D");
+        StringBuilder sb = new StringBuilder();
+
+        list.parallelStream().forEachOrdered(sb::append);
+        System.out.println(sb);
+    }
 }
