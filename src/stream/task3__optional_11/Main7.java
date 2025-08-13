dcbb21ace9e6fc231a924ed10e4458a8f7a106bb
morozvitali <morozvitali99@gmail.com>
2025-08-13 12:56:23 +0300

    Stream task03 [05 of 11]

--- src/stream/task3__optional_11/Main7.java
+++ src/stream/task3__optional_11/Main7.java
@@ -1,27 +1,40 @@
 package stream.task3__optional_11;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
 public class Main7 {
-    public void practice1 () {
+    public void practice1() {
         List<Integer> numbers = List.of(3, 8, 10, 15, 4);
-        numbers.stream().filter(value ->value%5 ==0).findFirst().orElse(-1);
+        numbers.stream().filter(value -> value % 5 == 0).findFirst().orElse(-1);
     }
 
-    public int practice2 () {
+    public int practice2() {
         Optional<Integer> value = Optional.of(7);
-        return value.map(n->n*n).orElse(-1);
+        return value.map(n -> n * n).orElse(-1);
     }
 
-    public String practice3 () {
-        Optional <String> sentence = Optional.of("strr");
-        return sentence.filter(s->s.length() >5).orElse("");
+    public String practice3() {
+        Optional<String> sentence = Optional.of("strr");
+        return sentence.filter(s -> s.length() > 5).orElse("");
     }
 
-    public String practice4 () {
-        Optional <String> name = Optional.of("Vitalii");
-        return name.map(n->"Hello" + n + "!").orElse("");
+    public String practice4() {
+        Optional<String> name = Optional.of("Vitalii");
+        return name.map(n -> "Hello" + n + "!").orElse("");
+    }
+
+    public void practice5() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+        numbers.stream()
+                .max(Comparator.naturalOrder())
+                .ifPresentOrElse(value -> System.out.println("max " + value),
+                () -> System.out.println("empty"));
+    }
+
+    public void practice6 () {
+
     }
 
 }
