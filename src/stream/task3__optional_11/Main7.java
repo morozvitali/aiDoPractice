e016512ec5b6f85369a460a3313811db09b556db
morozvitali <morozvitali99@gmail.com>
2025-08-13 12:47:55 +0300

    Stream task03 [02 of 11]

--- src/stream/task3__optional_11/Main7.java
+++ src/stream/task3__optional_11/Main7.java
@@ -1,6 +1,7 @@
 package stream.task3__optional_11;
 
 import java.util.List;
+import java.util.Optional;
 
 public class Main7 {
     public void practice1 () {
@@ -8,5 +9,8 @@ public class Main7 {
         numbers.stream().filter(value ->value%5 ==0).findFirst().orElse(-1);
     }
 
-
+    public int practice2 () {
+        Optional<Integer> value = Optional.of(7);
+        return value.map(n->n*n).orElse(-1);
+    }
 }
