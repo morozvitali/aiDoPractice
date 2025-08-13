1b66235a6af2eb7e4aeeffc96751471cae41c58d
morozvitali <morozvitali99@gmail.com>
2025-08-13 09:50:14 +0300

    Stream task01 [02 of 08]

--- src/stream/task1_peek_mapping_chartoint_8/Main6.java
+++ src/stream/task1_peek_mapping_chartoint_8/Main6.java
@@ -1,10 +1,15 @@
 package stream.task1_peek_mapping_chartoint_8;
 
 public class Main6 {
-    public void practice1 (int n) {
-        String.valueOf(n).chars().map(c->c-'0')
+    public int practice1 (int n) {
+        return String.valueOf(n).chars().map(c->c-'0')
                 .map(d->d*d)
                 .sum();
+    }
+
+    public boolean practice2 (int n) {
+        return String.valueOf(n).chars().map(c->c-'0')
+                .allMatch(a->a%2==0);
     }
 
 
