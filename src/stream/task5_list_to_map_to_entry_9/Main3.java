bbc06b79059177625f45b33cbc8a538aeebe06f1
morozvitali <morozvitali99@gmail.com>
2025-08-13 16:27:27 +0300

    Stream task05 [06 of 09]

--- src/stream/task5_list_to_map_to_entry_9/Main3.java
+++ src/stream/task5_list_to_map_to_entry_9/Main3.java
@@ -2,53 +2,59 @@ package stream.task5_list_to_map_to_entry_9;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class Main3 {
-    public String practice1 () {
+    public String practice1() {
         List<String> words = List.of("stream", "code",
                 "developer", "Engineer");
-        return words.stream().map(w-> Map.entry(w, w.length()))
-                .reduce((a,b)->a.getValue() >b.getValue() ? a :b)
+        return words.stream().map(w -> Map.entry(w, w.length()))
+                .reduce((a, b) -> a.getValue() > b.getValue() ? a : b)
                 .map(Map.Entry::getKey).orElse("");
     }
 
 
-    public String practice2 () {
+    public String practice2() {
         List<String> words = List.of("stream", "code",
                 "developer", "Engineer");
-        return words.stream().map(w->Map.entry(w, w.length()))
-                .reduce((a,b)-> a.getValue() < b.getValue() ? a:b)
+        return words.stream().map(w -> Map.entry(w, w.length()))
+                .reduce((a, b) -> a.getValue() < b.getValue() ? a : b)
                 .map(Map.Entry::getKey).orElse("empty");
     }
 
-    public String practice3 () {
+    public String practice3() {
         List<String> list = List.of("Java", "C",
                 "Python", "Go", "Kotlin");
-        return list.stream().map(w-> Map.entry(w, countVowels(w)))
-                .reduce((a,b)-> a.getValue() < b.getValue() ? a : b)
+        return list.stream().map(w -> Map.entry(w, countVowels(w)))
+                .reduce((a, b) -> a.getValue() < b.getValue() ? a : b)
                 .map(Map.Entry::getKey).orElse("empty");
     }
 
-    public long countVowels (String s) {
-        return s.chars().map(c->(char)c).filter(c->"aeiou".indexOf(c) >=0).count();
+    public long countVowels(String s) {
+        return s.chars().map(c -> (char) c).filter(c -> "aeiou".indexOf(c) >= 0).count();
     }
 
-    public String practice4 () {
+    public String practice4() {
         List<String> list = List.of("apple", "pear",
                 "banana", "kiwi");
-        return list.stream().map(w->Map.entry(w, w.chars().distinct().count()))
-                .reduce((a,b)-> a.getValue() > b.getValue() ? a : b)
+        return list.stream().map(w -> Map.entry(w, w.chars().distinct().count()))
+                .reduce((a, b) -> a.getValue() > b.getValue() ? a : b)
                 .map(Map.Entry::getKey).orElse("empty");
     }
 
-    public String practice5 () {
+    public String practice5() {
         List<String> list = List.of("apple",
                 "committee", "banana", "success");
-        return list.stream().map(w->Map.entry(w, w.length() - w.chars().distinct().count()))
-                .reduce((a,b) -> a.getValue() > b.getValue() ? a : b)
+        return list.stream().map(w -> Map.entry(w, w.length() - w.chars().distinct().count()))
+                .reduce((a, b) -> a.getValue() > b.getValue() ? a : b)
                 .map(Map.Entry::getKey).orElse("");
     }
 
+    public void practice6() {
+        Stream.of("abc", "aaa", "zzz").map(w -> Map.entry(w, w.chars().sum()))
+                .reduce((a, b) -> a.getValue() > b.getValue() ? a : b)
+                .map(Map.Entry::getKey).orElse("");
+    }
 
 
 }
