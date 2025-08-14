27eea8bb5bcd37db78f4169c84dfb7e6da24d35d
morozvitali <morozvitali99@gmail.com>
2025-08-14 11:19:06 +0300

    Stream task0 String builder [02 of 20]

--- src/stream/task6_regex_20/Main1.java
+++ src/stream/task6_regex_20/Main1.java
@@ -1,8 +1,12 @@
 package stream.task6_regex_20;
 
 public class Main1 {
-    public void practice1 (String password) {
-        password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()\\-+=])\\S{8,20}$");
+    public boolean practice1 (String password) {
+        return password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()\\-+=])\\S{8,20}$");
+    }
+
+    public boolean practice2 (String email) {
+        return email.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$]");
     }
 
 
