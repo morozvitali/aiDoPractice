347c1aff02519b8419dc9e0600914e8bd43cc0be
morozvitali <morozvitali99@gmail.com>
2025-08-14 15:28:13 +0300

    Stream task8 Binary Stream [3 of 11]

--- src/stream/task8_binary_11/Main2.java
+++ src/stream/task8_binary_11/Main2.java
@@ -1,11 +1,23 @@
 package stream.task8_binary_11;
 
 public class Main2 {
-    public int practice1 (int number) {
+    public long practice1 (int number) {
         return Integer.toBinaryString(number).chars().filter(c->c=='1').count();
     }
 
+    public long practice2 (int number) {
+        return Integer.toBinaryString(number).chars().filter(c->c=='1').count();
+    }
 
-
+    public int practice3 (int number) {
+        int count = 0;
+        while (number != 0) {
+            if ((number & 1) == 1) {
+                count++;
+            }
+            number=number>>1;
+        }
+        return count;
+    }
 
 }
