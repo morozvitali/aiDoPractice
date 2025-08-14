49eeaa73dacf6aff8e7f34aa2d4ce10beedeaeec
morozvitali <morozvitali99@gmail.com>
2025-08-14 15:50:23 +0300

    Stream task8 Binary Stream [6 of 11]

--- src/stream/task8_binary_11/Main2.java
+++ src/stream/task8_binary_11/Main2.java
@@ -1,5 +1,7 @@
 package stream.task8_binary_11;
 
+import java.util.Arrays;
+
 public class Main2 {
     public long practice1 (int number) {
         return Integer.toBinaryString(number).chars().filter(c->c=='1').count();
@@ -24,6 +26,18 @@ public class Main2 {
         return Integer.toBinaryString(Integer.MAX_VALUE).chars().filter(c->c=='1').count();
     }
 
+    public boolean practice5 (int a, int b) {
+        return countBits(a) > countBits(b);
+    }
+
+    public long countBits (int number) {
+        return Integer.toBinaryString(number).chars().filter(c->c == '1').count();
+    }
+
+    public long practice6 () {
+        int[] arr = {1, 2, 3, 4};
+        return Arrays.stream(arr).boxed().mapToLong(value -> Integer.toBinaryString(value).chars().filter(c->c=='1').count()).sum();
+    }
 
 
 
