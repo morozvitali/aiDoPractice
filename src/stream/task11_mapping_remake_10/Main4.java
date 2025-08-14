7aab51033642cd77bc75346250f95e0e160575c5
morozvitali <morozvitali99@gmail.com>
2025-08-14 17:04:26 +0300

    Stream task10 Binary Stream [4 of 10]

--- src/stream/task11_mapping_remake_10/Main4.java
+++ src/stream/task11_mapping_remake_10/Main4.java
@@ -15,4 +15,19 @@ public class Main4 {
     public String practice2 (String sentence) {
         return Arrays.stream(sentence.split(" ")).map(w->w.length() <5 ? w : "[...]").collect(Collectors.joining(" "));
     }
+
+    public String practice3 (String sentence) {
+        String [] words = sentence.split(" ");
+        String temp = words[0];
+        words[0] = words[words.length -1];
+        words[words.length-1] = temp;
+        return String.join(" ", words);
+    }
+
+    public String practice4 (String sentence) {
+        return Arrays.stream(sentence.split(" ")).map(w-> "(" + w + ")")
+                .collect(Collectors.joining(" "));
+    }
+
+
 }
