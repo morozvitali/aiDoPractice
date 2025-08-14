2f0ae4393f96b60db1a342621d1b1f5dd593a5ed
morozvitali <morozvitali99@gmail.com>
2025-08-14 12:51:05 +0300

    Stream task7 Comparators [3 of 8]

--- src/stream/task7_comparator_8/Main2.java
+++ src/stream/task7_comparator_8/Main2.java
@@ -2,6 +2,7 @@ package stream.task7_comparator_8;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -14,4 +15,18 @@ public class Main2 {
         return s.chars().filter(c->"aeiou".indexOf(c) >= 0).count();
     }
 
+    public String practice2 () {
+        return Stream.of("alpha", "arena",
+       "java", "banana", "lava").map(w-> Map.entry(w, countletter(w, 'a'))).max(Comparator.comparing(Map.Entry::getValue))
+                        .map(Map.Entry::getKey)
+                                .orElse("empty");
+    }
+
+    public long countletter (String s, char c) {
+        return s.chars().filter(ch->ch==c).count();
+    }
+
+    public int practice3 () {
+        return Stream.of(111, 123, 444, 1212).map(n->Map.entry(n, String.valueOf(n).chars().distinct().count())).max(Comparator.comparing(Map.Entry::getValue)).map(Map.Entry::getKey).orElse(-1);
+    }
 }
