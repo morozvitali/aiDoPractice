4fd9ec16e28fa4fcd80e958fa663ca30812d954e
morozvitali <morozvitali99@gmail.com>
2025-08-15 14:56:32 +0300

    Stream task17 Binary Stream [1 of 13]

--- src/stream/task18_flatMap_13/Main2.java
+++ src/stream/task18_flatMap_13/Main2.java
@@ -1,5 +1,6 @@
 package stream.task18_flatMap_13;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -13,6 +14,14 @@ public class Main2 {
         ).flatMap(list->list.stream()).filter(a->a>50).collect(Collectors.toList());
     }
 
+public List <String> practice () {
+    List<String> sentences = List.of(
+            "Java is powerful",
+            "Streams are cool",
+            "Java makes sense"
+    );
+    return sentences.stream().flatMap(sent-> Arrays.stream(sent.split(" "))).distinct().collect(Collectors.toList());
+}
 
 
 }
