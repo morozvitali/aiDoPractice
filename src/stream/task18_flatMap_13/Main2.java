dd739b980a347f751471fa80bd1fd0b1c6827354
morozvitali <morozvitali99@gmail.com>
2025-08-15 15:00:17 +0300

    Stream task17 Binary Stream [2 of 13]

--- src/stream/task18_flatMap_13/Main2.java
+++ src/stream/task18_flatMap_13/Main2.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main2 {
-    public void practice1 () {
+    public void practice0 () {
         Stream.of(
                 List.of(40, 55, 60),
                 List.of(30, 70),
@@ -14,13 +14,22 @@ public class Main2 {
         ).flatMap(list->list.stream()).filter(a->a>50).collect(Collectors.toList());
     }
 
-public List <String> practice () {
+public List <String> practice1 () {
     List<String> sentences = List.of(
             "Java is powerful",
             "Streams are cool",
             "Java makes sense"
     );
     return sentences.stream().flatMap(sent-> Arrays.stream(sent.split(" "))).distinct().collect(Collectors.toList());
+}
+
+public int practice2 () {
+    int[][] data = {
+            {3, 11},
+            {6, 7},
+            {10}
+    };
+    return Arrays.stream(data).flatMapToInt(ar->Arrays.stream(ar)).filter(a->a%2==0).min().orElse(-1);
 }
 
 
