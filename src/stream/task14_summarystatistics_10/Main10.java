000f49902899d5089728b8f2aa94fee1aa057c4e
morozvitali <morozvitali99@gmail.com>
2025-08-15 11:19:26 +0300

    Stream task14 Binary Stream [2 of 10]

--- src/stream/task14_summarystatistics_10/Main10.java
+++ src/stream/task14_summarystatistics_10/Main10.java
@@ -2,13 +2,20 @@ package stream.task14_summarystatistics_10;
 
 import java.util.Arrays;
 import java.util.IntSummaryStatistics;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main10 {
-    public long practice1 () {
+    public long practice1() {
         int[] numbers = {2, 4, 2, 6, 8, 4, 10, 10, 12};
-        IntSummaryStatistics stats = Arrays.stream(numbers).filter(a->a%2==0).distinct().summaryStatistics();
+        IntSummaryStatistics stats = Arrays.stream(numbers).filter(a -> a % 2 == 0).distinct().summaryStatistics();
         return stats.getCount();
+    }
+
+    public int practice2() {
+        List<String> words = List.of("APPLE",
+                "Banana", "CHERRY", "kiwi", "PLUM");
+        return words.stream().filter(s -> s.length() % 2 == 0).map(String::toLowerCase).mapToInt(String::length).sum();
     }
 
 
