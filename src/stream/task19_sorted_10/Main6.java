24d89cfd59234d1f12af1e78c87d49af18f9d949
morozvitali <morozvitali99@gmail.com>
2025-08-18 15:38:20 +0300

    Stream task19 sorted [3 of 10]

--- src/stream/task19_sorted_10/Main6.java
+++ src/stream/task19_sorted_10/Main6.java
@@ -3,6 +3,7 @@ package stream.task19_sorted_10;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main6 {
     public void practice1 () {
@@ -13,6 +14,19 @@ public class Main6 {
                 .collect(Collectors.toList());
     }
 
+    public void practice2 () {
+        Stream.of("apple", "banana", "lemon", "zebra")
+                .map(String::toLowerCase)
+                .sorted()
+                .collect(Collectors.toList());
+    }
 
+    public List <Integer> practice3 () {
+        List<Integer> numbers = List.of(3, 7, 1, 9, 2);
+        return numbers.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+    }
 
+    public void practice4 () {
+
+    }
 }
