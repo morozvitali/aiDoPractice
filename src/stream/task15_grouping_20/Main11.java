00c28a090eeda2ae684e863a22ea5025ba311d3a
morozvitali <morozvitali99@gmail.com>
2025-08-18 13:26:49 +0300

    Stream task15 Grouping [2 of 20]

--- src/stream/task15_grouping_20/Main11.java
+++ src/stream/task15_grouping_20/Main11.java
@@ -1,8 +1,11 @@
 package stream.task15_grouping_20;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Main11 {
@@ -19,6 +22,12 @@ public class Main11 {
     public Long practice1 (String s) {
         return s.toLowerCase().chars().mapToObj(c->(char)c).collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream().filter(e->e.getValue() >1).count();
     }
+
+    public String practice2 (String s) {
+        Map <String, Long> map = Arrays.stream(s.split(" ")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        return Collections.max(map.entrySet(), Comparator.comparingLong(entry ->entry.getValue())).getKey();
+    }
+
 
 
 }
