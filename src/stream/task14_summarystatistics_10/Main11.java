1dc68a6c0ad3835d63ad997d9435570774670cf2
morozvitali <morozvitali99@gmail.com>
2025-08-18 12:23:42 +0300

    Stream task14 Binary Stream [5 of 10]

--- src/stream/task14_summarystatistics_10/Main11.java
+++ src/stream/task14_summarystatistics_10/Main11.java
@@ -1,6 +1,7 @@
 package stream.task14_summarystatistics_10;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -39,6 +40,12 @@ public class Main11 {
                 .mapToInt(String::length)
                 .summaryStatistics()
                 .getAverage();
+    }
+
+    public String practice5 () {
+        String[] words = {"apple", "orange", "banana",
+                "ice", "umbrella", "echo"};
+        return Arrays.stream(words).filter(w->"aeiou".indexOf(w.charAt(0))>=0).min(Comparator.comparingInt(String::length)).orElse("");
     }
 
 
