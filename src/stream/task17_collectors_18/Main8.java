b282c19de979881b127190f64cc18a0720e0e3b4
morozvitali <morozvitali99@gmail.com>
2025-08-18 14:15:46 +0300

    Stream task17 collectors [03 of 18]

--- src/stream/task17_collectors_18/Main8.java
+++ src/stream/task17_collectors_18/Main8.java
@@ -1,12 +1,25 @@
 package stream.task17_collectors_18;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main8 {
     public Double practice1 () {
         List<Integer> numbers = List.of(4, 8, 15, 16, 23, 42);
         return numbers.stream().mapToInt(a->(int)a)
                 .summaryStatistics().getAverage();
+    }
+
+    public Long practice2 () {
+        List<Integer> numbers = List.of(4, 8, 15, 16, 23, 42);
+        return numbers.stream().mapToInt(a->a).summaryStatistics().getCount();
+    }
+
+    public Map<Integer, List <String>> practice3 () {
+        List<String> words = List.of("hi", "book",
+                "sun", "day", "Java", "sky");
+        return words.stream().collect(Collectors.groupingBy(String::length));
     }
 
 
