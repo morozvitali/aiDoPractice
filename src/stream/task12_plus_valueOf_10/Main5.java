f98adf26fae06721cf762878fc210825c13da359
morozvitali <morozvitali99@gmail.com>
2025-08-18 11:19:42 +0300

    Stream task12 Binary Stream [2 of 10]

--- src/stream/task12_plus_valueOf_10/Main5.java
+++ src/stream/task12_plus_valueOf_10/Main5.java
@@ -3,10 +3,16 @@ package stream.task12_plus_valueOf_10;
 import java.util.stream.Collectors;
 
 public class Main5 {
-    public String practice1 (int n) {
-        return String.valueOf(n).chars().map(a->Character.getNumericValue(a))
-                .map(a->a*a).mapToObj(String::valueOf).collect(Collectors.joining());
+    public int practice1 (int n) {
+        return Integer.valueOf(String.valueOf(n).chars().map(a->Character.getNumericValue(a))
+                .map(a->a*a).mapToObj(String::valueOf).collect(Collectors.joining()));
     }
 
+    public Integer practice2 (int n) {
+        return Integer.valueOf(String.valueOf(String.valueOf(n)).chars().map(Character::getNumericValue)
+                .map(a->a*a*a)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining()));
+    }
 
 }
