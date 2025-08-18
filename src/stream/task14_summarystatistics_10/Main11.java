83e01df87fa717a571f773870e5663aa1b8b8bff
morozvitali <morozvitali99@gmail.com>
2025-08-18 12:12:36 +0300

    Stream task14 Binary Stream [3 of 10]

--- src/stream/task14_summarystatistics_10/Main11.java
+++ src/stream/task14_summarystatistics_10/Main11.java
@@ -2,6 +2,9 @@ package stream.task14_summarystatistics_10;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main11 {
@@ -19,6 +22,14 @@ public class Main11 {
         return words.stream().map(a->a.toLowerCase()).mapToInt(String::length).summaryStatistics().getCount();
     }
 
+    public Map <String, Long> practice3 () {
+        String[] words = {"sky", "apple",
+                "moon", "dry", "banana"};
+        return Arrays.stream(words).filter(a->a.length() > 3 && countVowels(a) >1).collect(Collectors.toMap(Function.identity(), w->countVowels(w)));
+    }
+    public long countVowels (String s) {
+        return s.chars().filter(c->"aeiou".indexOf(c) >=0).count();
+    }
 
 
 }
