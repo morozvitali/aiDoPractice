3ce4aafd04d8db71e40e4af9215c42c085ecf7b9
morozvitali <morozvitali99@gmail.com>
2025-08-18 12:51:14 +0300

    Stream task15 Grouping [0 of 20]

---
+++ src/stream/task15_grouping_20/Main11.java
@@ -0,0 +1,20 @@
+package stream.task15_grouping_20;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Main11 {
+    public Map<String, Boolean> practice0 () {
+        String[] words = {"sky", "apple",
+                "dry", "orange", "sun"};
+        return Arrays.stream(words)
+                .collect(Collectors.toMap(Function.identity(), w->isHave(w)));
+    }
+    public boolean isHave(String s) {
+        return s.chars().filter(c->"aeiou".indexOf(c)>=0).count() > 0;
+    }
+
+
+}
