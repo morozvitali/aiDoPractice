27d2963365e37ca67d4e4cbceb18a9153bf8a30c
morozvitali <morozvitali99@gmail.com>
2025-08-18 14:44:15 +0300

    Stream task17 flatMap [1 of 13]

---
+++ src/stream/task18_flatMap_13/Main6.java
@@ -0,0 +1,17 @@
+package stream.task18_flatMap_13;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main6 {
+    public List <Integer> practice1 () {
+        List<List<Integer>> weeklyData = List.of(
+                List.of(40, 55, 60),
+                List.of(30, 70),
+                List.of(65, 10, 90)
+        );
+        return weeklyData.stream().flatMap(list->list.stream()).filter(a->a>50).collect(Collectors.toList());
+    }
+
+
+}
