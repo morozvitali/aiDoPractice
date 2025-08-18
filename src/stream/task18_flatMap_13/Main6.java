3f3ffa3390ff2b19057e47bf3cd8583951aee672
morozvitali <morozvitali99@gmail.com>
2025-08-18 14:52:22 +0300

    Stream task17 flatMap [2 of 13]

--- src/stream/task18_flatMap_13/Main6.java
+++ src/stream/task18_flatMap_13/Main6.java
@@ -1,11 +1,12 @@
 package stream.task18_flatMap_13;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main6 {
-    public List <Integer> practice1 () {
+    public List <Integer> practice0 () {
         List<List<Integer>> weeklyData = List.of(
                 List.of(40, 55, 60),
                 List.of(30, 70),
@@ -14,13 +15,22 @@ public class Main6 {
         return weeklyData.stream().flatMap(list->list.stream()).filter(a->a>50).collect(Collectors.toList());
     }
 
-    public List <String> practice2 () {
+    public List <String> practice1 () {
         List<String> sentences = List.of(
                 "Java is powerful",
                 "Streams are cool",
                 "Java makes sense"
         );
         return sentences.stream().flatMap(s -> Arrays.stream(s.split(" "))).distinct().map(String::toLowerCase).collect(Collectors.toList());
+    }
+
+    public int practice2 () {
+        int[][] data = {
+                {3, 11},
+                {6, 7},
+                {10}
+        };
+        return Arrays.stream(data).flatMapToInt(arr->Arrays.stream(arr)).boxed().min(Comparator.naturalOrder()).orElse(-1);
     }
 
 
