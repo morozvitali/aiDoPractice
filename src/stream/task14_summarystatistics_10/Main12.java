e9d61f9bdaae09ea467912b4cb4beadefa5603b7
morozvitali <morozvitali99@gmail.com>
2025-08-19 13:31:03 +0300

    Stream task14 summary [3 of 10]

--- src/stream/task14_summarystatistics_10/Main12.java
+++ src/stream/task14_summarystatistics_10/Main12.java
@@ -3,6 +3,8 @@ package stream.task14_summarystatistics_10;
 import java.util.Arrays;
 import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Main12 {
     public IntSummaryStatistics practice1() {
@@ -15,9 +17,22 @@ public class Main12 {
         List<String> words = List.of("APPLE",
                 "Banana", "CHERRY", "kiwi", "PLUM");
         return words.stream().filter(w -> w.length() % 2 == 0)
-                .map(w -> w.toLowerCase())
+                .map(String::toLowerCase)
                 .mapToInt(String::length)
                 .sum();
+    }
+
+    public void practice3 () {
+        String[] words = {"sky", "apple",
+                "moon", "dry", "banana"};
+        Arrays.stream(words).filter(w->w.length()>3 && countVowels(w) > 1).collect(Collectors.toMap(Function.identity(), w->countVowels(w)));
+    }
+    public long countVowels (String s) {
+        return s.chars().filter(c->"aeiou".indexOf(c) >= 0).count();
+    }
+
+    public void practice4 () {
+
     }
 
 }
