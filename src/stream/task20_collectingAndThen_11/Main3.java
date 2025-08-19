99430a3da9ed6ea3b22c61c7b06d828a9d135cf6
morozvitali <morozvitali99@gmail.com>
2025-08-19 10:38:44 +0300

    Stream task20 sorted [2 of 11]

--- src/stream/task20_collectingAndThen_11/Main3.java
+++ src/stream/task20_collectingAndThen_11/Main3.java
@@ -8,9 +8,17 @@ import java.util.stream.Stream;
 
 public class Main3 {
     public List<String> practice1 () {
-        return Stream.of("apple", "car", "banana", "hi", "cherry").filter(value -> value.length() > 4).collect(Collectors.collectingAndThen(Collectors.toList(), list -> Collections.unmodifiableList(list)));
+        return Stream.of("apple", "car", "banana", "hi", "cherry")
+                .filter(value -> value.length() > 4)
+                .collect(Collectors.collectingAndThen(Collectors.toList(),
+                        list -> Collections.unmodifiableList(list)));
     }
 
+    public int practice () {
+        return Stream.of(3, 9, 2, 4, 6, 7, 12).filter(n->n%3==0)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), List::size));
+
+    }
 
 
 }
