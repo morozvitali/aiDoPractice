58deda166a3efde6af20508d97ea855552af1c87
morozvitali <morozvitali99@gmail.com>
2025-08-19 15:13:37 +0300

    Stream task18 collectors [3 of 18]

--- src/stream/task17_collectors_18/Main9.java
+++ src/stream/task17_collectors_18/Main9.java
@@ -1,14 +1,24 @@
 package stream.task17_collectors_18;
 
+import java.util.Comparator;
 import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main9 {
-    public IntSummaryStatistics practice2 () {
+    public IntSummaryStatistics practice2() {
         List<Integer> numbers = List.of(4, 8, 15, 16, 23, 42);
-        return numbers.stream().mapToInt(i->(int)i).summaryStatistics();
+        return numbers.stream().mapToInt(i -> (int) i).summaryStatistics();
     }
 
+    public Map<Integer, List<String>> practice3() {
+        List<String> words = List.of("hi", "book",
+                "sun", "day", "Java", "sky");
+        return words.stream().collect(Collectors.groupingBy(String::length));
+    }
 
+    public void practice4 () {
 
+    }
 }
