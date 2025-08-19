8c06fb6f44d544fcd73091a8872bda97760d8817
morozvitali <morozvitali99@gmail.com>
2025-08-19 14:41:31 +0300

    Stream task15 summary [3 of 20]

--- src/stream/task15_grouping_20/Main12.java
+++ src/stream/task15_grouping_20/Main12.java
@@ -6,17 +6,23 @@ import java.util.Comparator;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main12 {
-    public void practice0 () {
+    public void practice1 () {
         String[] words = {"sky", "apple",
                 "dry", "orange", "sun"};
         Arrays.stream(words).collect(Collectors.toMap(Function.identity(), w->w.chars().anyMatch(c->"aeiou".indexOf(c)>=0)));
     }
 
-    public String practice1 () {
+    public String practice2 () {
         Map<String, Long> map =  Arrays.stream("java is fun and java is powerful".split(" "))
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         return Collections.max(map.entrySet(), Comparator.comparingLong(entry -> entry.getValue())).getKey();
     }
+
+    public Map <Integer, Integer> practice3 () {
+        return Stream.of(12, 23, 34, 45, 16, 7).collect(Collectors.groupingBy(w -> w % 10, Collectors.summingInt(w -> w)));
+    }
+
 }
