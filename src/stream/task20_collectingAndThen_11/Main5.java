44c1b309ca480057f2ceec99a64c712d0396d888
morozvitali <morozvitali99@gmail.com>
2025-08-20 11:03:07 +0300

    Stream task19 collectAndThen [9 of 11]

--- src/stream/task20_collectingAndThen_11/Main5.java
+++ src/stream/task20_collectingAndThen_11/Main5.java
@@ -56,4 +56,11 @@ public class Main5 {
         return list.get(0);
         }));
     }
+
+    public String practice9 () {
+        return Stream.of(1, 2, 3, 4, 5)
+                .filter(a->a%2 ==0)
+                .map(String::valueOf)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), list -> String.join(", ", list)));
+    }
 }
