4cb8bab8a8f727d5fde9eacd7d1d979325d17a17
morozvitali <morozvitali99@gmail.com>
2025-08-20 10:40:19 +0300

    Stream task19 collectAndThen [3 of 11]

--- src/stream/task20_collectingAndThen_11/Main5.java
+++ src/stream/task20_collectingAndThen_11/Main5.java
@@ -1,25 +1,30 @@
 package stream.task20_collectingAndThen_11;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main5 {
-    public List<String> practice1 () {
+    public List<String> practice1() {
         return Stream.of("apple", "car", "banana", "hi", "cherry")
-                .filter(w->w.length() > 4)
+                .filter(w -> w.length() > 4)
                 .collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
     }
 
-public Integer practice2 () {
+    public Integer practice2() {
         return Stream.of(3, 9, 2, 4, 6, 7, 12)
-                .filter(a->a%3==0)
+                .filter(a -> a % 3 == 0)
                 .collect(Collectors.collectingAndThen(Collectors.toList(), List::size));
-}
+    }
 
-public void practice3 () {
+    public String practice3() {
+        return Stream.of("sun", "supernova", "star")
+                .sorted(Comparator.comparing(String::length).reversed()).collect(Collectors.collectingAndThen(Collectors.toList(), List::getFirst));
+    }
 
-}
+    public void practice4() {
 
+    }
 }
