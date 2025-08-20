8b5d67bf5d8ceffb2f69294d987ece7bf9529939
morozvitali <morozvitali99@gmail.com>
2025-08-20 11:43:21 +0300

    Stream task12 valueof [6 of 10]

--- src/stream/task12_plus_valueOf_10/Main7.java
+++ src/stream/task12_plus_valueOf_10/Main7.java
@@ -36,5 +36,13 @@ public class Main7 {
         return Integer.parseInt(new StringBuilder(String.valueOf(n)).reverse().toString());
     }
 
+    public int practice6 (int n) {
+        return Integer.parseInt(String.valueOf(n).chars()
+                .map(Character::getNumericValue)
+                .map(a-> a%2==0 ? a*a : a)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining()));
+    }
+
 
 }
