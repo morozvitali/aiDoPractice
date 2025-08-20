b26351b482c4bd448e830e1bbc66a99e8019f87d
morozvitali <morozvitali99@gmail.com>
2025-08-20 13:43:58 +0300

    Stream task15 summary [2 of 20]

--- src/stream/task15_grouping_20/Main1.java
+++ src/stream/task15_grouping_20/Main1.java
@@ -1,6 +1,7 @@
 package stream.task15_grouping_20;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -14,6 +15,12 @@ public class Main1 {
 
     public boolean isHaveVowels (String s) {
         return s.chars().filter(c -> "aeiou".indexOf(c) >= 0).findAny().isPresent();
+    }
+
+    public String practice2 () {
+        return Arrays.stream("java is fun and java is powerful".split(" "))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet()
+                .stream().max(Comparator.comparing(Map.Entry::getValue)).get().getKey();
     }
 
 
