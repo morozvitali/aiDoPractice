942cabc2d2d72e0d479a9de4ea06c29b3d5f141c
morozvitali <morozvitali99@gmail.com>
2025-08-21 12:22:56 +0300

    Stream task20 sorted [1 of 11]

--- src/stream/task19_sorted_10/Main7.java
+++ src/stream/task19_sorted_10/Main7.java
@@ -61,6 +61,5 @@ public class Main7 {
     public List <Integer> practice9 () {
         List<Integer> numbers = List.of(3, 2, 5, 6, 4, 1);
         return numbers.stream().sorted(Comparator.comparingInt((Integer value) -> value%2).thenComparing(Comparator.reverseOrder())).toList();
-
     }
 }
