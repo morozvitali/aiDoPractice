fb65e2c0106f8d654aeca96f39d4492d9fc3ebd4
morozvitali <morozvitali99@gmail.com>
2025-08-21 14:38:55 +0300

    Stream task14 sorted [1 of 10]

---
+++ src/stream/task14_summarystatistics_10/Main14.java
@@ -0,0 +1,13 @@
+package stream.task14_summarystatistics_10;
+
+import java.util.Arrays;
+
+public class Main14 {
+
+    public Long practice1 () {
+        int[] numbers = {2, 4, 2, 6, 8, 4, 10, 10, 12};
+        return Arrays.stream(numbers).distinct().summaryStatistics().getCount();
+    }
+
+
+}
