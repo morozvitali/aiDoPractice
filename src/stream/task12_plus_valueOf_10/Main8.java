faf914d42265bf0812c790cecea88d071cef2cd7
morozvitali <morozvitali99@gmail.com>
2025-08-21 14:25:29 +0300

    Stream task12 sorted [6 of 10]

--- src/stream/task12_plus_valueOf_10/Main8.java
+++ src/stream/task12_plus_valueOf_10/Main8.java
@@ -34,6 +34,11 @@ public class Main8 {
         return Integer.parseInt(new StringBuilder(String.valueOf(Math.abs(n))).reverse().toString());
     }
 
+    public int practice6 (int n) {
+        return Integer.parseInt(String.valueOf(Math.abs(n)).chars().map(Character::getNumericValue)
+                .map(a->a%2==1 ? a : a*a).mapToObj(String::valueOf).collect(Collectors.joining()));
+    }
+
 
 
 }
