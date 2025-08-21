d33b111cc15bd3527231e5a2d842da98128de826
morozvitali <morozvitali99@gmail.com>
2025-08-21 14:30:41 +0300

    Stream task12 sorted [8 of 10]

--- src/stream/task12_plus_valueOf_10/Main8.java
+++ src/stream/task12_plus_valueOf_10/Main8.java
@@ -1,6 +1,7 @@
 package stream.task12_plus_valueOf_10;
 
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main8 {
     public int practice1 (int n) {
@@ -41,6 +42,16 @@ public class Main8 {
 
     public int practice7 (int n) {
         return String.valueOf(Math.abs(n)).length();
+    }
+
+    public String practice8 (int n) {
+        String s = String.valueOf(Math.abs(n));
+        return IntStream.range(0,s.length())
+                .mapToObj(i-> {
+                    int digit = Character.getNumericValue(s.charAt(i));
+                    return String.valueOf(digit * (i+1));
+                        }).collect(Collectors.joining());
+
     }
 
 
