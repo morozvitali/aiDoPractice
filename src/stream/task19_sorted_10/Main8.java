ed5f9abf122095274a4d6d2e3181f5867ab8f960
morozvitali <morozvitali99@gmail.com>
2025-08-22 13:01:47 +0300

    Stream task19 [5 of 10]

--- src/stream/task19_sorted_10/Main8.java
+++ src/stream/task19_sorted_10/Main8.java
@@ -25,4 +25,11 @@ public class Main8 {
         List<Integer> numbers = List.of(3, 7, 1, 9, 2);
         return numbers.stream().sorted(Comparator.reverseOrder()).toList();
     }
+
+    public List <Integer> practice5 () {
+        List<Integer> nums = List.of(3, 2, 5, 6, 1, 4);
+        return nums.stream().sorted(Comparator.comparingInt(value->(int)value%2).thenComparingInt(n->(int)n)).toList();
+    }
+
+
 }
