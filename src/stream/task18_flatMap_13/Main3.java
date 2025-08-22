9931c42f2b6da9aca48df70b4ba2004900872698
morozvitali <morozvitali99@gmail.com>
2025-08-22 11:52:06 +0300

    Stream task18 [3 of 13]

--- src/stream/task18_flatMap_13/Main3.java
+++ src/stream/task18_flatMap_13/Main3.java
@@ -4,29 +4,38 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main3 {
-    public List <String> practice1 () {
+    public List<String> practice1() {
         List<String> sentences = List.of(
                 "Java is powerful",
                 "Streams are cool",
                 "Java makes sense"
         );
-        return sentences.stream().flatMap(s-> Arrays.stream(s.split(" ")))
+        return sentences.stream().flatMap(s -> Arrays.stream(s.split(" ")))
                 .distinct().toList();
     }
 
-    public int practice2 () {
+    public int practice2() {
         int[][] data = {
                 {3, 11},
                 {6, 7},
                 {10}
         };
         return Arrays.stream(data).flatMapToInt(Arrays::stream)
-                .filter(a->a%2 ==0)
+                .filter(a -> a % 2 == 0)
                 .min()
                 .orElse(-1);
     }
 
-    public void practice3 () {
+    public int practice3() {
+        int[][] data = {
+                {-3, 5},
+                {2},
+                {-1, 4}
+        };
 
+        return Arrays.stream(data).flatMapToInt(Arrays::stream)
+                .filter(a -> a > 0)
+                .reduce((a, b) -> a * b)
+                .orElse(-1);
     }
 }
