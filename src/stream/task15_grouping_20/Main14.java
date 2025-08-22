847d09cda7ba7af61102468bf9aaebe3b74ba160
morozvitali <morozvitali99@gmail.com>
2025-08-22 17:34:57 +0300

    Stream task14 [12 of 20]

--- src/stream/task15_grouping_20/Main14.java
+++ src/stream/task15_grouping_20/Main14.java
@@ -1,9 +1,6 @@
 package stream.task15_grouping_20;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -61,5 +58,11 @@ public class Main14 {
         return Arrays.stream(words).collect(Collectors.groupingBy(w->w.charAt(0), Collectors.counting())).entrySet().stream().max(Comparator.comparing(entry -> entry.getValue())).get().getKey();
     }
 
-
+    public void practice12 () {
+        Map <Character, Long> map = new HashMap<>();
+        map.put('a', 2L);
+        map.put('b', 5L);
+        map.put('c', 1L);
+        map.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+    }
 }
