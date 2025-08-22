33bb6bc393422eb4eda92e2e3df879e64544752e
morozvitali <morozvitali99@gmail.com>
2025-08-22 17:18:26 +0300

    Stream task14 [2 of 20]

--- src/stream/task15_grouping_20/Main14.java
+++ src/stream/task15_grouping_20/Main14.java
@@ -1,10 +1,11 @@
 package stream.task15_grouping_20;
 
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main14 {
     public Map<String, Boolean> practice0 () {
@@ -21,6 +22,13 @@ public class Main14 {
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
 
+    public String practice2 () {
+        return Arrays.stream("java is fun and java is powerful".split(" "))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream().max(Comparator.comparing( entry->entry.getValue())).get().getKey();
+    }
 
-
+    public Map <Integer, Integer> practice3 () {
+        return Stream.of(12, 23, 34, 45, 16, 7).collect(Collectors.groupingBy(v->v%10, Collectors.summingInt(n->n)));
+    }
 }
