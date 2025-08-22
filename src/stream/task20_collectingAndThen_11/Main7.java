a67ca59c52f591e284fc1af8035d2a13caa1c8da
morozvitali <morozvitali99@gmail.com>
2025-08-22 14:53:39 +0300

    Stream task20 [5 of 11]

--- src/stream/task20_collectingAndThen_11/Main7.java
+++ src/stream/task20_collectingAndThen_11/Main7.java
@@ -3,32 +3,35 @@ package stream.task20_collectingAndThen_11;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main7 {
 
-    public List<String> practice1 () {
+    public List<String> practice1() {
         return Stream.of("apple", "car", "banana", "hi", "cherry")
-                .filter(w->w.length() >4)
+                .filter(w -> w.length() > 4)
                 .collect(Collectors.collectingAndThen(Collectors.toList(),
                         Collections::unmodifiableList));
     }
 
-    public Integer practice2 () {
+    public Integer practice2() {
         return Stream.of(3, 9, 2, 4, 6, 7, 12).collect(Collectors.collectingAndThen(Collectors.toList(), List::size));
     }
 
-    public String practice3 () {
+    public String practice3() {
         return Stream.of("sun", "supernova", "star").sorted(Comparator.comparing(String::length).reversed())
                 .collect(Collectors.collectingAndThen(Collectors.toList(), List::getFirst));
     }
 
-    public Integer practice4 () {
+    public Integer practice4() {
         return Stream.of(5, 3, 4, 4, 3, 6).distinct()
                 .collect(Collectors.collectingAndThen(Collectors.toList(), List::getFirst));
     }
 
-
+    public Set<Integer> practice5() {
+        return Stream.of(2, 4, 10, 12, 14, 12, 4).collect(Collectors.collectingAndThen(Collectors.toSet(), Collections::unmodifiableSet));
+    }
 
 }
