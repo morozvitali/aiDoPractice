8e403146b301495e0f1a4cd4195477da72c38d10
morozvitali <morozvitali99@gmail.com>
2025-08-22 16:22:11 +0300

    Stream task14 [4 of 10]

--- src/stream/task14_summarystatistics_10/Main15.java
+++ src/stream/task14_summarystatistics_10/Main15.java
@@ -2,6 +2,8 @@ package stream.task14_summarystatistics_10;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main15 {
     public long practice1 () {
@@ -22,7 +24,26 @@ public class Main15 {
                 .sum();
     }
 
-    public void practice3 () {
+    public Map<String, Long> practice3 () {
+        String[] words = {"sky", "apple",
+                "moon", "dry", "banana"};
 
+        return Arrays.stream(words).filter(w->w.length()>3 && countVowels(w)>1)
+                .collect(Collectors.toMap(a->a, Main15::countVowels));
     }
+
+    public static long countVowels (String s) {
+        return s.chars().filter(c->"aeiou".indexOf(c)>=0).count();
+    }
+
+    public Double practice4 () {
+        String[] words = {"sky", "apple", "moon",
+                "dry", "sun"};
+        return Arrays.stream(words).filter(w->countVowels(w) >0)
+                .mapToInt(String::length)
+                .average()
+                .orElse(-1);
+    }
+
+
 }
