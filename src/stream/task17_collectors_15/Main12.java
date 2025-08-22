a7b1ae7563412ce8628c563b72e20619cc487560
morozvitali <morozvitali99@gmail.com>
2025-08-22 17:50:44 +0300

    Stream task17 [4 of 15]

--- src/stream/task17_collectors_15/Main12.java
+++ src/stream/task17_collectors_15/Main12.java
@@ -29,4 +29,9 @@ public class Main12 {
                 "book", "sun", "day", "Java", "sky");
         return words.stream().collect(Collectors.groupingBy(String::length, Collectors.toSet()));
     }
+
+    public String practice5 () {
+        List<Integer> numbers = List.of(4, 8, 15, 16, 23, 42);
+        return numbers.stream().collect(Collectors.collectingAndThen(Collectors.counting(), count -> "Count " + count));
+    }
 }
