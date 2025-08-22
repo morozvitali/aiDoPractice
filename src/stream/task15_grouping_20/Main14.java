69c95c4751c86466493046b14c1235b9170fef4d
morozvitali <morozvitali99@gmail.com>
2025-08-22 17:08:40 +0300

    Stream task14 [2 of 20]

--- src/stream/task15_grouping_20/Main14.java
+++ src/stream/task15_grouping_20/Main14.java
@@ -1,12 +1,13 @@
 package stream.task15_grouping_20;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main14 {
-    public Map<String, Boolean> practice1 () {
+    public Map<String, Boolean> practice0 () {
         String[] words = {"sky", "apple",
                 "dry", "orange", "sun"};
     return Arrays.stream(words).collect(Collectors.toMap(Function.identity(), Main14::isHaveVOvel));
@@ -14,6 +15,12 @@ public class Main14 {
     public static Boolean isHaveVOvel (String s) {
         return s.chars().anyMatch(c->"aeiou".indexOf(c) >=0);
     }
+
+    public Map<Character, Long> practice1 (String s) {
+        return s.toLowerCase().chars().mapToObj(c->(char)c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
 
 
 }
