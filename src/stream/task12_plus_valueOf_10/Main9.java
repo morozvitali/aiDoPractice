dae0abef950aeb17c43d5de5697d552d0caaacad
morozvitali <morozvitali99@gmail.com>
2025-08-22 15:23:02 +0300

    Stream task12 [1 of 10]

---
+++ src/stream/task12_plus_valueOf_10/Main9.java
@@ -0,0 +1,16 @@
+package stream.task12_plus_valueOf_10;
+
+import java.util.stream.Collectors;
+
+public class Main9 {
+    public int practice1 (int n) {
+        return Integer.parseInt(String.valueOf(n)
+                .chars()
+                .map(Character::getNumericValue)
+                .map(a->a*a)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining()));
+    }
+
+
+}
