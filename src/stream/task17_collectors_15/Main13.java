6bb5147633b9353664b0eead61841468befc7dbe
morozvitali <morozvitali99@gmail.com>
2025-08-25 14:09:08 +0300

    Stream task17 [5 of 17]

--- src/stream/task17_collectors_15/Main13.java
+++ src/stream/task17_collectors_15/Main13.java
@@ -2,25 +2,40 @@ package stream.task17_collectors_15;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Main13 {
-    public Map <Character, List<String>> practice1 () {
+    public Map<Character, List<String>> practice1() {
         List<String> words = List.of("apple",
                 "ant", "banana", "bat", "car");
-        return words.stream().collect(Collectors.groupingBy(word->word.charAt(0),
+        return words.stream().collect(Collectors.groupingBy(word -> word.charAt(0),
                 Collectors.mapping(String::toUpperCase, Collectors.toList())));
     }
 
-    public Map <Character, Integer> practice2 () {
+    public Map<Character, Integer> practice2() {
         List<String> words = List.of("apple", "ant",
                 "banana", "bat", "car");
-        return words.stream().collect(Collectors.toMap(w->w.charAt(0), String::length));
+        return words.stream().collect(Collectors.toMap(w -> w.charAt(0), String::length));
     }
 
-    public Map<Boolean, Long> practice3 () {
+    public Map<Boolean, Long> practice3() {
         List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
-        return numbers.stream().collect(Collectors.partitioningBy(a->a%2==0, Collectors.counting()));
+        return numbers.stream().collect(Collectors.partitioningBy(a -> a % 2 == 0, Collectors.counting()));
     }
 
+    public Map<Integer, Set<String>> practice4() {
+        List<String> words = List.of("hi", "hi",
+                "book", "sun", "day", "Java", "sky");
+        return words.stream().collect(Collectors.groupingBy(String::length, Collectors.toSet()));
+    }
+
+    public void practice5() {
+        List<Integer> numbers = List.of(4, 8, 15, 16, 23, 42);
+        numbers.stream().collect(Collectors.collectingAndThen(Collectors.counting(), count -> "Count " + count));
+    }
+
+    public void practice6 () {
+
+    }
 }
