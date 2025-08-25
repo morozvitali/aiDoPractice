fa77a2a3faaeaa19abbb06e7122803b9bf3fc711
morozvitali <morozvitali99@gmail.com>
2025-08-25 14:34:27 +0300

    Stream task17 [10 of 17]

--- src/stream/task17_collectors_15/Main13.java
+++ src/stream/task17_collectors_15/Main13.java
@@ -1,9 +1,6 @@
 package stream.task17_collectors_15;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main13 {
@@ -60,4 +57,9 @@ public class Main13 {
         return words.stream().collect(Collectors.groupingBy(w->w.charAt(0), Collectors.collectingAndThen(Collectors.toList(), list -> list.stream().sorted(Comparator.comparingInt(String::length)).toList())));
     }
 
+    public Map <Integer, List <String>> practice10 () {
+        List<String> words = List.of("hi", "book",
+                "sun", "day", "Java", "sky");
+        return words.stream().collect(Collectors.groupingBy(String::length, TreeMap::new, Collectors.toList()));
+    }
 }
