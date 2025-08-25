4da1b458eb0b0728f188276177c1472e961ed128
morozvitali <morozvitali99@gmail.com>
2025-08-25 09:32:37 +0300

    Stream task14 [2 of 10]

--- src/stream/task14_summarystatistics_10/Main16.java
+++ src/stream/task14_summarystatistics_10/Main16.java
@@ -7,4 +7,15 @@ public class Main16 {
     public long practice1 () {
 return Stream.of(2, 4, 2, 6, 8, 4, 10, 10, 12).filter(a->a%2==0).distinct().mapToInt(val->val).summaryStatistics().getCount();
     }
+    public long practice2 () {
+        return Stream.of("APPLE",
+                "Banana", "CHERRY", "kiwi", "PLUM")
+                .filter(w->w.length()%2==0)
+                .map(a->a.toLowerCase())
+                .mapToInt(String::length)
+                .summaryStatistics()
+                .getSum();
+    }
+
+
 }
