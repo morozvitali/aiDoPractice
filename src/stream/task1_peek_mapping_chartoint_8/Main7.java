036ed6aee18992b5e3cc665542f6d3a4e5ef8a29
morozvitali <morozvitali99@gmail.com>
2025-08-26 17:35:22 +0300

    Stream task1 peek [5 of 8]

--- src/stream/task1_peek_mapping_chartoint_8/Main7.java
+++ src/stream/task1_peek_mapping_chartoint_8/Main7.java
@@ -1,5 +1,7 @@
 package stream.task1_peek_mapping_chartoint_8;
 
+import java.util.stream.IntStream;
+
 public class Main7 {
     public int practice1(int n) {
         return String.valueOf(n).chars()
@@ -8,25 +10,33 @@ public class Main7 {
                 .sum();
     }
 
-    public Boolean practice2 (int n) {
+    public Boolean practice2(int n) {
         return String.valueOf(n).chars()
-                .map(c->c-'0')
-                .allMatch(d->d%2==0);
+                .map(c -> c - '0')
+                .allMatch(d -> d % 2 == 0);
     }
 
-    public int practice3 (int n) {
+    public int practice3(int n) {
         return String.valueOf(n).chars()
-                .map(c->c-'0')
+                .map(c -> c - '0')
                 .max()
                 .orElse(-1);
     }
 
-    public long practice4 (int n) {
+    public long practice4(int n) {
         return String.valueOf(n).chars()
-                .map(c->c-'0')
-                .filter(a->a<=5)
+                .map(c -> c - '0')
+                .filter(a -> a <= 5)
                 .count();
     }
 
+    public int practice5(int n) {
+        int[] arr = String.valueOf(n).chars().map(c -> c - '0')
+                .toArray();
+        return IntStream.range(0, arr.length).filter(i -> i % 2 == 1)
+                .map(i -> arr[i])
+                .reduce((a, b) -> a * b)
+                .orElse(-1);
+    }
 
 }
