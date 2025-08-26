54c7c7514ad27410213206c29a73793c3f6473d9
morozvitali <morozvitali99@gmail.com>
2025-08-26 16:15:20 +0300

    Stream task0 SB [1 of 20]

---
+++ src/stream/task0_stringbuilder_20/Main2.java
@@ -0,0 +1,11 @@
+package stream.task0_stringbuilder_20;
+
+public class Main2 {
+    public void practice1() {
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append(" World");
+        System.out.println(sb);
+    }
+
+
+}
