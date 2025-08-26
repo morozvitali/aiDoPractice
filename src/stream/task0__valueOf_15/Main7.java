5a7c3589b7769a7a1b8dd0aaf346d7aa03007a3a
morozvitali <morozvitali99@gmail.com>
2025-08-26 15:23:12 +0300

    Stream task0 [1 of 15]

---
+++ src/stream/task0__valueOf_15/Main7.java
@@ -0,0 +1,14 @@
+package stream.task0__valueOf_15;
+
+import java.util.List;
+
+public class Main7 {
+    public List <String> practice1 () {
+        List<Integer> codes = List.of(97, 98, 99, 100, 101);
+        return codes.stream().map(c->String.valueOf((char)(int)c))
+                .toList();
+    }
+
+
+
+}
