de4e9f4aad22e9058eecdd15ad8def7e41300725
morozvitali <morozvitali99@gmail.com>
2025-08-26 17:24:03 +0300

    Stream task1 peek [1 of 8]

---
+++ src/stream/task1_peek_mapping_chartoint_8/Main7.java
@@ -0,0 +1,14 @@
+package stream.task1_peek_mapping_chartoint_8;
+
+public class Main7 {
+    public int practice1(int n) {
+        return String.valueOf(n).chars()
+                .map(c -> c - '0')
+                .map(d -> d * d)
+                .sum();
+    }
+
+
+
+
+}
