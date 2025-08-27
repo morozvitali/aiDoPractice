6b189f3026702ebc6b51c979bef36fa0cf2c5297
morozvitali <morozvitali99@gmail.com>
2025-08-27 14:57:01 +0300

    Stream task18 [9 of 13]

--- src/stream/task18_flatMap_13/Main9.java
+++ src/stream/task18_flatMap_13/Main9.java
@@ -89,5 +89,10 @@ public class Main9 {
         return Stream.of(10, "hello", true, 42, false, "42").filter(v->v instanceof Integer).map(i->(Integer)i).toList();
     }
 
+    public List <String> practice9 () {
+        return Stream.of("hello", 1, 2.5, true, "world")
+                .filter(a->a instanceof String).map(value -> (String) value).toList();
+    }
+
 
 }
