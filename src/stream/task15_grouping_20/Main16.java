4611870c3af9f265f2bbdc7980bd5d4016f3bf77
morozvitali <morozvitali99@gmail.com>
2025-08-27 11:45:37 +0300

    Stream task15 peek [3 of 20]

--- src/stream/task15_grouping_20/Main16.java
+++ src/stream/task15_grouping_20/Main16.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main16 {
     public long practice1 (String s) {
@@ -15,5 +16,7 @@ public String practice2 (String s) {
         return Arrays.stream(s.split(" ")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream().max(Comparator.comparing(Map.Entry::getValue)).get().getKey();
 }
 
-
+public Map <Integer, Integer> practice3 () {
+        return Stream.of(12, 23, 34, 45, 16, 7).collect(Collectors.groupingBy(v->v%10, Collectors.summingInt(n->n)));
+}
 }
