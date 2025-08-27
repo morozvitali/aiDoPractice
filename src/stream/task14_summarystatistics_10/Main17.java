0ed38755a45a41fe91472e4bd741263bb9f67c36
morozvitali <morozvitali99@gmail.com>
2025-08-27 10:19:53 +0300

    Stream task14 peek [2 of 10]

--- src/stream/task14_summarystatistics_10/Main17.java
+++ src/stream/task14_summarystatistics_10/Main17.java
@@ -2,12 +2,23 @@ package stream.task14_summarystatistics_10;
 
 import java.util.Arrays;
 import java.util.IntSummaryStatistics;
+import java.util.List;
 
 public class Main17 {
     public IntSummaryStatistics practice1 () {
         int[] numbers = {2, 4, 2, 6, 8, 4, 10, 10, 12};
         return Arrays.stream(numbers).distinct().summaryStatistics();
     }
+
+    public Long practice2 () {
+        List<String> words = List.of("APPLE",
+                "Banana", "CHERRY", "kiwi", "PLUM");
+        return words.stream().filter(w->w.length() %2==0)
+                .map(String::toLowerCase)
+                .mapToInt(String::length)
+                .summaryStatistics().getSum();
+    }
+
 
 
 }
