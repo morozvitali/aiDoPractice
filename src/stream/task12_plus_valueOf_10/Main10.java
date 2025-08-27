65974cd775e27a8d0c75393ce306934f554e2757
morozvitali <morozvitali99@gmail.com>
2025-08-27 09:57:56 +0300

    Stream task12 peek [6 of 10]

--- src/stream/task12_plus_valueOf_10/Main10.java
+++ src/stream/task12_plus_valueOf_10/Main10.java
@@ -3,43 +3,51 @@ package stream.task12_plus_valueOf_10;
 import java.util.stream.Collectors;
 
 public class Main10 {
-    public int practice1 (int n) {
+    public int practice1(int n) {
         return Integer.parseInt(String.valueOf(Math.abs(n))
                 .chars()
                 .map(Character::getNumericValue)
-                .map(a->a*a)
+                .map(a -> a * a)
                 .mapToObj(String::valueOf)
                 .collect(Collectors.joining()));
     }
 
-    public int practice2 (int n) {
+    public int practice2(int n) {
         return Integer.parseInt(String.valueOf(Math.abs(n))
                 .chars()
                 .map(Character::getNumericValue)
-                .map(a->a*a*a)
+                .map(a -> a * a * a)
                 .mapToObj(String::valueOf)
                 .collect(Collectors.joining()));
     }
 
-    public int practice3 (int n) {
+    public int practice3(int n) {
         return Integer.parseInt(String.valueOf(Math.abs(n)).chars()
                 .map(Character::getNumericValue)
-                .filter(a->a%2==0)
+                .filter(a -> a % 2 == 0)
                 .mapToObj(String::valueOf)
                 .collect(Collectors.joining()));
     }
 
-    public String practice4 (int n) {
+    public String practice4(int n) {
         return String.valueOf(n).chars().map(Character::getNumericValue)
                 .mapToObj("*"::repeat)
                 .collect(Collectors.joining());
     }
 
-    public int practice5 (int n) {
+    public int practice5(int n) {
         return Integer.parseInt(new StringBuilder(String
                 .valueOf(Math.abs(n)))
                 .reverse()
                 .toString());
+    }
+
+    public int practice6(int n) {
+        return Integer.parseInt(String.valueOf(n).chars()
+                        .map(Character::getNumericValue)
+                        .map(a -> a % 2 == 0 ? a : a * a)
+                        .mapToObj(String::valueOf)
+                        .collect(Collectors.joining()));
     }
 
 
