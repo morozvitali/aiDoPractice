a9ec2111c965d7d15091851291f980568f440ca6
morozvitali <morozvitali99@gmail.com>
2025-08-27 10:42:12 +0300

    Stream task14 peek [9 of 10]

--- src/stream/task14_summarystatistics_10/Main17.java
+++ src/stream/task14_summarystatistics_10/Main17.java
@@ -61,7 +61,11 @@ public IntSummaryStatistics practice8 () {
     return Arrays.stream(words).filter(w->w.length() - w.chars().distinct().count() > 2).mapToInt(String::length).summaryStatistics();
 }
 
-
+public List <String> practice9 () {
+    String[] words = {"education", "questionnaire",
+            "house", "audio", "universe"};
+    return Arrays.stream(words).filter(w->"aeiou".chars().allMatch(c->w.indexOf(c) >= 0)).toList();
+}
 
 
 
