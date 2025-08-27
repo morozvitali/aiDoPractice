bca8a659edbb94e1b74019eb9be25ce022d7a248
morozvitali <morozvitali99@gmail.com>
2025-08-27 16:00:32 +0300

    Stream task20 [5 of 11]

--- src/stream/task20_collectingAndThen_11/Main8.java
+++ src/stream/task20_collectingAndThen_11/Main8.java
@@ -3,6 +3,8 @@ package stream.task20_collectingAndThen_11;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -25,6 +27,13 @@ public class Main8 {
         return Stream.of(5, 3, 4, 4, 3, 6)
                 .distinct().collect(Collectors.collectingAndThen(Collectors.toList(), List::getFirst));
     }
+
+    public Set<Integer> practice5 () {
+        return Stream.of(2, 4, 10, 12, 14, 12, 4)
+                .filter(a->a>10)
+                .collect(Collectors.collectingAndThen(Collectors.toSet(), Collections::unmodifiableSet));
+    }
+
 
 
 }
