106d2b41534c531e8be5813287aa7772f5026c2d
morozvitali <morozvitali99@gmail.com>
2025-08-27 14:36:40 +0300

    Stream task18 [1 of 13]

---
+++ src/stream/task18_flatMap_13/Main9.java
@@ -0,0 +1,29 @@
+package stream.task18_flatMap_13;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main9 {
+    public List <String> practice1 () {
+        List<String> sentences = List.of(
+                "Java is powerful",
+                "Streams are cool",
+                "Java makes sense"
+        );
+
+        return sentences.stream().flatMap(s-> Arrays.stream(s.split(" ")))
+                .distinct().toList();
+    }
+
+    public int practice2 () {
+        int[][] data = {
+                {3, 11},
+                {6, 7},
+                {10}
+        };
+
+        return Arrays.stream(data).flatMapToInt(Arrays::stream).min().orElse(-1);
+    }
+
+
+}
