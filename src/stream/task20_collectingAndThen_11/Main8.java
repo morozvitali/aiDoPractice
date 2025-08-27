938be1b4ac68f0d9c34f7679e13a34d2cc642e95
morozvitali <morozvitali99@gmail.com>
2025-08-27 15:36:08 +0300

    Stream task20 [1 of 11]

---
+++ src/stream/task20_collectingAndThen_11/Main8.java
@@ -0,0 +1,19 @@
+package stream.task20_collectingAndThen_11;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main8 {
+    public List<String> practice1 () {
+        return Stream.of("apple", "car", "banana", "hi", "cherry")
+                .filter(w->w.length() > 4)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
+    }
+
+    public void practice2 () {
+
+    }
+
+}
