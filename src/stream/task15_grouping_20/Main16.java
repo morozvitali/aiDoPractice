7f77841c44e87b53319d3a9cb1116b2b2bfed9c4
morozvitali <morozvitali99@gmail.com>
2025-08-27 11:53:40 +0300

    Stream task15 peek [8 of 20]

--- src/stream/task15_grouping_20/Main16.java
+++ src/stream/task15_grouping_20/Main16.java
@@ -29,5 +29,9 @@ public Map <Integer, Long> practice7 () {
         return "ab123cc44a77".chars().filter(Character::isDigit).boxed().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 }
 
+public Boolean practice8 () {
+        return Stream.of(1, 2, 3, 2).collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream().anyMatch(e->e.getValue() > 1);
+}
+
 
 }
