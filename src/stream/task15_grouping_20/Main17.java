3d8a6b338f23346a793002478bfc1fa0f4def8cc
morozvitali <morozvitali99@gmail.com>
2025-08-28 17:21:22 +0300

    Stream task15 [4 of 20]

--- src/stream/task15_grouping_20/Main17.java
+++ src/stream/task15_grouping_20/Main17.java
@@ -33,5 +33,10 @@ public class Main17 {
         return Stream.of(12, 23, 34, 45, 16, 7).collect(Collectors.groupingBy(a->a%10, Collectors.summingInt(n->n)));
     }
 
+    public Integer practice6 () {
+        return Stream.of(1,2,2,3,3,3,4,4).collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream().max(Comparator.comparing(e->e.getValue())).get().getKey();
+    }
+
+
 
 }
