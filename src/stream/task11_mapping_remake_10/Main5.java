0b79d76166afc7dd3d4e7e1c3c4bdb7cda59290d
morozvitali <morozvitali99@gmail.com>
2025-08-28 12:48:57 +0300

    Stream task11 [4 of 10]

--- src/stream/task11_mapping_remake_10/Main5.java
+++ src/stream/task11_mapping_remake_10/Main5.java
@@ -4,29 +4,34 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class Main5 {
-    public String practice1 () {
+    public String practice1() {
         return Arrays.stream(("Hey fellow warriors").split(" "))
-                .map(w->w.length() >5 ? new StringBuilder(w).reverse().toString() : w).collect(Collectors.joining(" "));
+                .map(w -> w.length() > 5 ? new StringBuilder(w).reverse().toString() : w).collect(Collectors.joining(" "));
     }
 
-public String practice2 (String s) {
+    public String practice2(String s) {
         return Arrays.stream(s.split(" "))
-                .map(w->w.length() >5 ? "[...]" : w)
+                .map(w -> w.length() > 5 ? "[...]" : w)
                 .collect(Collectors.joining());
-}
-
-public String practice3 (String s) {
-        String [] arr = s.split(" ");
-                String temp = arr[0];
-                arr[0] = arr[arr.length-1];
-                arr[arr.length-1] = temp;
-                return String.join(" ", arr);
-                //return Arrays.stream(arr).collect(Collectors.joining(" "));
-}
-
-public void practice() {
-
     }
 
+    public String practice3(String s) {
+        String[] arr = s.split(" ");
+        String temp = arr[0];
+        arr[0] = arr[arr.length - 1];
+        arr[arr.length - 1] = temp;
+        return String.join(" ", arr);
+        //return Arrays.stream(arr).collect(Collectors.joining(" "));
+    }
+
+    public String practice4(String s) {
+        return Arrays.stream(s.split(" "))
+                .map(w->"(" + w + ")")
+                .collect(Collectors.joining(" "));
+    }
+
+    public void practice5 () {
+
+    }
 
 }
