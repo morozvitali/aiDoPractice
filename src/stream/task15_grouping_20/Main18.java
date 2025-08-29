52ec286d297f779d449ab2db45b264a922c20a2a
morozvitali <morozvitali99@gmail.com>
2025-08-29 17:24:50 +0300

    Stream task15 [1 of 20]

---
+++ src/stream/task15_grouping_20/Main18.java
@@ -0,0 +1,17 @@
+package stream.task15_grouping_20;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Main18 {
+    public Map<String, Boolean> practice1 () {
+        String[] words = {"sky", "apple",
+                "dry", "orange", "sun"};
+        return Arrays.stream(words).collect(Collectors.toMap(Function.identity(), w->w.chars().anyMatch(c->"aeiou".indexOf(c) >= 0)));
+    }
+
+
+
+}
