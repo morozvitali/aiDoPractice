9fa858613040cd03476f884d6931f38cb802a63f
morozvitali <morozvitali99@gmail.com>
2025-08-29 15:48:34 +0300

    Stream task12 [7 of 15]

--- src/stream/task12_plus_valueOf_10/Main12.java
+++ src/stream/task12_plus_valueOf_10/Main12.java
@@ -57,5 +57,13 @@ public class Main12 {
                 .collect(Collectors.joining()));
     }
 
+    public int practice7 (int n) {
+        return Integer.parseInt(String.valueOf(Math.abs(n)).chars()
+                .map(Character::getNumericValue)
+                .map(a->a*a*a)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining()));
+    }
+
 
 }
