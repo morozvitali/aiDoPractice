793782f3702650d1b275c1280b205e098e3c5ffa
morozvitali <morozvitali99@gmail.com>
2025-08-29 16:03:28 +0300

    Stream task14 [2 of 10]

--- src/stream/task14_summarystatistics_10/Main19.java
+++ src/stream/task14_summarystatistics_10/Main19.java
@@ -1,6 +1,7 @@
 package stream.task14_summarystatistics_10;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Main19 {
     public long practice1 () {
@@ -8,6 +9,13 @@ public class Main19 {
         return Arrays.stream(numbers).distinct().summaryStatistics().getCount();
     }
 
+    public Long practice2 () {
+        List<String> words = List.of("APPLE",
+                "Banana", "CHERRY", "kiwi", "PLUM");
+        return words.stream().map(String::toLowerCase)
+                .mapToInt(String::length)
+                .summaryStatistics().getSum();
+    }
 
 
 }
