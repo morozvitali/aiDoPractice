22fc9435334efb508e2f15e2e358e94a65fa7d17
morozvitali <morozvitali99@gmail.com>
2025-08-29 15:41:15 +0300

    Stream task12 [5 of 15]

--- src/stream/task12_plus_valueOf_10/Main12.java
+++ src/stream/task12_plus_valueOf_10/Main12.java
@@ -3,28 +3,28 @@ package stream.task12_plus_valueOf_10;
 import java.util.List;
 
 public class Main12 {
-    public long practice1 () {
+    public long practice1() {
         String input = "12345";
         //return Long.parseLong(input); -->int
         return Long.valueOf(input); //--> autounboxing from Integer
     }
 
-    public String practice2 () {
+    public String practice2() {
         long number = 98765L;
         //return String.valueOf(number); any type
         return Long.toString(number); // specific -> long чи Long??
     }
 
-    public List <Long> practice3 () {
+    public List<Long> practice3() {
         List<String> numbers = List.of("10", "20", "30");
 
         //return numbers.stream().map(Long::valueOf).toList();
         return numbers.stream().map(Long::parseLong).toList(); //чомусь автозапак
     }
 
-    public Boolean practice4 () {
+    public Boolean practice4() {
         List<String> values = List.of("100", "abc", "300");
-        return values.stream().allMatch(s-> {
+        return values.stream().allMatch(s -> {
             try {
                 Long.parseLong(s);
                 return true;
@@ -33,4 +33,22 @@ public class Main12 {
             }
         });
     }
+
+    public void practice() {
+        /*
+        int → String
+        String → Integer
+        Integer → long
+         */
+        int number = 1;
+
+        String s = String.valueOf(number);
+        Integer i = Integer.valueOf(s);
+        Long l1 = i.longValue();  // !
+
+        Long l2 = Long.parseLong(String.valueOf(number));
+    }
+
+
+
 }
