f59450c3d02ab2ae3d60b27ae4b51379a7af8970
morozvitali <morozvitali99@gmail.com>
2025-08-29 13:04:15 +0300

    Stream task19 [1 of 10]

--- src/stream/task19_sorted_10/Main10.java
+++ src/stream/task19_sorted_10/Main10.java
@@ -2,14 +2,18 @@ package stream.task19_sorted_10;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main10 {
-    public List <String> practice1 () {
+    public List <String> practice0 () {
         List<String> words = List.of("sun", "banana", "kiwi",
                 "cherry", "apple", "tea", "pear");
         return words.stream().sorted(Comparator.comparingInt(String::length).thenComparing(Comparator.naturalOrder())).toList();
     }
 
+    public List <String> practice2 () {
+        return Stream.of("pear", "apple", "banana").sorted(Comparator.naturalOrder()).toList();
+    }
 
 
 }
