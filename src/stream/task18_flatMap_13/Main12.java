257da27be1f1ac6819c45d3a1fc6b7c21c61ad07
morozvitali <morozvitali99@gmail.com>
2025-09-01 12:04:47 +0300

    Stream task18 grouping [3 of 13]

--- src/stream/task18_flatMap_13/Main12.java
+++ src/stream/task18_flatMap_13/Main12.java
@@ -11,15 +11,29 @@ public class Main12 {
                 .map(String::toLowerCase).distinct().collect(Collectors.toList());
     }
 
-public int practice2 () {
-    int[][] data = {
-            {3, 11},
-            {6, 7},
-            {10}
-    };
-    return Arrays.stream(data).flatMapToInt(Arrays::stream)
-            .filter(a->a%2==0)
-            .min()
-            .orElse(-1);
-}
+    public int practice2() {
+        int[][] data = {
+                {3, 11},
+                {6, 7},
+                {10}
+        };
+        return Arrays.stream(data).flatMapToInt(Arrays::stream)
+                .filter(a -> a % 2 == 0)
+                .min()
+                .orElse(-1);
+    }
+
+    public int practice3() {
+        int[][] data = {
+                {-3, 5},
+                {2},
+                {-1, 4}
+        };
+
+        return Arrays.stream(data).flatMapToInt(Arrays::stream)
+                .filter(a -> a > 0)
+                .reduce((a, b) -> a * b)
+                .orElse(-1);
+    }
+
 }
