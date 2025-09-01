aa02b04c0888ebd83c80a4cd1a2f14daf7c23737
morozvitali <morozvitali99@gmail.com>
2025-09-01 14:55:26 +0300

    Stream task19 grouping [3 of 10]

--- src/stream/task19_sorted_10/Main12.java
+++ src/stream/task19_sorted_10/Main12.java
@@ -5,13 +5,19 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class Main12 {
-public List<String> practice1 () {
-    return Stream.of("tea", "lemon",
-            "fig", "grape", "banana")
-            .filter(s->s.length() > 3)
-            .sorted(Comparator.comparing(String::length))
-            .toList();
-}
+    public List<String> practice1() {
+        return Stream.of("tea", "lemon",
+                        "fig", "grape", "banana")
+                .filter(s -> s.length() > 3)
+                .sorted(Comparator.comparing(String::length))
+                .toList();
+    }
+
+    public List<Integer> practice3() {
+        return Stream.of(3, 7, 1, 9, 2).sorted(Comparator.reverseOrder()).toList();
+    }
 
 
+
 }
+
