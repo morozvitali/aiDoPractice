a4c9f608f1379d0dd4e5000ca80a2b046577ba0d
morozvitali <morozvitali99@gmail.com>
2025-09-01 17:01:09 +0300

    Stream task20 grouping [8 of 11]

--- src/stream/task20_collectingAndThen_11/Main11.java
+++ src/stream/task20_collectingAndThen_11/Main11.java
@@ -5,43 +5,49 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main11 {
-    public List<String> practice1 () {
+    public List<String> practice1() {
         return Stream.of("apple", "car", "banana", "hi", "cherry")
-                .filter(w->w.length() >4)
+                .filter(w -> w.length() > 4)
                 .collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
     }
 
-    public Integer practice2 () {
+    public Integer practice2() {
         return Stream.of(3, 9, 2, 4, 6, 7, 12)
-                .filter(n->n%3==0)
+                .filter(n -> n % 3 == 0)
                 .collect(Collectors.collectingAndThen(Collectors.toList(), List::size));
     }
 
-    public String practice3 () {
+    public String practice3() {
         List<String> data = List.of("apple", "car", "banana", "hi", "cherry");
         return data.stream().sorted(Comparator.comparing(String::length).reversed())
                 .collect(Collectors.collectingAndThen(Collectors.toList(), list -> list.get(0)));
     }
 
-    public Integer practice4 () {
+    public Integer practice4() {
         return Stream.of(5, 3, 4, 4, 3, 6)
                 .distinct().collect(Collectors.collectingAndThen(Collectors.toList(), List::getFirst));
     }
 
-    public Set<Integer> practice5 () {
+    public Set<Integer> practice5() {
         return Stream.of(2, 4, 10, 12, 14, 12, 4).collect(Collectors.collectingAndThen(Collectors.toSet(), set -> Collections.unmodifiableSet(set)));
     }
 
-   public Double practice6 () {
-        return Stream.of("code", "stream", "java").collect(Collectors.collectingAndThen(Collectors.toList(), list->list.stream().mapToInt(String::length).average().orElse(-1)));
-   }
+    public Double practice6() {
+        return Stream.of("code", "stream", "java").collect(Collectors.collectingAndThen(Collectors.toList(), list -> list.stream().mapToInt(String::length).average().orElse(-1)));
+    }
 
-   public Map<Character, List <String>> practice7 () {
+    public Map<Character, List<String>> practice7() {
         return Stream.of("apple", "ant", "banana",
-                "bat", "car").collect(Collectors.collectingAndThen(Collectors.groupingBy(w->w.charAt(0)), Collections::unmodifiableMap));
-   }
+                "bat", "car").collect(Collectors.collectingAndThen(Collectors.groupingBy(w -> w.charAt(0)), Collections::unmodifiableMap));
+    }
 
-   public void practice8 () {
-
-   }
+    public String practice8() {
+        return Stream.of("java", "zip", "jazz").filter(w -> w.contains("z"))
+                .collect(Collectors.collectingAndThen(Collectors.collectingAndThen(Collectors.toList(), list -> {\
+                    if (list.size() != 1) {
+                        throw new IllegalStateException("exept one z");
+                    }
+                    return list.get(0);
+                })));
+    }
 }
