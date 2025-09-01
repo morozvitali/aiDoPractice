98103ffb204687356c603384c5659d68d4b4ef7b
morozvitali <morozvitali99@gmail.com>
2025-09-01 17:23:50 +0300

    Stream task12 grouping [4 of 15]

--- src/stream/task12_plus_valueOf_10/Main13.java
+++ src/stream/task12_plus_valueOf_10/Main13.java
@@ -1,5 +1,7 @@
 package stream.task12_plus_valueOf_10;
 
+import java.util.List;
+
 public class Main13 {
     String input = "12345";
     long result = Long.parseLong(input);
@@ -7,5 +9,21 @@ public class Main13 {
     String s = String.valueOf(result);
     String s2 = Long.toString(result);
 
+    public List<Long> practice3() {
+        List<String> numbers = List.of("10", "20", "30");
+        return numbers.stream().map(Long::valueOf).toList();
+    }
+
+    public boolean practice4 () {
+        List<String> values = List.of("100", "abc", "300");
+        return values.stream().allMatch(s->{
+            try {
+                Long.parseLong(s);
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        });
+    }
 
 }
