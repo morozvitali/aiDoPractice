5795943ae77e9cf94d281b57d3427fbe96643434
morozvitali <morozvitali99@gmail.com>
2025-09-02 17:08:58 +0300

    Stream task18 grouping [1 of 13]

---
+++ src/stream/task18_flatMap_13/Main13.java
@@ -0,0 +1,19 @@
+package stream.task18_flatMap_13;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main13 {
+    public List <String> practice1 () {
+        List<String> sentences = List.of(
+                "Java is powerful",
+                "Streams are cool",
+                "Java makes sense"
+        );
+        return sentences.stream().flatMap(s-> Arrays.stream(s.split(" "))).distinct().toList();
+    }
+
+
+
+}
