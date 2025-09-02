8ef54bea09e6eba449c7a2b806f710c2d867f7e7
morozvitali <morozvitali99@gmail.com>
2025-09-02 16:40:31 +0300

    Stream task17 grouping [5 of 15]

--- src/stream/task17_collectors_15/Main17.java
+++ src/stream/task17_collectors_15/Main17.java
@@ -29,5 +29,10 @@ public class Main17 {
         return words.stream().collect(Collectors.groupingBy(String::length, Collectors.toSet()));
     }
 
+    public String practice5 () {
+        List<Integer> numbers = List.of(4, 8, 15, 16, 23, 42);
+        return numbers.stream().collect(Collectors.collectingAndThen(Collectors.counting(), count-> "count - " + count));
+    }
+
 
 }
