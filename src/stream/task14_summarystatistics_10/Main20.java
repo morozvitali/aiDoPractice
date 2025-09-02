7adacf5222b22c206f09acf786b9d5395aec6b87
morozvitali <morozvitali99@gmail.com>
2025-09-02 14:20:21 +0300

    Stream task14 grouping [9 of 10]

--- src/stream/task14_summarystatistics_10/Main20.java
+++ src/stream/task14_summarystatistics_10/Main20.java
@@ -62,4 +62,12 @@ public class Main20 {
         return Arrays.stream(words).filter(w->w.length() - w.chars().distinct().count() > 2)
                 .mapToInt(String::length).summaryStatistics();
     }
+
+    public List <String> practice9 () {
+        String[] words = {"education", "questionnaire",
+                "house", "audio", "universe"};
+        return Arrays.stream(words).filter(w->"aeiou".chars().allMatch(c->w.indexOf(c)>=0)).collect(Collectors.toList());
+    }
+
+
 }
