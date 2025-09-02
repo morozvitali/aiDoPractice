bbf889f63967a0847a6e48489b8ac6f5a219c2fa
morozvitali <morozvitali99@gmail.com>
2025-09-02 14:01:18 +0300

    Stream task14 grouping [4 of 10]

--- src/stream/task14_summarystatistics_10/Main20.java
+++ src/stream/task14_summarystatistics_10/Main20.java
@@ -33,4 +33,11 @@ public class Main20 {
     }
 
 
+    public Double practice4 () {
+        String[] words = {"sky", "apple", "moon",
+                "dry", "sun"};
+        return Arrays.stream(words).filter(w->countVowels(w) > 0).mapToInt(String::length).summaryStatistics().getAverage();
+    }
+
+
 }
