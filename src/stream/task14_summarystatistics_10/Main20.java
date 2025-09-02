1ad8bb89c24f9733491f07c9085fa459ae1c4030
morozvitali <morozvitali99@gmail.com>
2025-09-02 14:04:31 +0300

    Stream task14 grouping [5 of 10]

--- src/stream/task14_summarystatistics_10/Main20.java
+++ src/stream/task14_summarystatistics_10/Main20.java
@@ -1,6 +1,7 @@
 package stream.task14_summarystatistics_10;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -39,5 +40,9 @@ public class Main20 {
         return Arrays.stream(words).filter(w->countVowels(w) > 0).mapToInt(String::length).summaryStatistics().getAverage();
     }
 
-
+    public String practice5 () {
+        String[] words = {"apple", "orange", "banana",
+                "ice", "umbrella", "echo"};
+        return Arrays.stream(words).filter(w->"aeiou".indexOf(w.charAt(0))>=0).min(Comparator.comparing(String::length)).orElse("");
+    }
 }
