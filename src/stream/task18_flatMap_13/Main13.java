32178206b4ba215179e42d249b83e5978d243160
morozvitali <morozvitali99@gmail.com>
2025-09-02 17:12:58 +0300

    Stream task18 grouping [3 of 13]

--- src/stream/task18_flatMap_13/Main13.java
+++ src/stream/task18_flatMap_13/Main13.java
@@ -5,24 +5,37 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main13 {
-    public List <String> practice1 () {
+    public List<String> practice1() {
         List<String> sentences = List.of(
                 "Java is powerful",
                 "Streams are cool",
                 "Java makes sense"
         );
-        return sentences.stream().flatMap(s-> Arrays.stream(s.split(" "))).distinct().toList();
+        return sentences.stream().flatMap(s -> Arrays.stream(s.split(" "))).distinct().toList();
     }
 
-    public int practice2 () {
+    public int practice2() {
         int[][] data = {
                 {3, 11},
                 {6, 7},
                 {10}
         };
 
-        Arrays.stream(data).flatMapToInt(Arrays::stream)
-                .filter(a->a%2==0).min().orElse(-1);
+        return Arrays.stream(data).flatMapToInt(Arrays::stream)
+                .filter(a -> a % 2 == 0).min().orElse(-1);
     }
+
+    public int practice3() {
+        int[][] data = {
+                {-3, 5},
+                {2},
+                {-1, 4}
+        };
+        return Arrays.stream(data).flatMapToInt(Arrays::stream)
+                .filter(v->v>0).reduce((a,b)-> a*b).orElse(-1);
+    }
+
+
+
 
 }
