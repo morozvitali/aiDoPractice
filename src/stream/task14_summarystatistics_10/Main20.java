92e88b115d33a196a6a87372e2f8f687ade5777e
morozvitali <morozvitali99@gmail.com>
2025-09-02 13:58:32 +0300

    Stream task14 grouping [3 of 10]

--- src/stream/task14_summarystatistics_10/Main20.java
+++ src/stream/task14_summarystatistics_10/Main20.java
@@ -2,24 +2,35 @@ package stream.task14_summarystatistics_10;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main20 {
-        public Long practice1() {
-            int[] numbers = {2, 4, 2, 6, 8, 4, 10, 10, 12};
-            return Arrays.stream(numbers).filter(a->a%2==0)
-                    .distinct()
-                    .summaryStatistics().getCount();
-        }
+    public Long practice1() {
+        int[] numbers = {2, 4, 2, 6, 8, 4, 10, 10, 12};
+        return Arrays.stream(numbers).filter(a -> a % 2 == 0)
+                .distinct()
+                .summaryStatistics().getCount();
+    }
 
-        public int practice2() {
-            List<String> words = List.of("APPLE",
-                    "Banana", "CHERRY", "kiwi", "PLUM");
-            return words.stream().filter(w->w.length()%2==0)
-                    .map(String::toLowerCase)
-                    .mapToInt(String::length)
-                    .sum();
-        }
+    public int practice2() {
+        List<String> words = List.of("APPLE",
+                "Banana", "CHERRY", "kiwi", "PLUM");
+        return words.stream().filter(w -> w.length() % 2 == 0)
+                .map(String::toLowerCase)
+                .mapToInt(String::length)
+                .sum();
+    }
 
+    public Map<String, Long> practice3() {
+        String[] words = {"sky", "apple",
+                "moon", "dry", "banana"};
+        return Arrays.stream(words).filter(w -> countVowels(w) > 1 && w.length() > 3).collect(Collectors.toMap(w->w, w->countVowels(w)));
+    }
+
+    public static long countVowels(String s) {
+        return s.chars().filter(c -> "aeiou".indexOf(c) >= 0).count();
+    }
 
 
 }
