e0bf9582040622eb9917d7c5cdcb277a3509889c
morozvitali <morozvitali99@gmail.com>
2025-09-03 17:36:33 +0300

    Stream task17 [2 of 15]

--- src/stream/task17_collectors_15/Main18.java
+++ src/stream/task17_collectors_15/Main18.java
@@ -1,5 +1,6 @@
 package stream.task17_collectors_15;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -9,6 +10,12 @@ public class Main18 {
         List<String> words = List.of("apple",
                 "ant", "banana", "bat", "car");
         return words.stream().collect(Collectors.groupingBy(w->w.charAt(0), Collectors.mapping(w->w.toUpperCase(), Collectors.toList())));
+    }
+
+    public Map <Character, List<String>> practice2 () {
+        List<String> words = List.of("apple", "ant",
+                "banana", "bat", "car");
+        return words.stream().collect(Collectors.groupingBy(w->w.charAt(0), Collectors.mapping(String::valueOf, Collectors.toList())));
     }
 
 }
