0bfe9d48d128517e6604068b5f257fbfa53333a3
morozvitali <morozvitali99@gmail.com>
2025-09-03 15:58:48 +0300

    Stream task12 [4 of 10]

--- src/stream/task12_plus_valueOf_10/Main14.java
+++ src/stream/task12_plus_valueOf_10/Main14.java
@@ -4,21 +4,42 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main14 {
-    public void practice1 () {
+    public void practice1() {
         //String.valueOf(Math.abs(n)).chars().
         String input = "12345";
         long result = Long.parseLong(input);
         System.out.println(result);
     }
 
-    public void practice2 (int i) {
+    public void practice2(int i) {
         String s1 = Integer.toString(i);
         String s2 = String.valueOf(i);
         System.out.println(s1 + ", " + s2);
     }
 
-    public List <Long> practice3 () {
+    public List<Long> practice3() {
         List<String> numbers = List.of("10", "20", "30");
         return numbers.stream().map(Long::valueOf).toList();
     }
+
+    public boolean practice4() {
+        List<String> values = List.of("100", "abc", "300");
+
+
+        for (String value : values) {
+            {
+                try {
+
+                    System.out.println(Long.parseLong(value));
+                    return true;
+                } catch (NumberFormatException e) {
+                    return false;
+                }
+
+            }
+        }
+        return true;
+    }
+
+
 }
