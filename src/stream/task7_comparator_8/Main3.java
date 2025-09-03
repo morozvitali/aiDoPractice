bed0b993e02056a475bb28445869082ec879da26
morozvitali <morozvitali99@gmail.com>
2025-09-03 14:07:45 +0300

    Stream task7 [3 of 8]

--- src/stream/task7_comparator_8/Main3.java
+++ src/stream/task7_comparator_8/Main3.java
@@ -8,25 +8,30 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main3 {
-    public List <String> practice1 () {
+    public List<String> practice1() {
         List<String> words = List.of("sky", "banana",
                 "apple", "grape", "orange", "plum");
         return words.stream().sorted(Comparator.comparing(Main3::getCount)).toList();
     }
 
     public static long getCount(String s) {
-        return s.chars().filter(c->"aeiou".indexOf(c)>=0).count();
+        return s.chars().filter(c -> "aeiou".indexOf(c) >= 0).count();
     }
 
-    public String practice2 () {
+    public String practice2() {
         return Stream.of("alpha", "arena", "java", "banana", "lava")
-                .collect(Collectors.toMap(Function.identity(), w->getcount2(w, 'a')))
+                .collect(Collectors.toMap(Function.identity(), w -> getcount2(w, 'a')))
                 .entrySet().stream().max(Comparator.comparing(Map.Entry::getValue)).map(Map.Entry::getKey).orElse("");
     }
 
-    public long getcount2 (String s, char c) {
+    public long getcount2(String s, char c) {
         return s.chars().filter(ch -> ch == c).count();
     }
 
-
+    public int practice3(int n) {
+        return String.valueOf(Math.abs(n)).chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream().max(Map.Entry.comparingByValue()).get().getKey();
+    }
 }
