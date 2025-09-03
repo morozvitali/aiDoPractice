fb77374a116678617c8fdf8810f2284a6aa57911
morozvitali <morozvitali99@gmail.com>
2025-09-03 16:46:45 +0300

    Stream task15 [1 of 20]

---
+++ src/stream/task15_grouping_20/Main20.java
@@ -0,0 +1,11 @@
+package stream.task15_grouping_20;
+
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Main20 {
+    public long practice1 (String s) {
+        return s.chars().mapToObj(c->(char)c).collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream().filter(e->e.getValue() >1).count();
+    }
+
+}
