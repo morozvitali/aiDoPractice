c9979a2b85195c41aad259b2d46e3bb051eb9e75
morozvitali <morozvitali99@gmail.com>
2025-09-03 15:48:44 +0300

    Stream task12 [2 of 10]

--- src/stream/task12_plus_valueOf_10/Main14.java
+++ src/stream/task12_plus_valueOf_10/Main14.java
@@ -8,5 +8,11 @@ public class Main14 {
         System.out.println(result);
     }
 
+    public void practice2 (int i) {
+        String s1 = Integer.toString(i);
+        String s2 = String.valueOf(i);
+        System.out.println(s1 + ", " + s2);
+    }
+
 
 }
