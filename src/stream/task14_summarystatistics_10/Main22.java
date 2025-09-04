b3303ae49273a504a3616ce29b16b6250ab809b1
morozvitali <morozvitali99@gmail.com>
2025-09-04 15:54:54 +0300

    Stream task14 [9 of 10]

--- src/stream/task14_summarystatistics_10/Main22.java
+++ src/stream/task14_summarystatistics_10/Main22.java
@@ -67,4 +67,9 @@ public class Main22 {
             return Arrays.stream(words).filter(w->w.length() - w.chars().distinct().count() >2).mapToInt(String::length).summaryStatistics();
         }
 
+        public List <String> practice9 () {
+            String[] words = {"education", "questionnaire",
+                    "house", "audio", "universe"};
+            return Arrays.stream(words).filter(w->"aeiou".chars().allMatch(c->w.indexOf(c)>=0)).toList();
+        }
 }
