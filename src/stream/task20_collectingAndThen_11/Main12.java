b2dcc59da7dac47ccd2b564245743dd731692d69
morozvitali <morozvitali99@gmail.com>
2025-09-04 13:56:16 +0300

    Stream task20 [6 of 10]

--- src/stream/task20_collectingAndThen_11/Main12.java
+++ src/stream/task20_collectingAndThen_11/Main12.java
@@ -8,31 +8,37 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main12 {
-    public List<String> practice1 () {
+    public List<String> practice1() {
         return Stream.of("apple", "car", "banana", "hi", "cherry")
                 .collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
     }
 
-public Integer practice2 () {
+    public Integer practice2() {
         return Stream.of(3, 9, 2, 4, 6, 7, 12)
-                .filter(a->a%3==0)
-                .collect(Collectors.collectingAndThen(Collectors.toList(), list-> list.size()));
-}
+                .filter(a -> a % 3 == 0)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), list -> list.size()));
+    }
 
-public String practice3 () {
+    public String practice3() {
         return Stream.of("sun", "supernova", "star")
-                .collect(Collectors.collectingAndThen(Collectors.toList(), list-> list.stream().sorted(Comparator.comparing(String::length).thenComparing(Comparator.reverseOrder())).toList().getFirst()));
-}
+                .collect(Collectors.collectingAndThen(Collectors.toList(), list -> list.stream().sorted(Comparator.comparing(String::length).thenComparing(Comparator.reverseOrder())).toList().getFirst()));
+    }
 
-public Integer practice4 () {
+    public Integer practice4() {
         return Stream.of(5, 3, 4, 4, 3, 6)
                 .distinct().collect(Collectors.collectingAndThen(Collectors.toList(), List::getFirst));
-}
+    }
 
-public Set<Integer> practice5 () {
+    public Set<Integer> practice5() {
         return Stream.of(2, 4, 10, 12, 14, 12, 4)
                 .collect(Collectors.collectingAndThen(Collectors.toSet(), Collections::unmodifiableSet));
-}
+    }
+
+    public Double practice6() {
+        return Stream.of("code", "stream", "java")
+                .collect(Collectors.collectingAndThen(Collectors.toList(), list -> list.stream().mapToInt(String::length)
+                        .average().orElse(-1)));
+    }
 
 
 }
