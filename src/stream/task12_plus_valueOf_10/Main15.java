6635ffcf9880156a5cf61ee3ead2b11e52b54060
morozvitali <morozvitali99@gmail.com>
2025-09-04 14:32:38 +0300

    Stream task12 [3 of 10]

--- src/stream/task12_plus_valueOf_10/Main15.java
+++ src/stream/task12_plus_valueOf_10/Main15.java
@@ -1,5 +1,7 @@
 package stream.task12_plus_valueOf_10;
 
+import java.util.List;
+
 public class Main15 {
     public void practice1() {
         String input = "12345";
@@ -13,5 +15,12 @@ public class Main15 {
         String s1 = String.valueOf(l);
         String s2 = Long.toString(l);
     }
+
+    public List <Long> practice3 () {
+        List<String> numbers = List.of("10", "20", "30");
+        return numbers.stream().map(Long::valueOf).toList();
+    }
+
+
 
 }
