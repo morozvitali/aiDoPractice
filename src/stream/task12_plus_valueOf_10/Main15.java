1095c389813a5dcb7755266048d2b0391b5639ae
morozvitali <morozvitali99@gmail.com>
2025-09-04 14:26:26 +0300

    Stream task12 [1 of 10]

---
+++ src/stream/task12_plus_valueOf_10/Main15.java
@@ -0,0 +1,11 @@
+package stream.task12_plus_valueOf_10;
+
+public class Main15 {
+    public void practice1() {
+        String input = "12345";
+        Long l1 = Long.valueOf(input);
+        long l2 = Long.parseLong(input);
+    }
+
+
+}
