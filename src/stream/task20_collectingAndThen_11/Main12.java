56f9d09c0880f2ab05e1ec90f05e22bf30844ca9
morozvitali <morozvitali99@gmail.com>
2025-09-04 13:41:43 +0300

    Stream task20 [2 of 10]

--- src/stream/task20_collectingAndThen_11/Main12.java
+++ src/stream/task20_collectingAndThen_11/Main12.java
@@ -11,6 +11,11 @@ public class Main12 {
                 .collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
     }
 
+public Integer practice2 () {
+        return Stream.of(3, 9, 2, 4, 6, 7, 12)
+                .filter(a->a%3==0)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), list-> list.size()));
+}
 
 
 }
