07c018067f5c55041d3f9fe93d524bd356ae4865
morozvitali <morozvitali99@gmail.com>
2025-09-05 11:55:52 +0300

    Stream task15 [1 of 20]

--- src/stream/task14_summarystatistics_10/Main23.java
+++ src/stream/task14_summarystatistics_10/Main23.java
@@ -5,81 +5,79 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main23 {
-public Long practice1 () {
-    int[] numbers = {2, 4, 2, 6, 8, 4, 10, 10, 12};
-    return Arrays.stream(numbers).distinct().summaryStatistics().getCount();
-}
+    public Long practice1() {
+        int[] numbers = {2, 4, 2, 6, 8, 4, 10, 10, 12};
+        return Arrays.stream(numbers).distinct().summaryStatistics().getCount();
+    }
 
-public int practice2 () {
-    List<String> words = List.of("APPLE",
-            "Banana", "CHERRY", "kiwi", "PLUM");
+    public int practice2() {
+        List<String> words = List.of("APPLE",
+                "Banana", "CHERRY", "kiwi", "PLUM");
 
-    return words.stream().filter(w->w.length()%2==0)
-            .map(String::toLowerCase)
-            .mapToInt(String::length)
-            .sum();
-}
+        return words.stream().filter(w -> w.length() % 2 == 0)
+                .map(String::toLowerCase)
+                .mapToInt(String::length)
+                .sum();
+    }
 
-public Map<String, Long> practice3 () {
-    String[] words = {"sky", "apple",
-            "moon", "dry", "banana"};
-    return Arrays.stream(words).filter(w->w.length() > 3)
-            .collect(Collectors.toMap(Function.identity(), Main23::counter));
-}
+    public Map<String, Long> practice3() {
+        String[] words = {"sky", "apple",
+                "moon", "dry", "banana"};
+        return Arrays.stream(words).filter(w -> w.length() > 3)
+                .collect(Collectors.toMap(Function.identity(), Main23::counter));
+    }
 
-public static long counter (String s) {
-    return s.chars().filter(c->"aeiuo".indexOf(c) >=0).count();
-}
+    public static long counter(String s) {
+        return s.chars().filter(c -> "aeiuo".indexOf(c) >= 0).count();
+    }
 
-public Double practice4 () {
-    String[] words = {"sky", "apple", "moon",
-            "dry", "sun"};
-    return Arrays.stream(words).filter(w->counter(w)>0)
-            .mapToInt(String::length)
-            .summaryStatistics().getAverage();
-}
+    public Double practice4() {
+        String[] words = {"sky", "apple", "moon",
+                "dry", "sun"};
+        return Arrays.stream(words).filter(w -> counter(w) > 0)
+                .mapToInt(String::length)
+                .summaryStatistics().getAverage();
+    }
 
-public String practice5 () {
-    String[] words = {"apple", "orange", "banana",
-            "ice", "umbrella", "echo"};
-    return Arrays.stream(words).filter(w->"aeiou".indexOf(w.charAt(0))>=0)
-            .min(Comparator.comparing(String::length))
-            .orElse("");
-}
+    public String practice5() {
+        String[] words = {"apple", "orange", "banana",
+                "ice", "umbrella", "echo"};
+        return Arrays.stream(words).filter(w -> "aeiou".indexOf(w.charAt(0)) >= 0)
+                .min(Comparator.comparing(String::length))
+                .orElse("");
+    }
 
-public Map <Character, Double> practice6 () {
-    String[] words = {"apple", "ant",
-            "banana", "blue", "berry", "dolphin"};
-    return Arrays.stream(words)
-            .collect(Collectors.groupingBy(w->w.charAt(0), Collectors.averagingInt(String::length)));
-}
+    public Map<Character, Double> practice6() {
+        String[] words = {"apple", "ant",
+                "banana", "blue", "berry", "dolphin"};
+        return Arrays.stream(words)
+                .collect(Collectors.groupingBy(w -> w.charAt(0), Collectors.averagingInt(String::length)));
+    }
 
-public Map <Character, Long> practice7 () {
-    String[] words = {"apple", "orange",
-            "banana", "umbrella"};
-    return Arrays.stream(words)
-            .flatMap(w->w.chars().mapToObj(c->(char)c))
-            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-}
+    public Map<Character, Long> practice7() {
+        String[] words = {"apple", "orange",
+                "banana", "umbrella"};
+        return Arrays.stream(words)
+                .flatMap(w -> w.chars().mapToObj(c -> (char) c))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
 
-public IntSummaryStatistics practice8 () {
-    String[] words = {"apple", "moon",
-            "sky", "banana", "loop"};
-    return Arrays.stream(words).filter(w->w.length() - w.chars().distinct().count() > 2)
-            .mapToInt(String::length).summaryStatistics();
-}
+    public IntSummaryStatistics practice8() {
+        String[] words = {"apple", "moon",
+                "sky", "banana", "loop"};
+        return Arrays.stream(words).filter(w -> w.length() - w.chars().distinct().count() > 2)
+                .mapToInt(String::length).summaryStatistics();
+    }
 
-public List <String> practice9 () {
-    String[] words = {"education", "questionnaire",
-            "house", "audio", "universe"};
-    return Arrays.stream(words).filter(w->"aeiou".chars().allMatch(c->w.indexOf(c)>=0)).collect(Collectors.toList());
-}
+    public List<String> practice9() {
+        String[] words = {"education", "questionnaire",
+                "house", "audio", "universe"};
+        return Arrays.stream(words).filter(w -> "aeiou".chars().allMatch(c -> w.indexOf(c) >= 0)).collect(Collectors.toList());
+    }
 
-public Map <Boolean, List <String>> practice10 () {
-    String[] words = {"Apple", "banana",
-            "Cherry", "date", "Eggplant"};
-    return Arrays.stream(words).collect(Collectors.groupingBy(w->Character.isUpperCase(w.charAt(0))));
-}
-
-
+    public Map<Boolean, List<String>> practice10() {
+        String[] words = {"Apple", "banana",
+                "Cherry", "date", "Eggplant"};
+        return Arrays.stream(words).collect(Collectors.groupingBy(w -> Character.isUpperCase(w.charAt(0))));
+    }
 }
