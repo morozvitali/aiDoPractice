ed7d6007347644c3f17d335adc4e93363f9b8137
morozvitali <morozvitali99@gmail.com>
2025-09-05 10:09:16 +0300

    Stream task12 [2 of 12]

--- src/stream/task12_plus_valueOf_10/Main16.java
+++ src/stream/task12_plus_valueOf_10/Main16.java
@@ -1,7 +1,20 @@
 package stream.task12_plus_valueOf_10;
 
 public class Main16 {
-    String input = "12345";
-    long l1 = Long.parseLong(input);
-    Long l2 = Long.valueOf(input);
+
+    public void practice1() {
+        String input = "12345";
+        long l1 = Long.parseLong(input);
+        Long l2 = Long.valueOf(input);
+    }
+
+    public void practice2 () {
+        long number = 98765L;
+        String s1 = Long.toString(number);
+        String s2 = String.valueOf(number);
+    }
+
+
+
+
 }
