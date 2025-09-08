581cd28da5b8cbb79078238c3e7c1a89ce77809c
morozvitali <morozvitali99@gmail.com>
2025-09-08 14:23:27 +0300

    Stream task14 [3 of 10]

--- src/stream/task14_summarystatistics_10/Main24.java
+++ src/stream/task14_summarystatistics_10/Main24.java
@@ -1,12 +1,35 @@
 package stream.task14_summarystatistics_10;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Main24 {
     public long practice1 () {
         int[] numbers = {2, 4, 2, 6, 8, 4, 10, 10, 12};
         return Arrays.stream(numbers).filter(a->a%2==0)
                 .distinct().summaryStatistics().getCount();
+    }
+
+    public long practice2 () {
+        List<String> words = List.of("APPLE",
+                "Banana", "CHERRY", "kiwi", "PLUM");
+        return words.stream().filter(a->a.length()%2==0)
+                .map(String::toLowerCase)
+                .mapToInt(String::length)
+                .summaryStatistics()
+                .getSum();
+    }
+
+    public Map<String, Long> practice3 () {
+        String[] words = {"sky", "apple",
+                "moon", "dry", "banana"};
+        return Arrays.stream(words)
+                .filter(a->a.length()>3)
+                .filter(a->a.chars().anyMatch(c->"aeiou".indexOf(c)>=0))
+                .collect(Collectors.toMap(Function.identity(), w->w.chars().filter(c->"aeiou".indexOf(c)>=0).count()));
     }
 
 
