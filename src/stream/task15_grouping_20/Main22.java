076a81ec33469e2ef4c87ed5342148451a8a3580
morozvitali <morozvitali99@gmail.com>
2025-09-08 15:55:28 +0300

    Stream task15 [8 of 20]

--- src/stream/task15_grouping_20/Main22.java
+++ src/stream/task15_grouping_20/Main22.java
@@ -39,5 +39,11 @@ public class Main22 {
         return "ab123cc44a77".chars().filter(Character::isDigit).boxed().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
 
+    public boolean practice8 () {
+        return Stream.of(1, 2, 3, 2).collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream().anyMatch(e->e.getValue() > 1);
+    }
+
+
+
 
 }
