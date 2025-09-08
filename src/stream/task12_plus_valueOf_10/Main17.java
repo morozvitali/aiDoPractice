f73726db1f39eab52c972bd3c68dbdc1f861f7b0
morozvitali <morozvitali99@gmail.com>
2025-09-08 11:25:32 +0300

    Stream task12 [1 of 10]

---
+++ src/stream/task12_plus_valueOf_10/Main17.java
@@ -0,0 +1,15 @@
+package stream.task12_plus_valueOf_10;
+
+public class Main17 {
+    public void practice1() {
+
+        String s = "1";
+        Long l1 = Long.parseLong(s);
+        Long l2 = Long.valueOf(s);
+    }
+
+    public void practice2() {
+    }
+
+}
+
