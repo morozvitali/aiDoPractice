f497005823301b726d3753cfb21861881b601819
morozvitali <morozvitali99@gmail.com>
2025-09-08 14:57:24 +0300

    Stream task14 [10 of 10]

--- src/stream/task14_summarystatistics_10/Main24.java
+++ src/stream/task14_summarystatistics_10/Main24.java
@@ -72,6 +72,10 @@ public class Main24 {
         return Arrays.stream(words).filter(w->"aeiou".chars().allMatch((c->w.indexOf(c) >= 0))).toList();
     }
 
-
+    public Map <Boolean, List <String>> practice10 () {
+        String[] words = {"Apple", "banana",
+                "Cherry", "date", "Eggplant"};
+        return Arrays.stream(words).collect(Collectors.partitioningBy(w->Character.isUpperCase(w.charAt(0))));
+    }
 
 }
