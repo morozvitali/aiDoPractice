93dd1f2481e239e410620d1c3e8fd97b9f300216
morozvitali <morozvitali99@gmail.com>
2025-09-09 11:57:27 +0300

    Stream task16 [3 of 8]

--- src/stream/task16_comparator_8/Main4.java
+++ src/stream/task16_comparator_8/Main4.java
@@ -2,6 +2,9 @@ package stream.task16_comparator_8;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main4 {
@@ -23,4 +26,16 @@ public class Main4 {
     public static long getCharCount (String s) {
         return s.chars().filter(c->c=='a').count();
     }
+
+    public int practice3 () {
+        return Stream.of(111, 123, 444, 1212)
+                .collect(Collectors.toMap(Function.identity(), Main4::getIntCount))
+                .entrySet().stream().max(Comparator.comparingLong(Map.Entry::getValue)).map(Map.Entry::getKey).orElse(-1);
+    }
+
+    public static long getIntCount(int n) {
+        return String.valueOf(Math.abs(n)).length() - String.valueOf(Math.abs(n)).chars().distinct().count();
+    }
+
+
 }
