78055596ca532d63aad96c7e4112181d05a4b0b3
morozvitali <morozvitali99@gmail.com>
2025-09-09 15:24:11 +0300

    Stream task13 [1 of 11]

---
+++ src/stream/task20_collectingAndThen_11/Main13.java
@@ -0,0 +1,15 @@
+package stream.task20_collectingAndThen_11;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main13 {
+    public List<String> practice1() {
+        return Stream.of("apple", "car", "banana", "hi", "cherry")
+                .collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
+    }
+
+
+}
