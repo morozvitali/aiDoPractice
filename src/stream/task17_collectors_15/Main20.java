86fc546fb55a29198b4c7aab57889f2c694e1619
morozvitali <morozvitali99@gmail.com>
2025-09-09 13:53:29 +0300

    Stream task1 [7 of 15]

--- src/stream/task17_collectors_15/Main20.java
+++ src/stream/task17_collectors_15/Main20.java
@@ -40,7 +40,13 @@ public class Main20 {
         return words.stream().collect(Collectors.groupingBy(w->w.charAt(0), Collectors.joining(", ")));
     }
 
-    public void practice7 () {
-
+    public Map <Integer, Integer> practice7 () {
+        List<Integer> numbers = List.of(1, 2, 3, 4,
+                5, 6, 7, 8, 9);
+        return numbers.stream().collect(Collectors.groupingBy(a->a%3, Collectors.summingInt(n->n)));
     }
+
+
+
+
 }
