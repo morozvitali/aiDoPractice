f16373dd404195711c0ce97610e94ccce99dd3ed
morozvitali <morozvitali99@gmail.com>
2025-09-09 14:00:46 +0300

    Stream task1 [9 of 15]

--- src/stream/task17_collectors_15/Main20.java
+++ src/stream/task17_collectors_15/Main20.java
@@ -1,5 +1,6 @@
 package stream.task17_collectors_15;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -52,7 +53,18 @@ public class Main20 {
         return words.stream().collect(Collectors.reducing("",(a,b)->a.length() > b.length() ? a : b));
     }
 
+    public Map <Character, List <String>> practice9 () {
+        List<String> words = List.of("apple", "ant",
+                "alphabet", "axe", "banana", "bat");
+        return words.stream().collect(Collectors
+                .groupingBy(w->w.charAt(0),
+                        Collectors.collectingAndThen(Collectors
+                                .toList(), list->list
+                                .stream().sorted(Comparator
+                                        .comparingInt(String::length))
+                                .toList())));
 
+    }
 
 
 
