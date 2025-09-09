7a966beab2173796209b2cc24a081f1ec182c27d
morozvitali <morozvitali99@gmail.com>
2025-09-09 15:01:42 +0300

    Stream task18 [6 of 10]

--- src/stream/task19_sorted_10/Main15.java
+++ src/stream/task19_sorted_10/Main15.java
@@ -21,4 +21,9 @@ public List <Integer> practice5 () {
     return numbers.stream().sorted(Comparator.comparingInt(n->(int)n%2).thenComparingInt(n->(int)n)).toList();
 }
 
+public List <String> practice6 () {
+    List<String> words = List.of("kiwi", "apple",
+            "banana", "grape", "plum");
+    return words.stream().filter(w->w.length() >4).sorted(Comparator.comparing(w->w.charAt(w.length()-1))).toList();
+}
 }
