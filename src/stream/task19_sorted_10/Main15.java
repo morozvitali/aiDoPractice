c88f80809db60df32945a83e333a52dd9f48dce9
morozvitali <morozvitali99@gmail.com>
2025-09-09 14:53:59 +0300

    Stream task18 [1 of 10]

---
+++ src/stream/task19_sorted_10/Main15.java
@@ -0,0 +1,18 @@
+package stream.task19_sorted_10;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Main15 {
+    public List<String> practice1 () {
+        List<String> words = List.of("tea", "lemon",
+                "fig", "grape", "banana");
+        return words.stream().filter(w->w.length() > 3)
+                .sorted(Comparator.comparingInt(String::length)).toList();
+    }
+
+
+
+
+
+}
