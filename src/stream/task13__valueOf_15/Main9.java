8f2c7a50261eec53303e5b089222b9d05071ee0c
morozvitali <morozvitali99@gmail.com>
2025-09-10 10:41:39 +0300

    Stream task13 [5 of 16]

--- src/stream/task13__valueOf_15/Main9.java
+++ src/stream/task13__valueOf_15/Main9.java
@@ -3,29 +3,37 @@ package stream.task13__valueOf_15;
 import java.util.List;
 
 public class Main9 {
-    public List <Character> practice1 () {
+    public List<Character> practice1() {
         List<Integer> codes = List.of(97, 98, 99, 100, 101);
-        return codes.stream().map(a->(char)(int)a)
+        return codes.stream().map(a -> (char) (int) a)
                 //.map(String::valueOf)
                 .toList();
     }
 
-    public List <Integer> practice2 () {
+    public List<Integer> practice2() {
         String mixed = "a1b2c3x9z7";
         return mixed.chars().filter(Character::isDigit).mapToObj(Character::getNumericValue)
                 .sorted().toList();
     }
 
-    public List <String> practice3 () {
+    public List<String> practice3() {
         String s = "abracadabra";
-        return s.chars().distinct().mapToObj(c->(char)c)
+        return s.chars().distinct().mapToObj(c -> (char) c)
                 .map(String::valueOf)
                 .toList();
     }
 
-    public List<Integer> practice4 () {
-            String text = "Code";
-            return text.chars().boxed().toList();
-        }
+    public List<Integer> practice4() {
+        String text = "Code";
+        return text.chars().boxed().toList();
+    }
+
+    public List<String> practice5 () {
+        String text = "Code";
+        return text.chars()
+                .mapToObj(c->String.valueOf((char)(int)c))
+                .toList();
+    }
+
 
 }
