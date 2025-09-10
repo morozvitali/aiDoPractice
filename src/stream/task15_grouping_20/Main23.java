76741b92326b9283f0eb53cba1d401ced52ba3eb
morozvitali <morozvitali99@gmail.com>
2025-09-10 14:47:04 +0300

    Stream task15 [3 of 10]

--- src/stream/task15_grouping_20/Main23.java
+++ src/stream/task15_grouping_20/Main23.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main23 {
 
@@ -15,6 +16,11 @@ public class Main23 {
 
     public String practice2 (String s) {
         return Arrays.stream(s.split(" ")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream().max(Comparator.comparing(e->e.getValue())).map(Map.Entry::getKey).orElse("");
+    }
+
+    public Map<Integer, Integer> practice3 () {
+        return Stream.of(12, 23, 34, 45, 16, 7)
+                .collect(Collectors.groupingBy(a->a%10, Collectors.summingInt(n->n)));
     }
 
 
