16af7a20478f863ac55c2f61079e56f601b7c0c6
morozvitali <morozvitali99@gmail.com>
2025-09-10 17:43:10 +0300

    Stream task19 [7 of 10]

--- src/stream/task19_sorted_10/Main16.java
+++ src/stream/task19_sorted_10/Main16.java
@@ -41,4 +41,8 @@ public class Main16 {
         return words.stream().sorted(Comparator.comparing(String::length).thenComparing(Comparator.reverseOrder())).toList();
     }
 
+public List<Integer> practice9 () {
+    List<Integer> numbers = List.of(3, 2, 5, 6, 4, 1);
+    return numbers.stream().sorted(Comparator.comparing(a -> (int) a % 2).reversed()).toList();}
+
 }
