d7c92d00bfc19a7d1231bf3f36285c7f233f31da
morozvitali <morozvitali99@gmail.com>
2025-09-10 14:06:09 +0300

    Stream task14 [6 of 10]

--- src/stream/task14_summarystatistics_10/Main25.java
+++ src/stream/task14_summarystatistics_10/Main25.java
@@ -45,6 +45,11 @@ public class Main25 {
                 .min(Comparator.comparing(String::length)).orElse("");
     }
 
+    public Map <Character, Double> practice6 () {
+        String[] words = {"apple", "ant",
+                "banana", "blue", "berry", "dolphin"};
+        return Arrays.stream(words).collect(Collectors.groupingBy(w->w.charAt(0), Collectors.averagingInt(String::length)));
+    }
 
 
 
