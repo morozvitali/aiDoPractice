77c1905b5f4933be382bf6e2b683f6f32b64b235
morozvitali <morozvitali99@gmail.com>
2025-09-10 14:21:13 +0300

    Stream task14 [8 of 10]

--- src/stream/task14_summarystatistics_10/Main25.java
+++ src/stream/task14_summarystatistics_10/Main25.java
@@ -1,80 +1,74 @@
 package stream.task14_summarystatistics_10;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main25 {
-    public long practice1 () {
+    public long practice1() {
         int[] numbers = {2, 4, 2, 6, 8, 4, 10, 10, 12};
         return Arrays.stream(numbers).distinct().summaryStatistics().getCount();
     }
 
-    public long practice2 () {
+    public long practice2() {
         List<String> words = List.of("APPLE",
                 "Banana", "CHERRY", "kiwi", "PLUM");
-        return words.stream().filter(w->w.length()%2==0)
+        return words.stream().filter(w -> w.length() % 2 == 0)
                 .mapToInt(String::length)
                 .summaryStatistics()
                 .getSum();
     }
 
-    public Map <String, Long> practice3 () {
+    public Map<String, Long> practice3() {
         String[] words = {"sky", "apple",
                 "moon", "dry", "banana"};
-        return Arrays.stream(words).filter(w->w.length() > 3 && getCount(w) > 1).collect(Collectors.toMap(Function.identity(), w->getCount(w)));
+        return Arrays.stream(words).filter(w -> w.length() > 3 && getCount(w) > 1).collect(Collectors.toMap(Function.identity(), w -> getCount(w)));
     }
 
-    public static long getCount (String s) {
-        return s.chars().filter(c->"aeiou".indexOf(c)>=0).count();
+    public static long getCount(String s) {
+        return s.chars().filter(c -> "aeiou".indexOf(c) >= 0).count();
     }
 
-    public Double practice4 () {
+    public Double practice4() {
         String[] words = {"sky", "apple", "moon",
                 "dry", "sun"};
-        return Arrays.stream(words).filter(w->getCount(w) > 1).mapToInt(String::length).summaryStatistics().getAverage();
+        return Arrays.stream(words).filter(w -> getCount(w) > 1).mapToInt(String::length).summaryStatistics().getAverage();
     }
 
-    public String practice5 () {
+    public String practice5() {
         String[] words = {"apple", "orange", "banana",
                 "ice", "umbrella", "echo"};
-        return Arrays.stream(words).filter(w->"aeiou".indexOf(w.charAt(0))>=0)
+        return Arrays.stream(words).filter(w -> "aeiou".indexOf(w.charAt(0)) >= 0)
                 .min(Comparator.comparing(String::length)).orElse("");
     }
 
-    public Map <Character, Double> practice6 () {
+    public Map<Character, Double> practice6() {
         String[] words = {"apple", "ant",
                 "banana", "blue", "berry", "dolphin"};
-        return Arrays.stream(words).collect(Collectors.groupingBy(w->w.charAt(0), Collectors.averagingInt(String::length)));
+        return Arrays.stream(words).collect(Collectors.groupingBy(w -> w.charAt(0), Collectors.averagingInt(String::length)));
     }
 
-    public Long practice7 () {
+    public Long practice7() {
         String[] words = {"apple", "orange",
                 "banana", "umbrella"};
-        return Arrays.stream(words).mapToLong(w->getClear(w)).summaryStatistics().getSum();
+        return Arrays.stream(words).mapToLong(w -> getClear(w)).summaryStatistics().getSum();
     }
 
-    public static long getClear (String s) {
-        return s.chars().filter(c->"aeoiu".indexOf(c)>=0).count();
+    public static long getClear(String s) {
+        return s.chars()
+                .filter(c -> "aeoiu".indexOf(c) >= 0)
+                .count();
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public IntSummaryStatistics practice8() {
+        String[] words = {"apple", "moon",
+                "sky", "banana", "loop"};
+        return Arrays.stream(words)
+                .filter(w -> w.length() - w.chars()
+                        .distinct().count() > 2)
+                .mapToInt(String::length)
+                .summaryStatistics();
+    }
 
 
 }
