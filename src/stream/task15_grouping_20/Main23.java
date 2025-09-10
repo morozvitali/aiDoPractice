f1de21ee62fdd034199ed82aaf5eb14deab73f29
morozvitali <morozvitali99@gmail.com>
2025-09-10 15:01:41 +0300

    Stream task15 [9 of 10]

--- src/stream/task15_grouping_20/Main23.java
+++ src/stream/task15_grouping_20/Main23.java
@@ -2,6 +2,7 @@ package stream.task15_grouping_20;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -37,7 +38,9 @@ public class Main23 {
         return Stream.of(1, 2, 3, 2).collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream().anyMatch(e->e.getValue() >1);
     }
 
-
+    public Map <String, List<Integer>> practice9 () {
+        return Stream.of(1, 2, 3, 4, 5).collect(Collectors.groupingBy(a->a%2==0? "even" : "odd"));
+    }
 
 
 }
