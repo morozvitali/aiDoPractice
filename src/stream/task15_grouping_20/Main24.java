e3c3de25d1f5596d3a553c08375523cbc505b0c8
morozvitali <morozvitali99@gmail.com>
2025-09-11 16:45:56 +0300

    Stream task15 [7 of 20]

--- src/stream/task15_grouping_20/Main24.java
+++ src/stream/task15_grouping_20/Main24.java
@@ -2,6 +2,7 @@ package stream.task15_grouping_20;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -35,4 +36,7 @@ public class Main24 {
         return Stream.of(1, 2, 3, 2).collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream().filter(e->e.getValue() > 1).count();
     }
 
+    public Map<String, List<Integer>> practice7 () {
+        return Stream.of(1, 2, 3, 4, 5).collect(Collectors.groupingBy(a->a%2==0 ? "even": " odd"));
+    }
 }
