4a7d5c71675e8f94ec9bb4ff48beccf55dda133f
morozvitali <morozvitali99@gmail.com>
2025-09-11 14:17:40 +0300

    Stream task12 [2 of 10]

--- src/stream/task12_plus_valueOf_10/Main19.java
+++ src/stream/task12_plus_valueOf_10/Main19.java
@@ -5,10 +5,19 @@ import java.util.stream.Collectors;
 public class Main19 {
     public int practice1(int n) {
         return Integer.parseInt(String.valueOf(Math.abs(n)).chars().map(Character::getNumericValue)
-                .map(a->a*a)
+                .map(a -> a * a)
                 .mapToObj(String::valueOf)
                 .collect(Collectors.joining()));
     }
+
+    public int practice2(int n) {
+        return Integer.parseInt(String.valueOf(Math.abs(n)).chars().map(Character::getNumericValue)
+                .map(a -> a * a * a)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining()));
+    }
+
+
 
 
 
