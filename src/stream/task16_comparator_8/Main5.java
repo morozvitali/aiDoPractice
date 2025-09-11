dd289987e622c212d56452c11bed2c3f33c0cd97
morozvitali <morozvitali99@gmail.com>
2025-09-11 17:01:18 +0300

    Stream task16 [1 of 10]

--- src/stream/task16_comparator_8/Main5.java
+++ src/stream/task16_comparator_8/Main5.java
@@ -2,6 +2,10 @@ package stream.task16_comparator_8;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main5 {
     public List <String> practice1 () {
@@ -10,6 +14,12 @@ public class Main5 {
         return words.stream().sorted(Comparator.comparing(w->w.chars().filter(c->"aeiou".indexOf(c)>=0).count())).toList();
     }
 
+    public String practice2 () {
+        return Stream.of("alpha", "arena", "java", "banana", "lava").collect(Collectors.toMap(Function.identity(), Main5::counter)).entrySet().stream().max(Comparator.comparing(Map.Entry::getValue)).map(Map.Entry::getKey).orElse("");
+    }
 
+    public static long counter (String s) {
+        return s.chars().filter (c->c=='a').count();
+    }
 
 }
