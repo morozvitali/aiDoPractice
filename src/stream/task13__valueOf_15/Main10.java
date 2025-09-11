7c3079cb6c91a9cd837a9b0ddd7a6e89864ec544
morozvitali <morozvitali99@gmail.com>
2025-09-11 14:45:58 +0300

    Stream task13 [5 of 15]

--- src/stream/task13__valueOf_15/Main10.java
+++ src/stream/task13__valueOf_15/Main10.java
@@ -24,4 +24,12 @@ public class Main10 {
         return text.chars().boxed().toList();
     }
 
+    public List<String> practice5 () {
+        String input = "hEllO WoRLd";
+        return input.chars().filter(Character::isUpperCase)
+                .mapToObj(String::valueOf)
+                .toList();
+    }
+
+
 }
