8af3c08ce81611fda0f6391961eeeeaba1489ee0
morozvitali <morozvitali99@gmail.com>
2025-09-11 12:31:03 +0300

    Stream task10 [3 of 10]

--- src/stream/task10_list_to_map_to_entry/Main7.java
+++ src/stream/task10_list_to_map_to_entry/Main7.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main7 {
     public String practice1() {
@@ -30,5 +31,23 @@ public class Main7 {
     public static long getCounter (String s) {
         return s.chars().filter(c->"aeiou".indexOf(c)>=0).count();
     }
+
+    public String practice4 () {
+        return Stream.of("apple", "pear",
+                "banana", "kiwi").collect(Collectors.toMap(Function.identity(), w->w.chars().distinct().count()))
+                .entrySet().stream().max(Comparator.comparing(e->e.getValue())).map(Map.Entry::getKey).orElse("");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
