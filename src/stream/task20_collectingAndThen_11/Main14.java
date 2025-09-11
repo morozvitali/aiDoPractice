0a0d4623225611bbe489dc18064826f610164c85
morozvitali <morozvitali99@gmail.com>
2025-09-11 10:11:20 +0300

    Stream task20 [3 of 11]

--- src/stream/task20_collectingAndThen_11/Main14.java
+++ src/stream/task20_collectingAndThen_11/Main14.java
@@ -1,6 +1,7 @@
 package stream.task20_collectingAndThen_11;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -15,6 +16,12 @@ public class Main14 {
     public Integer practice2() {
         return Stream.of(3, 9, 2, 4, 6, 7, 12)
                 .collect(Collectors.collectingAndThen(Collectors.toList(), List::size));
+    }
+
+    public String practice3 () {
+        return Stream.of("sun", "supernova", "star")
+                .sorted(Comparator.comparing(String::length).reversed())
+                .collect(Collectors.collectingAndThen(Collectors.toList(), list->list.get(0)));
     }
 
 
