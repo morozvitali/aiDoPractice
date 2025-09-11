1be68aba565755a469589af4bfc0bb5dbde21893
morozvitali <morozvitali99@gmail.com>
2025-09-11 14:37:56 +0300

    Stream task13 [2 of 15]

--- src/stream/task13__valueOf_15/Main10.java
+++ src/stream/task13__valueOf_15/Main10.java
@@ -8,4 +8,11 @@ public class Main10 {
         return Stream.of(97, 98, 99, 100, 101).map(a->(char)(int)a).map(String::valueOf).toList();
     }
 
+    public List<Integer> practice2 () {
+        String mixed = "a1b2c3x9z7";
+        return mixed.chars().filter(Character::isDigit).map(Character::getNumericValue).sorted().boxed().toList();
+    }
+
+
+
 }
