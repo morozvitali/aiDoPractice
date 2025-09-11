7ca9f568a936c490d8c5ad7601f9467be31c873a
morozvitali <morozvitali99@gmail.com>
2025-09-11 14:21:14 +0300

    Stream task12 [5 of 10]

--- src/stream/task12_plus_valueOf_10/Main19.java
+++ src/stream/task12_plus_valueOf_10/Main19.java
@@ -30,5 +30,12 @@ public class Main19 {
                 .collect(Collectors.joining());
     }
 
+    public int practice5 (int n) {
+        return Integer.parseInt(new StringBuilder(String.valueOf(Math.abs(n))).reverse().toString());
+    }
+
+
+
+
 
 }
