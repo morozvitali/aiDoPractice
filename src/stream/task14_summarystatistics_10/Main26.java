2cd9830b73843d11bddd371aa82d799d15d3cbf2
morozvitali <morozvitali99@gmail.com>
2025-09-11 15:20:06 +0300

    Stream task13 [6 of 10]

--- src/stream/task14_summarystatistics_10/Main26.java
+++ src/stream/task14_summarystatistics_10/Main26.java
@@ -38,7 +38,11 @@ public String practice5 () {
         return Arrays.stream(words).filter(w->"aeiou".indexOf(w.charAt(0))>=0).min(Comparator.comparing(String::length)).orElse("");
 }
 
-
+public Map<Character, Double> practice6 () {
+    String[] words = {"apple", "ant",
+            "banana", "blue", "berry", "dolphin"};
+    return Arrays.stream(words).collect(Collectors.groupingBy(w->w.charAt(0), Collectors.averagingInt(String::length)));
+}
 
 
 
