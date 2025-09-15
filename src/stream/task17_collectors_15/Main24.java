dac83941ae19667c579d9245d943832174863284
morozvitali <morozvitali99@gmail.com>
2025-09-15 17:40:34 +0300

    Stream task17 [7 of 15]

--- src/stream/task17_collectors_15/Main24.java
+++ src/stream/task17_collectors_15/Main24.java
@@ -45,5 +45,11 @@ public class Main24 {
         return words.stream().collect(Collectors.groupingBy(w->w.charAt(0), Collectors.joining(", ")));
     }
 
+    public Map<Integer, Integer> practice7 () {
+        List<Integer> numbers = List.of(1, 2, 3, 4,
+                5, 6, 7, 8, 9);
+        return numbers.stream().collect(Collectors.groupingBy(w->w%3, Collectors.summingInt(n->n)));
+    }
+
 
 }
