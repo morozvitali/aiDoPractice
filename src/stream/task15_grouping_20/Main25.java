0b471247ef62ccb656d5fb7efea732cf72f53d0e
morozvitali <morozvitali99@gmail.com>
2025-09-15 16:04:42 +0300

    Stream task15 [2 of 20]

--- src/stream/task15_grouping_20/Main25.java
+++ src/stream/task15_grouping_20/Main25.java
@@ -1,5 +1,8 @@
 package stream.task15_grouping_20;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -9,6 +12,10 @@ public class Main25 {
                 .entrySet().stream().filter(e->e.getValue() >1).count();
     }
 
+    public String practice2 (String s) {
+        return Arrays.stream(s.split(" ")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream().max(Comparator.comparingLong(Map.Entry::getValue)).map(Map.Entry::getKey).orElse(" ");
+    }
 
 
 }
