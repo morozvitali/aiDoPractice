5880d9e887b2c303372d4927d365970a59be9656
morozvitali <morozvitali99@gmail.com>
2025-09-15 16:07:32 +0300

    Stream task15 [3 of 20]

--- src/stream/task15_grouping_20/Main25.java
+++ src/stream/task15_grouping_20/Main25.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main25 {
     public Long practice1 (String s) {
@@ -15,6 +16,10 @@ public class Main25 {
     public String practice2 (String s) {
         return Arrays.stream(s.split(" ")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet().stream().max(Comparator.comparingLong(Map.Entry::getValue)).map(Map.Entry::getKey).orElse(" ");
+    }
+
+    public Map <Integer, Integer> practice3 () {
+        return Stream.of(12, 23, 34, 45, 16, 7).collect(Collectors.groupingBy(a->a%10, Collectors.summingInt(n->n)));
     }
 
 
