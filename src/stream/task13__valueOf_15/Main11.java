d6fab96ab5d5cad5e8af37447c24b60415d5de3d
morozvitali <morozvitali99@gmail.com>
2025-09-15 13:53:26 +0300

    Stream task13 [6 of 15]

--- src/stream/task13__valueOf_15/Main11.java
+++ src/stream/task13__valueOf_15/Main11.java
@@ -28,4 +28,11 @@ public class Main11 {
         String input = "hEllO WoRLd";
         return input.chars().filter(Character::isUpperCase).mapToObj(String::valueOf).toList();
     }
+
+    public List <Integer> practice6 () {
+        String s = "abcxyz";
+        return s.chars().mapToObj(c->c-'a'+1).toList();
+    }
+
+
 }
