d5bd018adc1b103c7fa715479a40ad80fe060b4a
morozvitali <morozvitali99@gmail.com>
2025-09-15 12:22:30 +0300

    Stream task12 [2 of 10]

---
+++ src/stream/task12_plus_valueOf_10/Main20.java
@@ -0,0 +1,26 @@
+package stream.task12_plus_valueOf_10;
+
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.IntStream;
+
+public class Main20 {
+
+    public void practice1() {
+        List<String> inputs = List.of("1010", "7B", "377", "42");
+        List<Integer> bases = List.of(2, 16, 8, 10);
+
+        List<Long> result = IntStream.range(0, inputs.size())
+                .mapToObj(i -> Long.parseLong(inputs.get(i), bases.get(i)))
+                .toList();
+    }
+
+    public void practice2 () {
+        long balance = 123456789L;
+        NumberFormat nf = NumberFormat.getInstance(Locale.ENGLISH);
+        String formatted = nf.format(balance);
+    }
+
+
+}
