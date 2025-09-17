71a06973ae4c2b13e0494b947c7a3d91b37cad16
morozvitali <morozvitali99@gmail.com>
2025-09-17 09:33:08 +0300

    task 15 1-10

---
+++ src/stream/task15_grouping_20/Main26.java
@@ -0,0 +1,15 @@
+package stream.task15_grouping_20;
+
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Main26 {
+
+    public long practice1 (String s) {
+        return s.chars().mapToObj(c->(char)c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream().filter(e->e.getValue() >1).count();
+    }
+
+
+}
