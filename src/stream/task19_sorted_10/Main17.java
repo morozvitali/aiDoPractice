8e7c7223ea08edd5e0b543b9624f53b2d6b5edf1
morozvitali <morozvitali99@gmail.com>
2025-09-17 15:55:01 +0300

    task 19 2-10

--- src/stream/task19_sorted_10/Main17.java
+++ src/stream/task19_sorted_10/Main17.java
@@ -10,4 +10,11 @@ public class Main17 {
                 "fig", "grape", "banana");
         return words.stream().filter(w->w.length() > 3).sorted(Comparator.comparingInt(String::length)).toList();
     }
+
+    public List <Integer> practice2 () {
+        List<Integer> numbers = List.of(3, 7, 1, 9, 2);
+        return numbers.stream().sorted(Comparator.reverseOrder()).toList();
+    }
+
+
 }
