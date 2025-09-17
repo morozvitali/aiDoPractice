1335820ecbb4d7af87a0db2ec57bb37f0413fbfa
morozvitali <morozvitali99@gmail.com>
2025-09-17 12:20:07 +0300

    task 17 3-10

--- src/stream/task17_collectors_15/Main25.java
+++ src/stream/task17_collectors_15/Main25.java
@@ -17,5 +17,11 @@ public class Main25 {
         return words.stream().collect(Collectors.groupingBy(w->w.charAt(0), Collectors.mapping(String::length, Collectors.toList())));
     }
 
+    public Map <Boolean, Long> practice3 () {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        return numbers.stream().collect(Collectors.partitioningBy(a->a%2==0, Collectors.counting()));
+    }
+
+
 
 }
