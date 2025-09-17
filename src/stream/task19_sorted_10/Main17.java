4ef1166578a539399cbeec7034e856fcdd0373d5
morozvitali <morozvitali99@gmail.com>
2025-09-17 16:08:36 +0300

    task 19 7-10

--- src/stream/task19_sorted_10/Main17.java
+++ src/stream/task19_sorted_10/Main17.java
@@ -38,7 +38,14 @@ public class Main17 {
         List<String> words = List.of("tea", "watermelon",
                 "apple", "pear", "banana");
         return words.stream().sorted().sorted(Comparator.comparingInt(String::length).reversed()).toList();
-
     }
+
+
+    public List<Integer> practice7 () {
+        List<Integer> numbers = List.of(3, 2, 5, 6, 4, 1);
+        return numbers.stream().sorted(Comparator.comparingInt(n->(int)n%2).reversed()).toList();
+    }
+
+
 
 }
