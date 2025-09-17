fcfcf83c0ab099092159f0227e978ec5152e6235
morozvitali <morozvitali99@gmail.com>
2025-09-17 11:22:11 +0300

    task 16 2-10

--- src/stream/task16_comparator_8/Main7.java
+++ src/stream/task16_comparator_8/Main7.java
@@ -2,6 +2,10 @@ package stream.task16_comparator_8;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main7 {
 
@@ -13,6 +17,15 @@ public class Main7 {
 
     public static long getCount(String s) {
         return s.chars().filter(c-> "aeiuo".indexOf(c)>=0).count();
+    }
+
+    public String practice2 () {
+        return Stream.of("alpha", "arena", "java", "banana", "lava")
+                .collect(Collectors.toMap(Function.identity(), Main7::getLetter)).entrySet().stream().max(Comparator.comparingLong(Map.Entry::getValue)).map(Map.Entry::getKey).orElse("");
+    }
+
+    public static long getLetter (String s) {
+        return s.chars().filter(c->c=='a').count();
     }
 
 
