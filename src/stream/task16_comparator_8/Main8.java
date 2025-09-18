6192dd456170ac265e363e6d9746da76056d5466
morozvitali <morozvitali99@gmail.com>
2025-09-18 13:57:17 +0300

    task 16 2-8

--- src/stream/task16_comparator_8/Main8.java
+++ src/stream/task16_comparator_8/Main8.java
@@ -2,6 +2,10 @@ package stream.task16_comparator_8;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main8 {
 
@@ -13,6 +17,14 @@ public class Main8 {
     }
 
     public static long countVowels (String s) {
-        s.chars().filter(c-> "aeiou".indexOf(c)>=0).count();
+        return s.chars().filter(c-> "aeiou".indexOf(c)>=0).count();
+    }
+
+    public String practice2 () {
+        return Stream.of("alpha", "arena", "java", "banana", "lava").collect(Collectors.toMap(Function.identity(), w->countLetter(w))).entrySet().stream().max(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse("");
+    }
+
+    public long countLetter (String s) {
+        return s.chars().filter(a->a=='a').count();
     }
 }
