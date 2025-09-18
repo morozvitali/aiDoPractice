c9f8f7471b2eaee4fd960231b209e53e6c4fae27
morozvitali <morozvitali99@gmail.com>
2025-09-18 15:36:17 +0300

    task 17 3-15

--- src/stream/task17_collectors_15/Main26.java
+++ src/stream/task17_collectors_15/Main26.java
@@ -5,10 +5,22 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main26 {
-    public Map<Character, List<String>> practice() {
+    public Map<Character, List<String>> practice1() {
         List<String> words = List.of("apple",
                 "ant", "banana", "bat", "car");
         return words.stream().collect(Collectors.groupingBy(w->w.charAt(0), Collectors.mapping(w->w.toUpperCase(), Collectors.toList())));
     }
+
+    public Map<Character, List<Integer>> practice2 () {
+        List<String> words = List.of("apple", "ant",
+                "banana", "bat", "car");
+        return words.stream().collect(Collectors.groupingBy(w->w.charAt(0), Collectors.mapping(String::length, Collectors.toList())));
+    }
+
+    public Map<Boolean, Long> practice3 () {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        return numbers.stream().collect(Collectors.partitioningBy(a->a%2==0, Collectors.counting()));
+    }
+
 
 }
