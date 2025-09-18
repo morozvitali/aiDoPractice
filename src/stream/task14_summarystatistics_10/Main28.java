91d88a37be443a13c960c2b453b142c12a6d0267
morozvitali <morozvitali99@gmail.com>
2025-09-18 11:37:08 +0300

    task 14 7-10

--- src/stream/task14_summarystatistics_10/Main28.java
+++ src/stream/task14_summarystatistics_10/Main28.java
@@ -48,4 +48,12 @@ public class Main28 {
         return Arrays.stream(words).collect(Collectors.groupingBy(w->w.charAt(0), Collectors.averagingInt(String::length)));
     }
 
+    public Map <Character, Long> practice7 () {
+        String[] words = {"apple", "orange",
+                "banana", "umbrella"};
+        return Arrays.stream(words).flatMap(w->w.chars().mapToObj(c->(char)c).filter(c->"aeiou".indexOf(c)>=0))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
+
 }
