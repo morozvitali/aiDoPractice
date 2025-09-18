03290851fe062417965238eb7840355d40c489d4
morozvitali <morozvitali99@gmail.com>
2025-09-18 17:08:29 +0300

    task 20 1-10

---
+++ src/stream/task20_collectingAndThen_11/Main16.java
@@ -0,0 +1,13 @@
+package stream.task20_collectingAndThen_11;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main16 {
+    public List<String> practice1 () {
+        return Stream.of("apple", "car", "banana",
+                "hi", "cherry").collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
+    }
+}
