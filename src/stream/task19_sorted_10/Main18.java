e0c5ee1ac8d23a2cd568f83bdff980e2c49dc5ae
morozvitali <morozvitali99@gmail.com>
2025-09-18 16:59:24 +0300

    task 19 7-10

--- src/stream/task19_sorted_10/Main18.java
+++ src/stream/task19_sorted_10/Main18.java
@@ -34,6 +34,15 @@ public List <String> practice6 () {
     return words.stream().sorted(Comparator.comparing(w->w.charAt(w.length()-1))).toList();
 }
 
+public List <String> practice7 () {
+    List<String> words = List.of("apple",
+            "banana", "cherry", "kiwi", "grape");
+    return words.stream().sorted(Comparator.comparing(w->getCount(w))).toList();
+}
+
+    public long getCount (String s) {
+        return s.chars().filter(c->"aeiou".indexOf(c)>=0).count();
+    }
 
 
 
