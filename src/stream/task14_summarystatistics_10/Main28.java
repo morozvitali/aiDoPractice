b7238fb83e9ef101278f7232f597cbf3d1fa0a69
morozvitali <morozvitali99@gmail.com>
2025-09-18 11:27:58 +0300

    task 14 5-10

--- src/stream/task14_summarystatistics_10/Main28.java
+++ src/stream/task14_summarystatistics_10/Main28.java
@@ -1,6 +1,7 @@
 package stream.task14_summarystatistics_10;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -34,5 +35,13 @@ public class Main28 {
                 "dry", "sun"};
         return Arrays.stream(words).filter(w->w.matches(".*[aeiou].*")).mapToInt(String::length).summaryStatistics().getAverage();
     }
+
+    public String practice5 () {
+        String[] words = {"apple", "orange", "banana",
+                "ice", "umbrella", "echo"};
+        return Arrays.stream(words).filter(w->w.matches("(?i)^[aeiou].*")).min(Comparator.comparing(String::length)).orElse("");
+    }
+
+
 
 }
