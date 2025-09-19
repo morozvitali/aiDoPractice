02172cad4f1fb2b4ef3f5613f2493370d4392558
morozvitali <morozvitali99@gmail.com>
2025-09-19 15:53:08 +0300

    task 14 8-10

--- src/stream/task14_summarystatistics_10/Main29.java
+++ src/stream/task14_summarystatistics_10/Main29.java
@@ -1,9 +1,6 @@
 package stream.task14_summarystatistics_10;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -52,6 +49,12 @@ public class Main29 {
         String[] words = {"apple", "orange",
                 "banana", "umbrella"};
         return Arrays.stream(words).flatMap(w->w.chars().mapToObj(c->(char)c)).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
+    public IntSummaryStatistics practice8 () {
+        String[] words = {"apple", "moon",
+                "sky", "banana", "loop"};
+        return Arrays.stream(words).mapToInt(String::length).summaryStatistics();
     }
 
 
