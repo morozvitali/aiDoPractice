5e6a94ea40807e90d02761ef4a115949351fb5d1
morozvitali <morozvitali99@gmail.com>
2025-09-19 17:45:04 +0300

    task 167 4-10

--- src/stream/task17_collectors_15/Main27.java
+++ src/stream/task17_collectors_15/Main27.java
@@ -2,6 +2,7 @@ package stream.task17_collectors_15;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Main27 {
@@ -20,5 +21,11 @@ public class Main27 {
     public Map<Boolean, Long> practice3 () {
         List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
         return numbers.stream().collect(Collectors.partitioningBy(a->a%2==0, Collectors.counting()));
+    }
+
+    public Map<Integer, Set<String>> practice4 () {
+        List<String> words = List.of("hi", "hi",
+                "book", "sun", "day", "Java", "sky");
+        return words.stream().collect(Collectors.groupingBy(String::length, Collectors.toSet()));
     }
 }
