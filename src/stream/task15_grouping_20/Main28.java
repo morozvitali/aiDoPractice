293454edf85c39adcaf9ef6878bbfeab074addec
morozvitali <morozvitali99@gmail.com>
2025-09-19 16:35:04 +0300

    task 15 3-10

--- src/stream/task15_grouping_20/Main28.java
+++ src/stream/task15_grouping_20/Main28.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main28 {
     public Long practice1 (String s) {
@@ -15,6 +16,11 @@ public class Main28 {
         return Arrays.stream("java is fun and java is powerful".split(" ")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet().stream().max(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse("");
     }
+
+    public Map <Integer, Integer> practice3 () {
+        return Stream.of(12, 23, 34, 45, 16, 7).collect(Collectors.groupingBy(a->a%10, Collectors.summingInt(n->n)));
+    }
+
 
 
 }
