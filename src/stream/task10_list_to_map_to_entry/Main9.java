4e8efef7f4abfcf19eff56123be2fb1d11c1b591
morozvitali <morozvitali99@gmail.com>
2025-09-19 12:01:46 +0300

    task 10 4-10

--- src/stream/task10_list_to_map_to_entry/Main9.java
+++ src/stream/task10_list_to_map_to_entry/Main9.java
@@ -2,6 +2,7 @@ package stream.task10_list_to_map_to_entry;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -31,6 +32,22 @@ public class Main9 {
 
     public static long getCount (String s) {
         return s.chars().filter(c->"aeiou".indexOf(c)>=0).count();
+    }
+
+    public String practice4 () {
+        List<String> list = List.of("apple", "pear",
+                "banana", "kiwi");
+
+        return list.stream().collect(Collectors.toMap(Function.identity(), w->getUniqLetter(w)))
+                .entrySet().stream().max(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse("");
+    }
+
+    public static int getUniqLetter (String s) {
+        Set<Character> set = null;
+
+        s.chars().forEach(c->set.add((char) c));
+        assert set != null;
+        return set.size();
     }
 
 
