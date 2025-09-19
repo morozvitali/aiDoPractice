a9839492c17904b60718c9ea18945cd8f3b8f94a
morozvitali <morozvitali99@gmail.com>
2025-09-19 15:10:27 +0300

    task 13 2-10

--- src/stream/task13__valueOf_15/Main12.java
+++ src/stream/task13__valueOf_15/Main12.java
@@ -1,14 +1,20 @@
 package stream.task13__valueOf_15;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main12 {
     public List <String> practice1 () {
         List<Integer> codes = List.of(97, 98, 99, 100, 101);
         return codes.stream().map(c->(char)(int)c).map(String::valueOf).toList();
-
-
-
     }
+
+    public List <Integer> practice2 () {
+        String mixed = "a1b2c3x9z7";
+        return mixed.chars().filter(Character::isDigit).map(Character::getNumericValue).boxed().toList();
+    }
+
+
+
 
 }
