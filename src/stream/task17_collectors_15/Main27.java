4f734897fcb605965c798ff6d2fbfd1caa96842e
morozvitali <morozvitali99@gmail.com>
2025-09-19 17:42:35 +0300

    task 167 3-10

--- src/stream/task17_collectors_15/Main27.java
+++ src/stream/task17_collectors_15/Main27.java
@@ -16,4 +16,9 @@ public class Main27 {
                 "banana", "bat", "car");
         return words.stream().collect(Collectors.groupingBy(w->w.charAt(0), Collectors.mapping(String::length, Collectors.toList())));
     }
+
+    public Map<Boolean, Long> practice3 () {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        return numbers.stream().collect(Collectors.partitioningBy(a->a%2==0, Collectors.counting()));
+    }
 }
