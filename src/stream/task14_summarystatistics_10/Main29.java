498a8609156dba5caa2f630ec097b59a5147e1f3
morozvitali <morozvitali99@gmail.com>
2025-09-19 15:49:28 +0300

    task 14 6-10

--- src/stream/task14_summarystatistics_10/Main29.java
+++ src/stream/task14_summarystatistics_10/Main29.java
@@ -42,5 +42,11 @@ public class Main29 {
         return Arrays.stream(words).filter(w->Character.isUpperCase(w.charAt(0))).min(Comparator.comparing(String::length)).orElse("");
     }
 
+    public Map <Character, Double> pracvtice6 () {
+        String[] words = {"apple", "ant",
+                "banana", "blue", "berry", "dolphin"};
+        return Arrays.stream(words).collect(Collectors.groupingBy(w->w.charAt(0), Collectors.averagingInt(String::length)));
+    }
+
 
 }
