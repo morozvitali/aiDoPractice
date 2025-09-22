ed7ddb666c9f4e1a0e64e2b7cf95bf938691e58b
morozvitali <morozvitali99@gmail.com>
2025-09-22 11:25:05 +0300

    task 10 1-10

---
+++ src/stream/task10_list_to_map_to_entry/Main10.java
@@ -0,0 +1,18 @@
+package stream.task10_list_to_map_to_entry;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Main10 {
+    public String practice1() {
+        List<String> words = List.of("stream", "code",
+                "developer", "Engineer");
+        return words.stream().collect(Collectors.toMap(Function.identity(), String::length))
+                .entrySet().stream().max(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse("");
+    }
+
+
+
+}
