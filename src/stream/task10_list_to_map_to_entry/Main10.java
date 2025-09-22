f2095fdb71b4e5624ed52139b29ac496e9c0b5f9
morozvitali <morozvitali99@gmail.com>
2025-09-22 11:35:24 +0300

    task 10 3-10

--- src/stream/task10_list_to_map_to_entry/Main10.java
+++ src/stream/task10_list_to_map_to_entry/Main10.java
@@ -1,5 +1,6 @@
 package stream.task10_list_to_map_to_entry;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -13,6 +14,22 @@ public class Main10 {
                 .entrySet().stream().max(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse("");
     }
 
+    public String practice2() {
+        List<String> list = List.of("Java", "C",
+                "Python", "Go", "Kotlin");
+        return list.stream().collect(Collectors.toMap(Function.identity(), String::length))
+                .entrySet().stream().min(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse("");
+    }
 
+    public String practice3 () {
+        List<String> list = List.of("apple", "banana",
+                "orange", "blueberry");
+        return list.stream().collect(Collectors.toMap(Function.identity(), w->getCount(w)))
+                .entrySet().stream().reduce((a,b)->a.getValue() > b.getValue() ? a: b)
+                .map(Map.Entry::getKey).orElse("");
+    }
 
+    public static long getCount (String s) {
+        return s.chars().filter(c->"aeiou".indexOf(c)>=0).count();
+    }
 }
