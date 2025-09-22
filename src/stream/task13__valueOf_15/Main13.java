782772f42681347d1df6f77f67b51966afcd200e
morozvitali <morozvitali99@gmail.com>
2025-09-22 13:05:17 +0300

    task 13 9-15

--- src/stream/task13__valueOf_15/Main13.java
+++ src/stream/task13__valueOf_15/Main13.java
@@ -6,44 +6,51 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main13 {
-    public List <Character> practice1 () {
+    public List<Character> practice1() {
         List<Integer> codes = List.of(97, 98, 99, 100, 101);
         return codes.stream().
-                map(c->(char)(int)c).toList();
+                map(c -> (char) (int) c).toList();
     }
 
-    public List <Integer> practice2 () {
+    public List<Integer> practice2() {
         String mixed = "a1b2c3x9z7";
         return mixed.chars().filter(Character::isDigit).mapToObj(Character::getNumericValue).sorted().toList();
     }
 
-    public List <String> practice3 () {
+    public List<String> practice3() {
         String s = "abracadabra";
         return s.chars().distinct().mapToObj(String::valueOf).toList();
     }
 
-    public List <Integer> practice4 () {
+    public List<Integer> practice4() {
         String text = "Code";
         return text.chars().boxed().toList();
     }
 
-    public List <String> practice5 () {
+    public List<String> practice5() {
         String input = "hEllO WoRLd";
         return input.chars().filter(Character::isUpperCase).mapToObj(String::valueOf).toList();
     }
 
-    public List <Integer> practice6 () {
+    public List<Integer> practice6() {
         String s = "abcxyz";
-        return s.chars().map(c->c-'a'+1).boxed().toList();
+        return s.chars().map(c -> c - 'a' + 1).boxed().toList();
     }
 
-    public List <String> practice7 () {
+    public List<String> practice7() {
         String s = "java";
         return s.chars().map(Character::toUpperCase).mapToObj(String::valueOf).toList();
     }
 
-    public Map<String, Long> practice8 () {
+    public Map<String, Long> practice8() {
         String s = "banana";
-        return s.chars().mapToObj(String::valueOf).collect(Collectors.groupingBy(w->w.toUpperCase(), Collectors.counting()));
+        return s.chars().mapToObj(String::valueOf).collect(Collectors.groupingBy(w -> w.toUpperCase(), Collectors.counting()));
     }
+
+    public String practce9() {
+        String s = "a1b2c3d4";
+        return s.chars().filter(c->!Character.isDigit(c)).mapToObj(String::valueOf).collect(Collectors.joining());
+    }
+
 }
+
