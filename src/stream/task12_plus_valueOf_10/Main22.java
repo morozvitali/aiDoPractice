973c6bde09b9cee2dfc0f4618e5ac7e3bcf8dd9c
morozvitali <morozvitali99@gmail.com>
2025-09-22 12:09:17 +0300

    task 12 3-10

--- src/stream/task12_plus_valueOf_10/Main22.java
+++ src/stream/task12_plus_valueOf_10/Main22.java
@@ -11,5 +11,9 @@ public class Main22 {
             return Integer.parseInt(String.valueOf(Math.abs(n)).chars().map(Character::getNumericValue).map(a->a*a*a).mapToObj(String::valueOf).collect(Collectors.joining()));
 }
 
+    public int practice3 (int n) {
+        return Integer.parseInt(String.valueOf(Math.abs(n)).chars().map(Character::getNumericValue).filter(a->a%2==0).mapToObj(String::valueOf).collect(Collectors.joining()));
+    }
+
 
 }
