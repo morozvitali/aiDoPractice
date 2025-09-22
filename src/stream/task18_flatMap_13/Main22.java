e73b7539e19aaa0c5252e40281457e4791193e18
morozvitali <morozvitali99@gmail.com>
2025-09-22 09:44:31 +0300

    task 18 7-10

--- src/stream/task18_flatMap_13/Main22.java
+++ src/stream/task18_flatMap_13/Main22.java
@@ -58,5 +58,18 @@ public class Main22 {
         return Arrays.stream(data).flatMapToInt(Arrays::stream).filter(n->n%2==0).summaryStatistics().getAverage();
     }
 
+    public int practice7 () {
+        int[][] data = {
+                {12},
+                {24, 36}
+        };
+        return Arrays.stream(data).flatMapToInt(Arrays::stream).reduce(Main22::gcd).orElse(-1);
+    }
+
+    public static int gcd (int a, int b) {
+        return b==0? a : gcd (b, a%b);
+    }
+
+
 
 }
