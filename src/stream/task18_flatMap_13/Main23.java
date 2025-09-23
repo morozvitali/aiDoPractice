24fbde7eeff778efdf4947f80114e60c20767db7
morozvitali <morozvitali99@gmail.com>
2025-09-23 13:11:25 +0300

    task 18 3-10

--- src/stream/task18_flatMap_13/Main23.java
+++ src/stream/task18_flatMap_13/Main23.java
@@ -22,6 +22,14 @@ public class Main23 {
         return Arrays.stream(data).flatMapToInt(Arrays::stream).min().orElse(-1);
     }
 
+    public  int practice3 () {
+        int[][] data = {
+                {-3, 5},
+                {2},
+                {-1, 4}
+        };
+        return Arrays.stream(data).flatMapToInt(Arrays::stream).filter(a->a>0).reduce((a,b)->a*b).orElse(-1);
+    }
 
 
 }
