64120e24f7ba3a5e307d54dc7a740d0e6a5b1a09
morozvitali <morozvitali99@gmail.com>
2025-09-23 14:28:24 +0300

    task 13 1-15

--- src/stream/task13__valueOf_15/Main14.java
+++ src/stream/task13__valueOf_15/Main14.java
@@ -8,4 +8,9 @@ public class Main14 {
         return codes.stream().map(c->String.valueOf((char)(int)c)).toList();
     }
 
+    public List <Integer> practice2 () {
+        String mixed = "a1b2c3x9z7";
+        return mixed.chars().filter(Character::isDigit).map(Character::getNumericValue).sorted().boxed().toList();
+    }
+
 }
