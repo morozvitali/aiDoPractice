d11202e9f9f8fb676cb50c2601bcdbf2d1b5d50d
morozvitali <morozvitali99@gmail.com>
2025-09-23 13:07:57 +0300

    task 18 1-10

---
+++ src/stream/task18_flatMap_13/Main23.java
@@ -0,0 +1,19 @@
+package stream.task18_flatMap_13;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main23 {
+    public List <String> practice1 () {
+        List<String> sentences = List.of(
+                "Java is powerful",
+                "Streams are cool",
+                "Java makes sense"
+        );
+        return sentences.stream().flatMap(s->Arrays.stream(s.split(" "))).distinct().toList();
+    }
+
+
+
+
+}
