c3f7ce828a5fb4996bec05244ea812e62bd226ae
morozvitali <morozvitali99@gmail.com>
2025-09-23 10:57:58 +0300

    task 15 2-10

--- src/stream/task15_grouping_20/Main29.java
+++ src/stream/task15_grouping_20/Main29.java
@@ -6,13 +6,19 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main29 {
-    public Map<String, Boolean> practice1 () {
+    public Map<String, Boolean> practice0 () {
         String[] words = {"sky", "apple",
                 "dry", "orange", "sun"};
         return Arrays.stream(words).collect(Collectors.toMap(Function.identity(), w->count(w)>0));
     }
     public static long count (String s) {
         return s.chars().filter(c->"aeiou".indexOf(c)>=0).count();
+    }
+
+    public Long practice1 (String s) {
+        return s.chars().mapToObj(c->(char)c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream().filter(e->e.getValue() >0).count();
     }
 
 
