54bb363320796d53f4412e274ab0ece3eb55316f
morozvitali <morozvitali99@gmail.com>
2025-09-24 10:18:13 +0300

    task 13 1-15

---
+++ src/stream/task13__valueOf_15/Main15.java
@@ -0,0 +1,12 @@
+package stream.task13__valueOf_15;
+
+import java.util.List;
+
+public class Main15 {
+    public List <String> practice1 () {
+        List<Integer> codes = List.of(97, 98, 99, 100, 101);
+        return codes.stream().map(c->(char)(int)c)
+                .map(String::valueOf).toList();
+    }
+
+}
