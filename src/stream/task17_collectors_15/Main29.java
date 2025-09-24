98990caff83d2decd8e5d72ec4b78b74e57365d4
morozvitali <morozvitali99@gmail.com>
2025-09-24 12:49:28 +0300

    task 17 5-15

--- src/stream/task17_collectors_15/Main29.java
+++ src/stream/task17_collectors_15/Main29.java
@@ -30,6 +30,10 @@ public class Main29 {
         return words.stream().collect(Collectors.groupingBy(String::length, Collectors.toSet()));
     }
 
-
+    public String practice5 () {
+        List<Integer> numbers = List.of(4, 8,
+                15, 16, 23, 42);
+        return numbers.stream().collect(Collectors.collectingAndThen(Collectors.counting(), count -> "counter" + count));
+    }
 
 }
