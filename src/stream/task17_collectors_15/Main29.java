a90561ccf6ef28e846c6f6490b398518fb79bd59
morozvitali <morozvitali99@gmail.com>
2025-09-24 12:52:51 +0300

    task 17 6-15

--- src/stream/task17_collectors_15/Main29.java
+++ src/stream/task17_collectors_15/Main29.java
@@ -36,4 +36,12 @@ public class Main29 {
         return numbers.stream().collect(Collectors.collectingAndThen(Collectors.counting(), count -> "counter" + count));
     }
 
+    public Map <Character, String> practice6 () {
+        List<String> words = List.of("apple",
+                "ant", "banana", "bat", "car");
+        return words.stream().collect(Collectors.groupingBy(w->w.charAt(0), Collectors.collectingAndThen(Collectors.toList(), list->String.join(" ", list))));
+    }
+
+
+
 }
