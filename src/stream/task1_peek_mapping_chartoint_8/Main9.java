083cf56dce6e860c5d646f87a81473c7b936656e
morozvitali <morozvitali99@gmail.com>
2025-09-25 17:03:53 +0300

    task0 stringbuilder 0 5-8

--- src/stream/task1_peek_mapping_chartoint_8/Main9.java
+++ src/stream/task1_peek_mapping_chartoint_8/Main9.java
@@ -1,6 +1,7 @@
 package stream.task1_peek_mapping_chartoint_8;
 
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main9 {
     public int practice1 (int n) {
@@ -20,6 +21,12 @@ return String.valueOf(Math.abs(n)).chars()
 
     public long practice4 (int n) {
         return String.valueOf(Math.abs(n)).chars().map(c->c-'0').filter(a->a>5).count();
+    }
+
+    public int practice5 (int n) {
+        int [] digits = String.valueOf(Math.abs(n)).chars().map(c->c-'0').toArray();
+        return IntStream.range(0, digits.length)
+                .filter(i->i%2==1).map(i->digits[i]).reduce(1, (a,b)-> a*b);
     }
 
 
