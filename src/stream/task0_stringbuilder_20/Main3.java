55612ce0052061eb75c8dafc32ed0343dc5aa31a
morozvitali <morozvitali99@gmail.com>
2025-09-25 14:40:03 +0300

    task0 stringbuilder 0 8-20

--- src/stream/task0_stringbuilder_20/Main3.java
+++ src/stream/task0_stringbuilder_20/Main3.java
@@ -1,38 +1,47 @@
 package stream.task0_stringbuilder_20;
 
 public class Main3 {
-    public void practice1 () {
+    public void practice1() {
         StringBuilder sb = new StringBuilder("Hello").append(" world");
         System.out.println(sb);
     }
 
-    public void practice2 () {
+    public void practice2() {
         StringBuilder sb = new StringBuilder("Java").insert(2, " cool ");
         System.out.println(sb);
     }
 
-    public void practice3 () {
-        StringBuilder sb = new StringBuilder("abcdef").delete(2,5);
+    public void practice3() {
+        StringBuilder sb = new StringBuilder("abcdef").delete(2, 5);
         System.out.println(sb);
     }
 
-    public void practice4 () {
+    public void practice4() {
         StringBuilder sb = new StringBuilder("level").reverse();
         System.out.println(sb);
     }
 
-    public void practice5 () {
-        StringBuilder sb = new StringBuilder("orange").replace(2,5, "X");
+    public void practice5() {
+        StringBuilder sb = new StringBuilder("orange").replace(2, 5, "X");
         System.out.println(sb);
     }
 
-    public void practice6 () {
+    public void practice6() {
         StringBuilder sb = new StringBuilder("123").append(4).append(5).append(6);
         System.out.println(sb);
     }
 
-    public void practice7 () {
-        StringBuilder sb = new StringBuilder("sunshine").insert(3,"___");
+    public void practice7() {
+        StringBuilder sb = new StringBuilder("sunshine").insert(3, "___");
         System.out.println(sb);
+    }
+
+    public void practice8() {
+        StringBuilder sb = new StringBuilder("ABCDEFG");
+        for (int i = sb.length() - 1; i >= 0; i = i - 2) {
+            if (i % 2 == 0) {
+                sb.deleteCharAt(i);
+            }
+        }
     }
 }
