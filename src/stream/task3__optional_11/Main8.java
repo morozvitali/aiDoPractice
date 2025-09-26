bcb308bad1f8129e6aa89627eca6a50748c6c279
morozvitali <morozvitali99@gmail.com>
2025-09-26 16:19:04 +0300

    task2 foreach ordered 6-11

--- src/stream/task3__optional_11/Main8.java
+++ src/stream/task3__optional_11/Main8.java
@@ -31,4 +31,11 @@ public class Main8 {
         //return list.stream().mapToInt(a->a).max().orElse(-1);
         list.stream().max((a,b)-> a.compareTo(b)).ifPresentOrElse(value -> System.out.println("Max = " + value), ()-> System.out.println("немає значень"));
     }
+
+    public void practice6 () {
+        List <Integer> list = List.of(1, 2, 3, 4, 5);
+        list.stream().min((a,b)->a < b ? a : b).ifPresentOrElse(value -> System.out.println("min " + value), () -> System.out.println("немає значень"));
+    }
+
+
 }
