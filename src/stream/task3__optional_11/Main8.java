366cc5c3339c41c81c3cfd321783f679a244a755
morozvitali <morozvitali99@gmail.com>
2025-09-26 15:52:45 +0300

    task2 foreach ordered 2-11

--- src/stream/task3__optional_11/Main8.java
+++ src/stream/task3__optional_11/Main8.java
@@ -1,13 +1,16 @@
 package stream.task3__optional_11;
 
 import java.util.List;
+import java.util.Optional;
 
 public class Main8 {
     public int practice1() {
         List<Integer> numbers = List.of(3, 8, 10, 15, 4);
         return numbers.stream().filter(a -> a % 5 == 0).findFirst().orElse(-1);
     }
-
-
+public int practice2 () {
+    Optional <Integer> value = Optional.of(7);
+    return value.map(v->v*2).orElse(-1);
+    }
 
 }
