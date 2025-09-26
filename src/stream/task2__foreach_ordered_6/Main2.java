4d7d467abbdd47177353857ce8fdb5b191c2137d
morozvitali <morozvitali99@gmail.com>
2025-09-26 12:41:27 +0300

    task2 foreach ordered 5-6

--- src/stream/task2__foreach_ordered_6/Main2.java
+++ src/stream/task2__foreach_ordered_6/Main2.java
@@ -1,27 +1,37 @@
 package stream.task2__foreach_ordered_6;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Main2 {
-    public void practice1 () {
+    public void practice1() {
         List<String> list = List.of("A", "B", "C", "D");
         list.stream().forEach(System.out::println);
     }
 
-    public void practice2 () {
+    public void practice2() {
         List<String> list = List.of("A", "B", "C", "D");
         list.stream().forEach(System.out::println);
         list.parallelStream().forEach(System.out::println);
     }
 
-    public void practice3 () {
+    public void practice3() {
         List<String> list = List.of("A", "B", "C", "D");
         list.parallelStream().forEachOrdered(System.out::println);
     }
 
-    public void practice4 () {
-        List.of(1, 2, 3, 4).parallelStream().peek(a-> System.out.println("Обробляю: " + a)).forEachOrdered(a-> System.out.println("Обробляю: " + a));
+    public void practice4() {
+        List.of(1, 2, 3, 4).parallelStream().peek(a -> System.out.println("Обробляю: " + a)).forEachOrdered(a -> System.out.println("Обробляю: " + a));
     }
 
-
+    public void practice5() {
+        IntStream.rangeClosed(1, 5).parallel().forEach(i -> {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                    }
+                    System.out.println(i + " ");
+                }
+        );
+    }
 }
