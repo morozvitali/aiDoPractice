f30548cadf1d3071fe32ad19e3fd2175ce9e40c4
morozvitali <morozvitali99@gmail.com>
2025-09-29 15:57:32 +0300

    task12 regex 3-10

--- src/stream/task12_plus_valueOf_10/Main23.java
+++ src/stream/task12_plus_valueOf_10/Main23.java
@@ -20,5 +20,14 @@ public class Main23 {
                 .mapToObj(String::valueOf)
                 .collect(Collectors.joining()));
     }
+    public int practice3 (int n) {
+        return Integer.parseInt(String.valueOf(Math.abs(n))
+                .chars()
+                .map(Character::getNumericValue)
+                .filter(a->a%2==0)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining()));
+    }
+
 
 }
