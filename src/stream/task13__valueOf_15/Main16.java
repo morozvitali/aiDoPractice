24f9120810bcabf21943644dca218233f0c98182
morozvitali <morozvitali99@gmail.com>
2025-09-29 16:35:42 +0300

    task13 regex 11-15

--- src/stream/task13__valueOf_15/Main16.java
+++ src/stream/task13__valueOf_15/Main16.java
@@ -56,5 +56,12 @@ public class Main16 {
         return s.chars().filter(Character::isDigit).map(Character::getNumericValue).sum();
     }
 
+    public String practice11 () {
+        String input = "ABCdefGHIjkl123";
+        return input.chars().filter(c->Character.isAlphabetic(c) && Character.isLowerCase(c)).mapToObj(String::valueOf).collect(Collectors.joining());
+    }
+
+
+
 
 }
