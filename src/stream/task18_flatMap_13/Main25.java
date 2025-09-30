38c5e278ab8c7f1e6af81781a977cee0b7ec2b47
morozvitali <morozvitali99@gmail.com>
2025-09-30 17:30:00 +0300

    task18 Comparators 5-13

--- src/stream/task18_flatMap_13/Main25.java
+++ src/stream/task18_flatMap_13/Main25.java
@@ -40,6 +40,13 @@ public class Main25 {
         return Arrays.stream(data).flatMapToInt(Arrays::stream).filter(a -> a % 3 == 0).max().orElse(-1);
     }
 
+    public int practice5 () {
+        int[][] data = {
+                {2, 3},
+                {4}
+        };
+        return Arrays.stream(data).flatMapToInt(Arrays::stream).filter(a->a%2==0).map(a->a*a).sum();
+    }
 
 
 
