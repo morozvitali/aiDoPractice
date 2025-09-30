3be87d7e982244f21f23b7d9ba53f11e850b0635
morozvitali <morozvitali99@gmail.com>
2025-09-30 17:05:58 +0300

    task17 Comparators 5-15

--- src/stream/task17_collectors_15/Main30.java
+++ src/stream/task17_collectors_15/Main30.java
@@ -31,4 +31,13 @@ public class Main30 {
         return words.stream().collect(Collectors.groupingBy(String::length, Collectors.toSet()));
     }
 
+    public String practice5 () {
+        List<Integer> numbers = List.of(4, 8,
+                15, 16, 23, 42);
+        return numbers.stream().collect(Collectors.collectingAndThen(Collectors.counting(), count -> "counter "  + count));
+    }
+
+
+
+
 }
