8a2ada0613ee82d925b73c88e3c08673c6d9acc8
morozvitali <morozvitali99@gmail.com>
2025-09-30 13:30:03 +0300

    task11 Product Catalog 5-10

---
+++ src/stream/task11_string_formater_goood/Place1.java
@@ -0,0 +1,17 @@
+package stream.task11_string_formater_goood;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Place1 {
+
+
+    public void count(List<String> orders) {
+        Map<String, Long> cityStats = orders.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        cityStats.entrySet().stream().sorted(Map.Entry.<String, Long>comparingByValue().reversed()).limit(3).forEach(System.out::println);
+    }
+
+
+}
