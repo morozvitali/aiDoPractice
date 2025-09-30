ce01a20931b5cfaaeaddd99eea2da600e6a4acfd
morozvitali <morozvitali99@gmail.com>
2025-09-30 12:08:32 +0300

    task11 hashMap 8-10

--- src/stream/task11_string_formater_goood/PhoneBook2.java
+++ src/stream/task11_string_formater_goood/PhoneBook2.java
@@ -1,5 +1,9 @@
 package stream.task11_string_formater_goood;
 
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class PhoneBook2 {
@@ -23,32 +27,42 @@ public class PhoneBook2 {
         return result;
     }
 
-    public void remove (String surname, String phone) {
+    public void remove(String surname, String phone) {
         if (phoneBook.containsKey(surname)) {
             phoneBook.get(surname).remove(phone);
         }
     }
 
-    public List <String> findSurnameByPhone (String phone) {
-        List <String> result = new ArrayList<>();
-        for (Map.Entry <String, List <String>> entry : phoneBook.entrySet()) {
-            if(entry.getValue().contains(phone)) {
+    public List<String> findSurnameByPhone(String phone) {
+        List<String> result = new ArrayList<>();
+        for (Map.Entry<String, List<String>> entry : phoneBook.entrySet()) {
+            if (entry.getValue().contains(phone)) {
                 result.add(entry.getKey());
             }
         }
         return result;
     }
 
-    public List <String> getAllSurnamesSorted() {
+    public List<String> getAllSurnamesSorted() {
         return phoneBook.keySet().stream().sorted().toList();
     }
 
-    public int countPhones (String surname) {
+    public int countPhones(String surname) {
         return phoneBook.getOrDefault(surname, Collections.emptyList()).size();
     }
 
-    public List <String> getSurrnamesSortedByPhones () {
-        return phoneBook.entrySet().stream().sorted((a,b)->Integer.compare(b.getValue().size(), a.getValue().size())).map(Map.Entry::getKey).toList();
+    public List<String> getSurrnamesSortedByPhones() {
+        return phoneBook.entrySet().stream().sorted((a, b) -> Integer.compare(b.getValue().size(), a.getValue().size())).map(Map.Entry::getKey).toList();
     }
+
+    public void merge(PhoneBook2 other) {
+        for (Map.Entry<String, List<String>> entry : other.phoneBook.entrySet()) {
+            phoneBook.merge(entry.getKey(), entry.getValue(), (oldSet, newSet) -> {
+                oldSet.addAll(newSet);
+                return oldSet;
+            });
+        }
+    }
+
 
 }
