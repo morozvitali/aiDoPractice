e80e15cae548087a95fa215bbcfa68edc2548aac
morozvitali <morozvitali99@gmail.com>
2025-10-01 11:56:44 +0300

    task12 value of 1-10

---
+++ src/stream/task12_plus_valueOf_10/Main24.java
@@ -0,0 +1,12 @@
+package stream.task12_plus_valueOf_10;
+
+import java.util.stream.Collectors;
+
+public class Main24 {
+
+    public int practice1 (int n) {
+        return Integer.parseInt(String.valueOf(Math.abs(n)).chars().map(Character::getNumericValue).map(a->a*a).mapToObj(String::valueOf).collect(Collectors.joining()));
+    }
+
+
+}
