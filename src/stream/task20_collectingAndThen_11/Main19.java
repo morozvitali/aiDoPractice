fc48e4d936d593fc57e0c2524670ce1a24282c08
morozvitali <morozvitali99@gmail.com>
2025-10-01 10:51:17 +0300

    task20 Collectors 9-10

--- src/stream/task20_collectingAndThen_11/Main19.java
+++ src/stream/task20_collectingAndThen_11/Main19.java
@@ -44,5 +44,9 @@ public class Main19 {
         }));
     }
 
+    public String practice9 () {
+        return Stream.of(1, 2, 3, 4, 5).filter(a->a%2==0).map(String::valueOf).collect(Collectors.collectingAndThen(Collectors.toList(), list-> String.join(", ", list)));
+    }
+
 
 }
