3559f2b817ad02a559f06413ba769224ffbe057c
morozvitali <morozvitali99@gmail.com>
2025-10-01 10:16:55 +0300

    task19 Sorting 8-10

--- src/stream/task19_sorted_10/Main20.java
+++ src/stream/task19_sorted_10/Main20.java
@@ -35,4 +35,13 @@ public class Main20 {
     public static long getCount (String s) {
         return s.chars().filter(c->"aeiou".indexOf(c)>=0).count();
     }
+
+    public List <String> practice8 () {
+        List<String> words = List.of("tea", "watermelon",
+                "apple", "pear", "banana");
+        return words.stream().sorted(Comparator.comparing(String::length).thenComparing(Comparator.reverseOrder())).toList();
+    }
+
+
+
 }
