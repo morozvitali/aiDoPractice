9e2b641a6f8e75a0032e0d46c384263f649a2a0e
morozvitali <morozvitali99@gmail.com>
2025-10-01 14:48:55 +0300

    task14 summary 5 - 10

--- src/stream/task15_grouping_20/Main32.java
+++ src/stream/task15_grouping_20/Main32.java
@@ -26,6 +26,9 @@ public class Main32 {
         return Stream.of(12, 23, 34, 45, 16, 7).collect(Collectors.groupingBy(a->a%10, Collectors.summingInt(n->n)));
     }
 
-
+    public Integer practice5 () {
+        return Stream.of(1,2,2,3,3,3,4,4).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream().max(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse(-1);
+    }
 
 }
