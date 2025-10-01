6d9ad408d746b345c800453d63a9f09557bc74d0
morozvitali <morozvitali99@gmail.com>
2025-10-01 10:27:48 +0300

    task20 Collectors 2-10

--- src/stream/task20_collectingAndThen_11/Main19.java
+++ src/stream/task20_collectingAndThen_11/Main19.java
@@ -6,12 +6,14 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main19 {
-    public List<String> practice1 () {
+    public List<String> practice1() {
         return Stream.of("apple", "car", "banana",
-                "hi", "cherry").filter(w->w.length() > 4).collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
+                "hi", "cherry").filter(w -> w.length() > 4).collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
     }
 
-
+    public Integer practice2 () {
+        return Stream.of(3, 9, 2, 4, 6, 7, 12).collect(Collectors.collectingAndThen(Collectors.toList(), List::size));
+    }
 
 
 }
