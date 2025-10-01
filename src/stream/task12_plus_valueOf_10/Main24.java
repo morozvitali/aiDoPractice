d18cc5d882da80303a1aea9a6ed48632a88cff28
morozvitali <morozvitali99@gmail.com>
2025-10-01 11:57:20 +0300

    task12 value of 2-10

--- src/stream/task12_plus_valueOf_10/Main24.java
+++ src/stream/task12_plus_valueOf_10/Main24.java
@@ -8,5 +8,10 @@ public class Main24 {
         return Integer.parseInt(String.valueOf(Math.abs(n)).chars().map(Character::getNumericValue).map(a->a*a).mapToObj(String::valueOf).collect(Collectors.joining()));
     }
 
+    public int practice2 (int n) {
+        return Integer.parseInt(String.valueOf(Math.abs(n)).chars().map(Character::getNumericValue).map(a->a*a*a).mapToObj(String::valueOf).collect(Collectors.joining()));
+    }
+
+
 
 }
