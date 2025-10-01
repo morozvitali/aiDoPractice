c7c6e2feda112ad6b9b098137bd73c58232a7646
morozvitali <morozvitali99@gmail.com>
2025-10-01 11:29:16 +0300

    task09 arrays differense 3-3

--- src/stream/task9_diff_21/Main4.java
+++ src/stream/task9_diff_21/Main4.java
@@ -21,6 +21,16 @@ public class Main4 {
         return Arrays.stream(a).filter(n->!setb.contains(n)).toArray();
     }
 
+    public List practice3 () {
+        int[] a = new int[]{1, 2, 3, 4};
+        int[] b = new int[]{2, 4, 6};
+        Set<Integer>  sa = Arrays.stream(a).boxed().collect(Collectors.toSet());
+        Set<Integer>  sb = Arrays.stream(b).boxed().collect(Collectors.toSet());
+
+        Set<Integer> one = sa.stream().filter(value -> !sb.contains(value)).collect(Collectors.toSet());
+        Set<Integer> two = sb.stream().filter(value -> !sa.contains(value)).collect(Collectors.toSet());
+        return Stream.concat(one.stream(), two.stream()).toList();
+    }
 
 
 
