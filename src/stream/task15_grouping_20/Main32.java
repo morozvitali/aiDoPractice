f81cbd4c40d7efe676d72385b82222343a1c426c
morozvitali <morozvitali99@gmail.com>
2025-10-01 14:35:55 +0300

    task14 summary 1 - 10

---
+++ src/stream/task15_grouping_20/Main32.java
@@ -0,0 +1,18 @@
+package stream.task15_grouping_20;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main32 {
+    public Map<String, Boolean> practice1 () {
+        String[] words = {"sky", "apple",
+                "dry", "orange", "sun"};
+        return Arrays.stream(words).collect(Collectors.toMap(Function.identity(), w->w.chars().filter(c->"aeiou".indexOf(c)>=0).findAny().isPresent()));
+    }
+
+
+
+}
