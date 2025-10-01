38bd26406856ba561081cfbe3ddc7cf2bc87eb2b
morozvitali <morozvitali99@gmail.com>
2025-10-01 13:51:45 +0300

    task14 summary 3 - 10

--- src/stream/task14_summarystatistics_10/Main33.java
+++ src/stream/task14_summarystatistics_10/Main33.java
@@ -1,6 +1,10 @@
 package stream.task14_summarystatistics_10;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Main33 {
 
@@ -9,5 +13,16 @@ public class Main33 {
         return Arrays.stream(numbers).distinct().summaryStatistics().getCount();
     }
 
+    public int practice2 () {
+        List<String> words = List.of("APPLE",
+                "Banana", "CHERRY", "kiwi", "PLUM");
+        return words.stream().filter(w->w.length() ==0).map(s->s.toLowerCase()).mapToInt(String::length).sum();
+    }
 
+
+    public Map<String, Long> practice3 () {
+        String[] words = {"sky", "apple",
+                "moon", "dry", "banana"};
+        return Arrays.stream(words).filter(w->w.length() > 3).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
 }
