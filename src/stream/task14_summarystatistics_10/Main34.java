9816584604f05e5c3fd1aef7e5c60eb5853d1f2d
morozvitali <morozvitali99@gmail.com>
2025-10-03 15:05:53 +0300

    tas12 value 10 - 10

---
+++ src/stream/task14_summarystatistics_10/Main34.java
@@ -0,0 +1,14 @@
+package stream.task14_summarystatistics_10;
+
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+
+public class Main34 {
+    public IntSummaryStatistics practice1 () {
+        int[] numbers = {2, 4, 2, 6, 8, 4, 10, 10, 12};
+        return Arrays.stream(numbers).distinct().summaryStatistics();
+    }
+
+
+
+}
