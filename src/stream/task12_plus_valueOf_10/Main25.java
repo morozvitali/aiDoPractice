5a75f20af7c66633b02a84a6b7ebb03aeebc7215
morozvitali <morozvitali99@gmail.com>
2025-10-03 14:53:16 +0300

    tas12 value 5 - 10

--- src/stream/task12_plus_valueOf_10/Main25.java
+++ src/stream/task12_plus_valueOf_10/Main25.java
@@ -38,6 +38,11 @@ public class Main25 {
                 .collect(Collectors.joining()));
     }
 
+    public int practice5 (int n) {
+        return Integer
+                .parseInt(new StringBuilder(String.valueOf(Math.abs(n)))
+                        .reverse().toString());
+    }
 
 
 }
