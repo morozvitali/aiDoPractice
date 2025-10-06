270202252ea01f839e0c2b2977962353019474d6
morozvitali <morozvitali99@gmail.com>
2025-10-06 15:44:14 +0300

    tas17 collectors 3 - 15

--- src/stream/task17_collectors_15/Main31.java
+++ src/stream/task17_collectors_15/Main31.java
@@ -5,22 +5,29 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main31 {
-    public Map<Character,List<String>> practice1 () {
+    public Map<Character, List<String>> practice1() {
         List<String> words = List.of("apple",
                 "ant", "banana", "bat", "car");
-        return words.stream().collect(Collectors.groupingBy(w->w.charAt(0), Collectors.mapping(w->w.toUpperCase(), Collectors.toList())));
+        return words.stream().collect(Collectors.groupingBy(w -> w.charAt(0), Collectors.mapping(w -> w.toUpperCase(), Collectors.toList())));
     }
 
-    public Map<Character, List<Integer>> practice2 () {
+    public Map<Character, List<Integer>> practice2() {
         List<String> words = List.of("apple", "ant",
                 "banana", "bat", "car");
         return words.stream()
                 .collect(Collectors.groupingBy(
-                        w->w.charAt(0),
+                        w -> w.charAt(0),
                         Collectors.mapping(
                                 String::length,
                                 Collectors.toList())));
     }
+
+
+    public Map<Boolean, Long> practice3() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        return numbers.stream().collect(Collectors.groupingBy(a -> a % 2 == 0, Collectors.counting()));
+    }
+
 
 
 }
