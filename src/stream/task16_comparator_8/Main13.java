f96879125cd606a90048c25e20a038703b6ab7de
morozvitali <morozvitali99@gmail.com>
2025-10-06 14:38:23 +0300

    tas16 comparing 2 - 10

--- src/stream/task16_comparator_8/Main13.java
+++ src/stream/task16_comparator_8/Main13.java
@@ -1,7 +1,11 @@
 package stream.task16_comparator_8;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Main13 {
 
@@ -16,5 +20,17 @@ public class Main13 {
         return s.chars().filter(c->"aeiou".indexOf(c)>=0).count();
     }
 
+
+    public String practice2 () {
+        Map<String, Long> map = List.of("alpha", "arena", "java", "banana", "lava")
+                .stream()
+                .collect(Collectors.toMap(Function.identity(), Main13::countLetter));
+                return Collections.max(map.entrySet(), Map.Entry.comparingByValue()).getKey();
+
+    }
+
+    public static long countLetter (String s) {
+        return s.chars().filter(c->c=='a').count();
+    }
 
 }
