f5b6c33dbd9687fdefce32cef18b2714aa27348d
morozvitali <morozvitali99@gmail.com>
2025-10-06 11:07:33 +0300

    tas14 summary 9 - 10

--- src/stream/task14_summarystatistics_10/Main35.java
+++ src/stream/task14_summarystatistics_10/Main35.java
@@ -56,6 +56,14 @@ public class Main35 {
         String[] words = {"apple", "moon",
                 "sky", "banana", "loop"};
         return Arrays.stream(words).mapToInt(String::length).summaryStatistics();
-
     }
+
+    public List <String> practice9 () {
+        String[] words = {"education", "questionnaire",
+                "house", "audio", "universe"};
+        return Arrays.stream(words).filter(w->"aeiou".chars().allMatch(c->w.indexOf(c) >=0)).collect(Collectors.toList());
+    }
+
+
+
 }
