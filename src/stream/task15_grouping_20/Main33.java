f55a9a60c6c82d147cf20766387345eb98d6dcdf
morozvitali <morozvitali99@gmail.com>
2025-10-06 11:41:55 +0300

    tas15 grouping 3 - 10

--- src/stream/task15_grouping_20/Main33.java
+++ src/stream/task15_grouping_20/Main33.java
@@ -1,6 +1,8 @@
 package stream.task15_grouping_20;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -16,8 +18,13 @@ public class Main33 {
         return  s.chars().filter(c->"aeiou".indexOf(c)>=0).count();
     }
 
-    public Long practce2 (String s) {
+    public Long practice2 (String s) {
         return s.chars().mapToObj(c->(char)c).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet().stream().filter(e->e.getValue() >1).count();
+    }
+
+    public String practice3 () {
+        Map<String, Long> map = Arrays.stream("java is fun and java is powerful".split(" ")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        return Collections.max(map.entrySet(), Map.Entry.comparingByValue()).getKey();
     }
 }
