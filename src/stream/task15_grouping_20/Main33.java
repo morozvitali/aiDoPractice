5c0cf077d0c62c407aa8e28c9197298ce0e9b7db
morozvitali <morozvitali99@gmail.com>
2025-10-06 12:45:09 +0300

    tas15 grouping 17 - 10

--- src/stream/task15_grouping_20/Main33.java
+++ src/stream/task15_grouping_20/Main33.java
@@ -98,5 +98,10 @@ public class Main33 {
         return Arrays.stream(words).filter(w->w.chars().anyMatch(c->"aeiou".indexOf(c) >=0)).collect(Collectors.groupingBy(String::length, Collectors.counting()));
     }
 
+    public Map <Boolean, List <String>> practice17 () {
+        String[] words = {"apple", "banana",
+                "dog", "kiwi", "plum"};
+        return Arrays.stream(words).collect(Collectors.groupingBy(w->w.chars().anyMatch(c->"aeiou".indexOf(c)>=0), Collectors.counting()));
+    }
 
 }
