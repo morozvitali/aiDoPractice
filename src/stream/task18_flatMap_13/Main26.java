d7b39620e2d87c1aa056711533b9efa8e2c3c288
morozvitali <morozvitali99@gmail.com>
2025-10-07 11:51:24 +0300

    task18 collectors 2 - 13

--- src/stream/task18_flatMap_13/Main26.java
+++ src/stream/task18_flatMap_13/Main26.java
@@ -13,6 +13,14 @@ public class Main26 {
         return sentences.stream().flatMap(s-> Arrays.stream(s.split(" "))).distinct().toList();
     }
 
+    public int practice2 () {
+        int[][] data = {
+                {3, 11},
+                {6, 7},
+                {10}
+        };
+        return Arrays.stream(data).flatMapToInt(Arrays::stream).min().orElse(-1);
+    }
 
 
 
