2c5127bd271ee5a5514a66137b3d350836d47968
itdep <morozvitali99@gmail.com>
2025-10-09 12:08:14 +0300

    tasks sorting 16 2-8

--- src/stream/task16_comparator_8/Main14.java
+++ src/stream/task16_comparator_8/Main14.java
@@ -1,7 +1,11 @@
 package stream.task16_comparator_8;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Main14 {
     public List<String> practice1 () {
@@ -12,6 +16,15 @@ public class Main14 {
 
     public static long getCount(String s) {
         return s.chars().filter(c->"aeiou".indexOf(c)>=0).count();
+    }
+
+    public String practice2 () {
+        Map<String, Long> map =  List.of("alpha", "arena", "java", "banana", "lava").stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        return Collections.max(map.entrySet(), Map.Entry.comparingByValue()).getKey();
+    }
+
+    public static long countLetter (String s) {
+        return s.chars().filter(c->c=='a').count();
     }
 
 
