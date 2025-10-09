f93c82385b494445ae7e9ed0d95441b277c151fd
itdep <morozvitali99@gmail.com>
2025-10-09 12:48:35 +0300

    tasks collectors 17 3-15

--- src/stream/task17_collectors_15/Main32.java
+++ src/stream/task17_collectors_15/Main32.java
@@ -19,6 +19,9 @@ public class Main32 {
         return words.stream().collect(Collectors.groupingBy(w->w.charAt(0), Collectors.mapping(w->w.length(), Collectors.toList())));
     }
 
-
+    public Map <Boolean, Long> practice3 () {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        return numbers.stream().collect(Collectors.partitioningBy(a->a%2==0, Collectors.counting()));
+    }
 
 }
