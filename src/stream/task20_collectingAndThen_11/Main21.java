4f87c1b618f2541c7aad60e7938f0b113889c6ba
itdep <morozvitali99@gmail.com>
2025-10-09 16:30:21 +0300

    tasks collectingAndThen 20 9-11

--- src/stream/task20_collectingAndThen_11/Main21.java
+++ src/stream/task20_collectingAndThen_11/Main21.java
@@ -44,6 +44,10 @@ public class Main21 {
         return Stream.of(2, 4, 10, 12, 14, 12, 4).collect(Collectors.collectingAndThen(Collectors.toSet(), Collections::unmodifiableSet));
     }
 
+    public String practice9 () {
+        return Stream.of(1, 2, 3, 4, 5).map(String::valueOf).collect(Collectors.collectingAndThen(Collectors.toList(), list -> String.join(", ", list)));
+    }
+
 
 
 }
