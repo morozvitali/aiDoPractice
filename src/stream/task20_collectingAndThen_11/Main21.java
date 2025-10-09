a9625083b3aa6a95d983e5fbd4ac6f2a8ffb920a
itdep <morozvitali99@gmail.com>
2025-10-09 16:16:13 +0300

    tasks collectingAndThen 20 6-11

--- src/stream/task20_collectingAndThen_11/Main21.java
+++ src/stream/task20_collectingAndThen_11/Main21.java
@@ -1,6 +1,7 @@
 package stream.task20_collectingAndThen_11;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -31,6 +32,11 @@ public class Main21 {
 
     public Integer practice5 () {
         return Stream.of(3, 9, 2, 4, 6, 7, 12).collect(Collectors.collectingAndThen(Collectors.toList(), List::size));
+    }
+
+    public String practice6 () {
+        List<String> data = List.of("apple", "car", "banana", "hi", "cherry");
+        return data.stream().collect(Collectors.collectingAndThen(Collectors.toList(), list->list.stream().sorted(Comparator.comparing(String::length).thenComparing(Comparator.reverseOrder())))).toList().getFirst();
     }
 
 
