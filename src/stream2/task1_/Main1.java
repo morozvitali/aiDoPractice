4e135edd29407b2c26dace93edcef33c54991740
itdep <morozvitali99@gmail.com>
2025-10-13 16:01:42 +0300

    Stream2 task1 6-10

--- src/stream2/task1_/Main1.java
+++ src/stream2/task1_/Main1.java
@@ -1,9 +1,6 @@
 package stream2.task1_;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collector;
 
 public class Main1 {
@@ -76,6 +73,16 @@ public class Main1 {
                 }
         ));
     }
+
+    public void practice6 () {
+        List<String> words = List.of("apple", "banana", "pear");
+        Map<Character, Integer> map = words.stream().collect(Collector.of(HashMap::new,
+                (m,s) -> m.put(s.charAt(0), s.length()),
+                (m1,m2) -> {m1.putAll(m2); return m1; }
+                ));
+    }
+
+
 
 
 }
