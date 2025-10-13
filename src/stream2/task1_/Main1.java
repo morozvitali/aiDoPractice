f53dde34647ec33f919a33b4e2a575d94102d05c
itdep <morozvitali99@gmail.com>
2025-10-13 15:57:36 +0300

    Stream2 task1 5-10

--- src/stream2/task1_/Main1.java
+++ src/stream2/task1_/Main1.java
@@ -1,5 +1,6 @@
 package stream2.task1_;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -19,36 +20,62 @@ public class Main1 {
         ));
     }
 
-    public void practice2 () {
+    public void practice2() {
         List<Integer> nums = List.of(1, 2, 3, 4);
 
         int sum = nums.stream().collect(Collector.of(
-                () -> new int [1],
-                (a,n) -> a[0] += n,
-                (a1, a2) -> {a1 [0] += a2[0]; return a1; },
-                a->a[0]
+                () -> new int[1],
+                (a, n) -> a[0] += n,
+                (a1, a2) -> {
+                    a1[0] += a2[0];
+                    return a1;
+                },
+                a -> a[0]
         ));
     }
 
-    public void practice3 () {
+    public void practice3() {
         List<Integer> list = List.of(5, 10, 15);
-        double avg  = list.stream().collect(Collector.of(
-                () -> new int [2],
-                (a,n) -> {a[0] += n; a[1]++;},
-                (a1,a2) -> {a1[0]+= a2[0]; a1[1] +=a2[1]; return a1; },
-                a-> (double)a [0]/a[1]
+        double avg = list.stream().collect(Collector.of(
+                () -> new int[2],
+                (a, n) -> {
+                    a[0] += n;
+                    a[1]++;
+                },
+                (a1, a2) -> {
+                    a1[0] += a2[0];
+                    a1[1] += a2[1];
+                    return a1;
+                },
+                a -> (double) a[0] / a[1]
         ));
     }
 
-    public void practice4 () {
+    public void practice4() {
         List<String> words = List.of("a", "b", "a", "c");
         Set<String> set = words.stream().collect(Collector.of(
                 HashSet::new,
                 Set::add,
-                (s1, s2) -> {s1.addAll(s2); return s1; }
+                (s1, s2) -> {
+                    s1.addAll(s2);
+                    return s1;
+                }
         ));
     }
 
+    public void practice5() {
+        List<Integer> nums = List.of(1, 2, 3, 4, 5);
+        List<Integer> evens = nums.stream().collect(Collector.of(
+                ArrayList::new,
+                (list, n) -> {
+                    if (n % 2 == 0) list.add(n);
+                },
+                (l1, l2) -> {
+                    l1.addAll(l2);
+                    return l1;
+                }
+        ));
+    }
 
 
 }
