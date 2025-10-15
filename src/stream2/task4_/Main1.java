ea5aa1ec69297bbdba9b2c7d89f519425aa5aa3f
itdep <morozvitali99@gmail.com>
2025-10-15 15:07:50 +0300

    task4 2-10

--- src/stream2/task4_/Main1.java
+++ src/stream2/task4_/Main1.java
@@ -3,10 +3,18 @@ package stream2.task4_;
 import java.util.stream.Stream;
 
 public class Main1 {
-    public void practice1 () {
-        Stream.iterate(0, n->n+2)
+    public void practice1() {
+        Stream.iterate(0, n -> n + 2)
                 .limit(5)
                 .forEach(System.out::println);
     }
+
+    public void practice2() {
+        Stream.generate(() -> (int) (Math.random() * 10))
+                .limit(5)
+                .forEach(System.out::println);
+    }
+
+
 
 }
