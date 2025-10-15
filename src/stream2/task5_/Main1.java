7955ba179cd4cc65de720b50a8118e012be5680a
itdep <morozvitali99@gmail.com>
2025-10-15 17:18:45 +0300

    task5 2-10

--- src/stream2/task5_/Main1.java
+++ src/stream2/task5_/Main1.java
@@ -1,5 +1,6 @@
 package stream2.task5_;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Main1 {
@@ -7,6 +8,15 @@ public class Main1 {
         Stream<String> a = Stream.of("A", "B", "C");
         Stream<String> b = Stream.of("D", "E");
         Stream.concat(a,b).forEach(System.out::println);
+    }
+
+    public void practice2 () {
+        List<String> one = List.of("apple", "banana", "cherry");
+        List<String> two = List.of("banana", "date", "apple");
+        List<String> merged = Stream.concat(one.stream(), two.stream())
+                .distinct()
+                .toList();
+        System.out.println(merged);
     }
 
 
