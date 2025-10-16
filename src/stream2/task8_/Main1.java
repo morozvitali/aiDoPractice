3b44fb6b5162d190f154a6e0e02590329e3187a2
itdep <morozvitali99@gmail.com>
2025-10-16 12:35:41 +0300

    task8 2-10

--- src/stream2/task8_/Main1.java
+++ src/stream2/task8_/Main1.java
@@ -9,6 +9,10 @@ public class Main1 {
         name.stream().forEach(System.out::println);
     }
 
+    public void practice2 () {
+        Optional <String> empty = Optional.empty();
+        empty.stream().forEach(System.out::println); // empty result, nothing
+    }
 
 
 }
