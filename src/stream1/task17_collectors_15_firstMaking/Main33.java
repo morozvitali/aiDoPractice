beb39467aa944812e15e2e61c91485380e998da8
itdep <morozvitali99@gmail.com>
2025-10-17 16:32:29 +0300

    task17 1-15

---
+++ src/stream1/task17_collectors_15_firstMaking/Main33.java
@@ -0,0 +1,16 @@
+package stream1.task17_collectors_15_firstMaking;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main33 {
+    public Map<Character, List<String>> practice1 () {
+        List<String> words = List.of("apple",
+                "ant", "banana", "bat", "car");
+        return words.stream().collect(Collectors.groupingBy(w->w.charAt(0), Collectors.mapping(w->w.toUpperCase(), Collectors.toList())));
+    }
+
+
+
+}
