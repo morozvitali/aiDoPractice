598695005c945b4f8e963c779c308ca850d3d928
itdep <morozvitali99@gmail.com>
2025-10-17 15:01:43 +0300

    task15 5-20

--- src/stream1/task15_grouping_20/Main35.java
+++ src/stream1/task15_grouping_20/Main35.java
@@ -34,6 +34,10 @@ public class Main35 {
         return Stream.of(12, 23, 34, 45, 16, 7).collect(Collectors.groupingBy(n->n%10, Collectors.summingInt(n->n)));
     }
 
+    public Integer practice5 () {
+        return  Stream.of(1,2,2,3,3,3,4,4).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream().max(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse(-1);
+    }
 
 
 }
