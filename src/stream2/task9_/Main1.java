9c61d03081b745963035274d0fb452e25ec27569
itdep <morozvitali99@gmail.com>
2025-10-20 13:54:18 +0300

    stream2 task9 1-10

---
+++ src/stream2/task9_/Main1.java
@@ -0,0 +1,15 @@
+package stream2.task9_;
+
+import java.util.List;
+
+public class Main1 {
+    public void practice1 () {
+        List.of(1,2,3,4,5)
+                .stream()
+                .forEach(System.out::println);
+    }
+
+    public void practice2 () {
+
+    }
+}
