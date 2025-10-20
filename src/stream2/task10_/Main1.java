7b599f96125d5379c0bdb0186f2c7c17fa520772
itdep <morozvitali99@gmail.com>
2025-10-20 16:50:42 +0300

    stream2 task10 2-10

--- src/stream2/task10_/Main1.java
+++ src/stream2/task10_/Main1.java
@@ -1,12 +1,43 @@
 package stream2.task10_;
 
 import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Main1 {
-    public void practice1 () {
-        IntSummaryStatistics stats = IntStream.of(5,10,15,20).summaryStatistics();
+    public void practice1() {
+        IntSummaryStatistics stats = IntStream.of(5, 10, 15, 20).summaryStatistics();
         System.out.println(stats.getAverage());
     }
 
+    public IntSummaryStatistics practice2() {
+        List<Product> products = List.of(
+                new Product("Apple", 10),
+                new Product("Pear", 8),
+                new Product("Mango", 12)
+        );
+
+        IntSummaryStatistics stats = products.stream().collect(Collectors.summarizingInt(Product::getPrice));
+        return stats;
+    }
+
+
+
+
+
+}
+
+class Product {
+    String fruit;
+    static int number;
+
+    Product(String fruit, int number) {
+        this.fruit = fruit;
+        this.number = number;
+    }
+
+    public int getPrice() {
+        return number;
+    }
 }
