4f665cbdef3c773c1ec1bf39e87f05a84dccf392
itdep <morozvitali99@gmail.com>
2025-10-22 14:34:47 +0300

    stream1 task19 4-10

--- src/stream1/task19_sorted_10/Main23.java
+++ src/stream1/task19_sorted_10/Main23.java
@@ -4,17 +4,21 @@ import java.util.Comparator;
 import java.util.List;
 
 public class Main23 {
-    public List <String> practice1 () {
+    public List<String> practice1() {
         List<String> words = List.of("tea", "lemon",
                 "fig", "grape", "banana");
-        return words.stream().filter(w->w.length() > 3).sorted(Comparator.comparing(String::length)).toList();
+        return words.stream().filter(w -> w.length() > 3).sorted(Comparator.comparing(String::length)).toList();
     }
 
-    public void practice3 () {
+    public void practice3() {
         List<Integer> numbers = List.of(3, 7, 1, 9, 2);
         numbers.stream().sorted(Comparator.reverseOrder()).toList();
     }
 
+    public List <Integer> practice5() {
+        List<Integer> nums = List.of(3, 2, 5, 6, 1, 4);
+        return nums.stream().sorted(Comparator.comparing(a->(int)a%2==0).thenComparing(a->(int)a)).toList();
+    }
 
 
 }
