22628bde04543d78794f29bb3d6815743c6d723b
itdep <morozvitali99@gmail.com>
2025-10-22 10:03:42 +0300

    stream1 task17 2-15

--- src/stream1/task17_collectors_15_firstMaking/Main34.java
+++ src/stream1/task17_collectors_15_firstMaking/Main34.java
@@ -1,15 +1,21 @@
 package stream1.task17_collectors_15_firstMaking;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main34 {
-    public void practice1 () {
+    public Map<Character, List <String>> practice1 () {
         List<String> words = List.of("apple",
                 "ant", "banana", "bat", "car");
-        words.stream().collect(Collectors.groupingBy(w->w.charAt(0), Collectors.mapping(w->w.toUpperCase(), Collectors.toList())));
+        return words.stream().collect(Collectors.groupingBy(w->w.charAt(0), Collectors.mapping(w->w.toUpperCase(), Collectors.toList())));
     }
 
+    public Map <Character, List <Integer>> practice2 () {
+        List<String> words = List.of("apple", "ant",
+                "banana", "bat", "car");
+        return words.stream().collect(Collectors.groupingBy(w->w.charAt(0), Collectors.mapping(String::length, Collectors.toList())));
+    }
 
 
 }
