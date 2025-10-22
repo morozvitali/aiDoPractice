a2eeb0aa2665336f42ad697bb8fc0e6aa3b68981
itdep <morozvitali99@gmail.com>
2025-10-22 14:31:01 +0300

    stream1 task19 3-10

--- src/stream1/task19_sorted_10/Main23.java
+++ src/stream1/task19_sorted_10/Main23.java
@@ -7,7 +7,14 @@ public class Main23 {
     public List <String> practice1 () {
         List<String> words = List.of("tea", "lemon",
                 "fig", "grape", "banana");
-
         return words.stream().filter(w->w.length() > 3).sorted(Comparator.comparing(String::length)).toList();
     }
+
+    public void practice3 () {
+        List<Integer> numbers = List.of(3, 7, 1, 9, 2);
+        numbers.stream().sorted(Comparator.reverseOrder()).toList();
+    }
+
+
+
 }
