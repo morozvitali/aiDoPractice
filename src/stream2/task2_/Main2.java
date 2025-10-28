ac24490abe4d9960301fa576ee38e56d32fcc68c
itdep <morozvitali99@gmail.com>
2025-10-28 11:04:36 +0200

    stream2 task2 2-10

--- src/stream2/task2_/Main2.java
+++ src/stream2/task2_/Main2.java
@@ -7,4 +7,11 @@ public class Main2 {
         IntStream.range(1, 5).forEach(System.out::println);
         IntStream.rangeClosed(1,5).forEach(System.out::println);
     }
+
+    public void practice2 () {
+        int sum = IntStream.rangeClosed(1,5).map(n->n*n).sum();
+        System.out.println(sum);
+    }
+
+
 }
