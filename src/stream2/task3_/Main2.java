4e9558503377712eb5013f4b4b66942b2ec99149
itdep <morozvitali99@gmail.com>
2025-10-28 14:43:22 +0200

    stream2 task3 1-10

---
+++ src/stream2/task3_/Main2.java
@@ -0,0 +1,14 @@
+package stream2.task3_;
+
+import java.util.stream.Stream;
+
+public class Main2 {
+    public void practice1() {
+        Stream.of(1, 2, 3, 4, 5, 6, 7, 4, 3)
+                .takeWhile(n->n<5)
+                .forEach(System.out::println);
+    }
+
+
+
+}
