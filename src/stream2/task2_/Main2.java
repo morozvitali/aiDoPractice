4e5486c73bc0def4f9d23cc1e75e944fa3b3b32a
itdep <morozvitali99@gmail.com>
2025-10-28 14:21:10 +0200

    stream2 task2 7-10

--- src/stream2/task2_/Main2.java
+++ src/stream2/task2_/Main2.java
@@ -1,5 +1,6 @@
 package stream2.task2_;
 
+import java.util.List;
 import java.util.OptionalDouble;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
@@ -34,6 +35,11 @@ public class Main2 {
 
     public void practice6 () {
         OptionalDouble avg = IntStream.of(10,20,30,40).average();
+    }
+
+    public void practice7 () {
+        List<String> list = IntStream.rangeClosed(1,3).mapToObj(i-> "Num " + i).toList();
+        System.out.println(list);
     }
 
 
