683e3ac0fb16b6fea8abc28843bd3e5ce07e8cde
itdep <morozvitali99@gmail.com>
2025-10-29 16:09:56 +0200

    stream1 task17 8-15

--- src/stream1/task17_collectors_15_firstMaking/Main35.java
+++ src/stream1/task17_collectors_15_firstMaking/Main35.java
@@ -1,5 +1,6 @@
 package stream1.task17_collectors_15_firstMaking;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -52,5 +53,13 @@ public class Main35 {
                 "sun", "day", "Java", "sky");
         return words.stream().collect(Collectors.reducing("",(a,b)-> a.length() > b.length() ? a : b));
     }
+
+    public Map <Character, List<String>> practice9 () {
+        List<String> words = List.of("apple", "ant",
+                "alphabet", "axe", "banana", "bat");
+        return words.stream().collect(Collectors.groupingBy(w->w.charAt(0), Collectors.collectingAndThen(Collectors.toList(), list->list.stream().sorted(Comparator.comparing(String::length)).toList())));
+    }
+
+
 
 }
