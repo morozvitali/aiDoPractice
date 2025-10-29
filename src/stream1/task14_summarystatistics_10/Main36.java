93271f77674c3ccafb9ed22302d8c265e225f9e6
itdep <morozvitali99@gmail.com>
2025-10-29 11:35:25 +0200

    stream1 task14 7-10

--- src/stream1/task14_summarystatistics_10/Main36.java
+++ src/stream1/task14_summarystatistics_10/Main36.java
@@ -50,6 +50,14 @@ public class Main36 {
         return Arrays.stream(words).collect(Collectors.groupingBy(w->w.charAt(0), Collectors.averagingInt(String::length)));
     }
 
+    public Map <Character, Long> practice7 () {
+        String[] words = {"apple", "orange",
+                "banana", "umbrella"};
+
+        return Arrays.stream(words).flatMap(w->w.chars().mapToObj(c->(char)c))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
 
 
 }
