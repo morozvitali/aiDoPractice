5c2ef6f5b48a717f52c53637068ca7d558938619
itdep <morozvitali99@gmail.com>
2025-10-29 13:49:48 +0200

    stream1 task15 9-10

--- src/stream1/task15_grouping_20/Main36.java
+++ src/stream1/task15_grouping_20/Main36.java
@@ -45,5 +45,11 @@ public class Main36 {
         return Stream.of(1, 2, 3, 4, 5).collect(Collectors.groupingBy(a->a%2==0 ? "even" : "odd"));
     }
 
+    public Map <Integer, Double> practice9 () {
+        return Stream.of(1, 2, 3, 4, 5).collect(Collectors.groupingBy(a->a%3, Collectors.averagingInt(n->n)));
+    }
+
+
+
 
 }
