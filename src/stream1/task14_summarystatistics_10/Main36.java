a3f5a762f84e53804ea0c089d0f60f82b7f84ce1
itdep <morozvitali99@gmail.com>
2025-10-29 11:08:46 +0200

    stream1 task14 2-10

--- src/stream1/task14_summarystatistics_10/Main36.java
+++ src/stream1/task14_summarystatistics_10/Main36.java
@@ -1,11 +1,17 @@
 package stream1.task14_summarystatistics_10;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Main36 {
     public Long practice1 () {
         int[] numbers = {2, 4, 2, 6, 8, 4, 10, 10, 12};
         return Arrays.stream(numbers).distinct().filter(a->a%2==0).summaryStatistics().getCount();
+    }
+
+    public Integer practice2 () {
+        List<String> words = List.of("APPLE", "Banana", "CHERRY", "kiwi", "PLUM");
+        return words.stream().filter(w->w.length()%2==0).map(String::toLowerCase).mapToInt(String::length).sum();
     }
 
 
