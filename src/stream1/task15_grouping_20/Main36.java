9df0060e01b46baa07a118ff0aa6db06d80659ed
itdep <morozvitali99@gmail.com>
2025-10-29 12:15:04 +0200

    stream1 task15 1-10

---
+++ src/stream1/task15_grouping_20/Main36.java
@@ -0,0 +1,22 @@
+package stream1.task15_grouping_20;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Main36 {
+    public Map<String, Boolean> practice1 () {
+        String[] words = {"sky", "apple", "dry", "orange", "sun"};
+        return Arrays.stream(words).collect(Collectors.toMap(Function.identity(), w->getCount(w)>0));
+    }
+
+    public static long getCount(String s) {
+        return s.chars().filter(c->"aeiou".indexOf(c)>=0).count();
+    }
+
+
+
+
+
+}
