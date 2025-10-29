e7e930edb29d192cb5dd7f21802216c364c36363
itdep <morozvitali99@gmail.com>
2025-10-29 15:47:44 +0200

    stream1 task17 3-15

--- src/stream1/task17_collectors_15_firstMaking/Main35.java
+++ src/stream1/task17_collectors_15_firstMaking/Main35.java
@@ -15,7 +15,13 @@ public class Main35 {
         List<String> words = List.of("apple", "ant",
                 "banana", "bat", "car");
         return words.stream().collect(Collectors.groupingBy(w->w.charAt(0), Collectors.mapping(String::length, Collectors.toList())));
-
     }
+
+    public Map<Boolean, Long> practice3 () {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        return numbers.stream().collect(Collectors.partitioningBy(a->a%2==0, Collectors.counting()));
+    }
+
+
 
 }
