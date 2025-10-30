93110e7d204964b862ca4fd8ec976e98b8afbaec
itdep <morozvitali99@gmail.com>
2025-10-30 16:26:04 +0200

    threading_concurency task1 6

---
+++ src/threading_concurency/part2/Main6.java
@@ -0,0 +1,27 @@
+package threading_concurency.part2;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Main6 {
+    static int counter = 0;
+    static ReentrantLock lock = new ReentrantLock();
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable task = () -> {
+            for (int i = 0; i < 1000; i++) {
+                lock.lock();
+                try {
+                    counter++;
+                } finally {
+                    lock.unlock();
+                }
+            }
+        };
+        Thread t1 = new Thread(task);
+        Thread t2 = new Thread(task);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+    }
+}
