fc3d6617977b6a3ed68dea8f04606706597ebf86
itdep <morozvitali99@gmail.com>
2025-10-30 15:04:57 +0200

    threading_concurency task1 3-5

---
+++ src/threading_concurency/part1/Main4.java
@@ -0,0 +1,18 @@
+package threading_concurency.part1;
+
+public class Main4 {
+    static int counter = 0;
+
+    public static void main(String[] args) {
+
+        //race condition
+
+
+
+
+
+
+
+
+    }
+}
