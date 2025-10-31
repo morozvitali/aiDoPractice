3e3bf06e61aab267c87cb546e422f42f016e9401
itdep <morozvitali99@gmail.com>
2025-10-31 14:38:48 +0200

    Stream1 Task 19 part 9-10

--- src/stream1/task19_sorted_10/Main24.java
+++ src/stream1/task19_sorted_10/Main24.java
@@ -43,4 +43,11 @@ public class Main24 {
         return words.stream().sorted(Comparator.comparing(String::length).reversed()).toList();
     }
 
+    public List <Integer> practice9 () {
+        List<Integer> numbers = List.of(3, 2, 5, 6, 4, 1);
+        return numbers.stream().sorted(Comparator.comparingInt((Integer a)->a%2).thenComparing(Comparator.reverseOrder())).toList();
+    }
+
+
+
 }
