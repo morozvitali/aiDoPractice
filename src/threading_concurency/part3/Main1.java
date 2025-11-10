0a42f2dfd36df664a2261d115147bfef57292347
itdep <morozvitali99@gmail.com>
2025-11-10 12:30:44 +0200

    Concurrency & Parallel Streams # 3-2

--- src/threading_concurency/part3/Main1.java
+++ src/threading_concurency/part3/Main1.java
@@ -1,11 +1,13 @@
 package threading_concurency.part3;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         ExecutorService executor = Executors.newFixedThreadPool(3);
         for (int i = 1; i<=5; i++) {
             int taskId = i;
@@ -14,5 +16,20 @@ public class Main1 {
 
         executor.shutdown();
 
+        System.out.println("//----------------------------------------//");
+        futureDemo();
+    }
+
+    public static void futureDemo () throws Exception {
+        ExecutorService exec = Executors.newSingleThreadExecutor();
+        Callable <Integer> task = () -> {
+            System.out.println("Calculating...");
+            Thread.sleep(1000);
+            return 42;
+        };
+
+        Future<Integer> result = exec.submit(task);
+        System.out.println("Result " + result.get());
+        exec.shutdown();
     }
 }
