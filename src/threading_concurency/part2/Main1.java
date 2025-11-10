c545cec8edfe36a107425c9174689f3c0f81c08f
itdep <morozvitali99@gmail.com>
2025-11-10 10:41:09 +0200

    Concurrency & Parallel Streams #2-3

--- src/threading_concurency/part2/Main1.java
+++ src/threading_concurency/part2/Main1.java
@@ -1,11 +1,13 @@
 package threading_concurency.part2;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main1 {
 
     static int counter =0;
     static ReentrantLock lock = new ReentrantLock();
+    static AtomicInteger counter3 = new AtomicInteger(0);
 
     public static void main(String[] args) throws InterruptedException {
         Runnable task = () -> {
@@ -31,7 +33,18 @@ public class Main1 {
             }
         };
 
-        new Thread(task2, "T1").start();
-        new Thread(task2, "T2").start();
+        Runnable task3 = () -> {
+            for (int i = 0; i < 1000; i++) {
+                counter3.incrementAndGet();
+            }
+        };
+
+        Thread t1 = new Thread(task3);
+        Thread t2 = new Thread(task3);
+        t1.start(); t2.start();
+        t1.join(); t2.join();
+
+
+        System.out.println("Counter = " + counter3.get());
     }
 }
