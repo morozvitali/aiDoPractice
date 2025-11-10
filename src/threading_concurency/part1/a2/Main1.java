f5656bfd260935a28f2317ab10b694c594dbe1e6
itdep <morozvitali99@gmail.com>
2025-11-10 09:59:22 +0200

    Concurrency & Parallel Streams #1-5

--- src/threading_concurency/part1/a2/Main1.java
+++ src/threading_concurency/part1/a2/Main1.java
@@ -1,13 +1,34 @@
 package threading_concurency.part1.a2;
 
 public class Main1 {
-    public static void main(String[] args) {
+    static int counter = 0;
+    public static void main(String[] args) throws InterruptedException {
         Runnable job = () -> {
             for (int i = 0; i <= 5; i++) {
                 System.out.println(Thread.currentThread().getName() + " → mesasage # " + i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         };
-        Thread tread = new Thread(job);
-        tread.start();
+
+
+        Runnable job2 = () -> {
+                for (int i = 0; i< 1000; i++) {
+                    counter++;
+                }
+        };
+
+        Thread thread1 = new Thread(job2);
+        Thread thread2 = new Thread(job2);
+
+
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println(" Counter = " + counter);
     }
 }
