47492ab682d39ac2524178bd6f2acd6070cdebc4
morozvitali <morozvitali99@gmail.com>
2025-11-16 18:20:18 +0200

    task 12 pr 01 of 10

--- src/stream1/task12_plus_valueOf_10/Main26.java
+++ src/stream1/task12_plus_valueOf_10/Main26.java
@@ -1,11 +1,29 @@
 package stream1.task12_plus_valueOf_10;
 
 import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 public class Main26 {
     long balance = 123456789L;
-NumberFormat nf = NumberFormat.getInstance(Locale.US);
-String formatted = nf.format(balance);
+    NumberFormat nf = NumberFormat.getInstance(Locale.US);
+    String formatted = nf.format(balance);
+
+    public List<Long> task3 () {
+        List<String> numbers = Arrays.asList("10",
+                null, "20", "", "30");
+        return numbers.stream().filter(s->!s.isBlank()).map(Long::valueOf).toList();
+    }
+
+    public int task01 (int number) {
+        return Integer.parseInt(String.valueOf(number).chars().map(Character::getNumericValue)
+                .map(a->a*a).mapToObj(String::valueOf)
+                .collect(Collectors.joining()));
+    }
+
+
 
 }
+
