95f74506b796d42c4b0acebf4f8754e73047d47d
morozvitali <morozvitali99@gmail.com>
2025-11-16 16:49:15 +0200

    task 10 pr 4 of 10

--- src/stream1/task10_list_to_map_to_entry/Main12.java
+++ src/stream1/task10_list_to_map_to_entry/Main12.java
@@ -4,22 +4,45 @@ import java.util.List;
 import java.util.Map;
 
 public class Main12 {
-    public String task1 () {
+    public String task1() {
         List<String> words = List.of("stream1", "code",
                 "developer", "Engineer");
-        return words.stream().map(w-> Map.entry(w,w.length()))
-                .reduce((a,b) -> a.getValue() >= b.getValue() ? a : b)
+        return words.stream().map(w -> Map.entry(w, w.length()))
+                .reduce((a, b) -> a.getValue() >= b.getValue() ? a : b)
                 .map(Map.Entry::getKey).orElse("");
     }
 
-    public String task2 () {
+    public String task2() {
         List<String> words = List.of("stream1", "code",
                 "developer", "Engineer");
-        return words.stream().map(w->Map.entry(w, w.length()))
-                .reduce((a,b)->a.getValue() < b.getValue() ? a : b)
+        return words.stream().map(w -> Map.entry(w, w.length()))
+                .reduce((a, b) -> a.getValue() < b.getValue() ? a : b)
                 .map(Map.Entry::getKey)
                 .orElse("");
     }
 
+    public String task3() {
+        List<String> list = List.of("apple", "banana",
+                "orange", "blueberry");
+        return list.stream().map(w -> Map.entry(w, countWovels(w)))
+                .reduce((a, b) -> a.getValue() > b.getValue() ? a : b)
+                .map(Map.Entry::getKey).orElse("");
+    }
+
+    public long countWovels(String s) {
+        return s.chars().filter(c -> "aeiou".indexOf(c) >= 0).count();
+    }
+
+    public String task4 () {
+        List<String> list = List.of("apple", "pear",
+                "banana", "kiwi");
+        return list.stream().map(w->Map.entry(w, getCount(w)))
+                .reduce((a,b)->a.getValue() > b.getValue() ? a : b)
+                .map(Map.Entry::getKey).orElse("");
+    }
+
+    public long getCount(String s) {
+        return s.length() - s.chars().distinct().count();
+    }
 
 }
