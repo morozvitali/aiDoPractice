0a6b95ac5b50bd69cb040a9ec542ce153ad5aeb5
morozvitali <morozvitali99@gmail.com>
2025-11-16 16:40:30 +0200

    task 10 pr 2 of 10

--- src/stream1/task10_list_to_map_to_entry/Main12.java
+++ src/stream1/task10_list_to_map_to_entry/Main12.java
@@ -7,8 +7,18 @@ public class Main12 {
     public String task1 () {
         List<String> words = List.of("stream1", "code",
                 "developer", "Engineer");
-        return words.stream().map(w-> Map.entry(w,w.length())).reduce((a,b) -> a.getValue() >= b.getValue() ? a : b)
+        return words.stream().map(w-> Map.entry(w,w.length()))
+                .reduce((a,b) -> a.getValue() >= b.getValue() ? a : b)
                 .map(Map.Entry::getKey).orElse("");
+    }
+
+    public String task2 () {
+        List<String> words = List.of("stream1", "code",
+                "developer", "Engineer");
+        return words.stream().map(w->Map.entry(w, w.length()))
+                .reduce((a,b)->a.getValue() < b.getValue() ? a : b)
+                .map(Map.Entry::getKey)
+                .orElse("");
     }
 
 
