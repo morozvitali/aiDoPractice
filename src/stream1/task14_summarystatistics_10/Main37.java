53e0cd00c74958760bfcdf49301ce8ed98dc11c4
itdep <morozvitali99@gmail.com>
2025-11-17 11:38:07 +0200

    Stream1 13 # 2-10

--- src/stream1/task14_summarystatistics_10/Main37.java
+++ src/stream1/task14_summarystatistics_10/Main37.java
@@ -2,11 +2,21 @@ package stream1.task14_summarystatistics_10;
 
 import java.util.Arrays;
 import java.util.IntSummaryStatistics;
+import java.util.List;
 
 public class Main37 {
     public IntSummaryStatistics task1 () {
         int[] numbers = {2, 4, 2, 6, 8, 4, 10, 10, 12};
         return Arrays.stream(numbers).filter(a->a%2==0).summaryStatistics();
+    }
+
+    public int task2 () {
+        List<String> words = List.of("APPLE",
+                "Banana", "CHERRY", "kiwi", "PLUM");
+        return words.stream().filter(w->w.length()%2==0)
+                .map(w->w.toLowerCase())
+                .mapToInt(String::length)
+                .sum();
     }
 
 
