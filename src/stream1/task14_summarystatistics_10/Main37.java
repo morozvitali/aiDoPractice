45b9fa34abda28f68ba70e63cee95435d1bbcf49
itdep <morozvitali99@gmail.com>
2025-11-18 10:09:25 +0200

    Stream1 13 # 9-10

--- src/stream1/task14_summarystatistics_10/Main37.java
+++ src/stream1/task14_summarystatistics_10/Main37.java
@@ -71,4 +71,9 @@ public class Main37 {
         return Arrays.stream(words).filter(w->"aeiou".chars().allMatch(c->w.indexOf(c) >=0)).toList();
     }
 
+    public Map<Boolean, List<String>> task10() {
+        String[] words = {"Apple", "banana",
+                "Cherry", "date", "Eggplant"};
+        return Arrays.stream(words).collect(Collectors.partitioningBy(w->Character.isUpperCase(w.charAt(0))));
+    }
 }
