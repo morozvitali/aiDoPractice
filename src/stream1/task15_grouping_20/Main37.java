8182cccb5b7480a99e0795d9805ac17cea2e44c8
itdep <morozvitali99@gmail.com>
2025-11-18 10:28:20 +0200

    Stream1 15 # 3-10

--- src/stream1/task15_grouping_20/Main37.java
+++ src/stream1/task15_grouping_20/Main37.java
@@ -1,6 +1,8 @@
 package stream1.task15_grouping_20;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -25,5 +27,9 @@ public class Main37 {
                 .count();
     }
 
+    public String task3 (String s) {
+        return Arrays.stream(s.split(" ")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream().max(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse("");
+    }
 
 }
