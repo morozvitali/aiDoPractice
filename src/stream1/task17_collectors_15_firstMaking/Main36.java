af62da0e7578f7ad0db7bc417bf39b3bb9ac8ac8
itdep <morozvitali99@gmail.com>
2025-11-18 11:58:03 +0200

    Stream1 17 # 3-10

--- src/stream1/task17_collectors_15_firstMaking/Main36.java
+++ src/stream1/task17_collectors_15_firstMaking/Main36.java
@@ -17,6 +17,10 @@ public class Main36 {
         return words.stream().collect(Collectors.groupingBy(w->w.charAt(0), Collectors.mapping(String::length, Collectors.toList())));
     }
 
+    public Map <Boolean, Long> task3 () {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        return numbers.stream().collect(Collectors.groupingBy(n->n%2==0, Collectors.counting()));
+    }
 
 
 }
