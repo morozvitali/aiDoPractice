04f6bf5387a10313c730f967abf7cdf8ef7832ed
itdep <morozvitali99@gmail.com>
2025-11-18 14:34:24 +0200

    Stream1 18 # 2-10

--- src/stream1/task18_flatMap_13/Main29.java
+++ src/stream1/task18_flatMap_13/Main29.java
@@ -16,4 +16,15 @@ public class Main29 {
     }
 
 
+    public int task2 () {
+        int[][] data = {
+                {3, 11},
+                {6, 7},
+                {10}
+        };
+        return Arrays.stream(data).flatMapToInt(Arrays::stream).filter(a->a%2==0).min().orElse(-1);
+    }
+
+
+
 }
