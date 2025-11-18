41204f976ed162b1f483ab2e90b8cf289b681b2d
itdep <morozvitali99@gmail.com>
2025-11-18 10:43:22 +0200

    Stream1 15 # 6-10

--- src/stream1/task15_grouping_20/Main37.java
+++ src/stream1/task15_grouping_20/Main37.java
@@ -42,7 +42,9 @@ public class Main37 {
                 .entrySet().stream().max(Comparator.comparingLong(Map.Entry::getValue)).map(Map.Entry::getKey).orElse(-1);
     }
 
-
+    public Map<Integer, Long> task6 (String s) {
+        return s.chars().filter(Character::isDigit).boxed().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
 
 
 }
