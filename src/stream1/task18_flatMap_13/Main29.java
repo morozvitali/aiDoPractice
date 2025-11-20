63d8f850b37aceb96acddd21b078fca524df3fdb
itdep <morozvitali99@gmail.com>
2025-11-20 12:26:15 +0200

    Stream1 18 # 9-10

--- src/stream1/task18_flatMap_13/Main29.java
+++ src/stream1/task18_flatMap_13/Main29.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main29 {
-    public void task1 () {
+    public void task1() {
         List<String> sentences = List.of(
                 "Java is powerful",
                 "Streams are cool",
@@ -16,49 +16,49 @@ public class Main29 {
     }
 
 
-    public int task2 () {
+    public int task2() {
         int[][] data = {
                 {3, 11},
                 {6, 7},
                 {10}
         };
-        return Arrays.stream(data).flatMapToInt(Arrays::stream).filter(a->a%2==0).min().orElse(-1);
+        return Arrays.stream(data).flatMapToInt(Arrays::stream).filter(a -> a % 2 == 0).min().orElse(-1);
     }
 
-    public int task3 () {
+    public int task3() {
         int[][] data = {
                 {-3, 5},
                 {2},
                 {-1, 4}
         };
-        return Arrays.stream(data).flatMapToInt(Arrays::stream).filter(a->a>0).reduce((a,b)->a*b).orElse(-1);
+        return Arrays.stream(data).flatMapToInt(Arrays::stream).filter(a -> a > 0).reduce((a, b) -> a * b).orElse(-1);
     }
 
 
-    public int task4 () {
+    public int task4() {
         int[][] data = {
                 {3, 9},
                 {8, 12},
                 {7}
         };
-        return Arrays.stream(data).flatMapToInt(Arrays::stream).filter(a->a%3==0).max().orElse(-1);
+        return Arrays.stream(data).flatMapToInt(Arrays::stream).filter(a -> a % 3 == 0).max().orElse(-1);
     }
 
 
-    public int task5 () {
-        int[][] data = {{2,3},{4}};
-        return Arrays.stream(data).flatMapToInt(Arrays::stream).filter(a->a%2==0).map(a->a*a).sum();
+    public int task5() {
+        int[][] data = {{2, 3}, {4}};
+        return Arrays.stream(data).flatMapToInt(Arrays::stream).filter(a -> a % 2 == 0).map(a -> a * a).sum();
     }
 
-    public double task6 () {
+    public double task6() {
         int[][] data = {
                 {3, 5},
                 {7}
         };
-        return Arrays.stream(data).flatMapToInt(Arrays::stream).filter(a->a%2==1).average().orElse(-1);
+        return Arrays.stream(data).flatMapToInt(Arrays::stream).filter(a -> a % 2 == 1).average().orElse(-1);
     }
 
-    public int task7 () {
+    public int task7() {
         int[][] data = {
                 {12},
                 {24, 36}
@@ -67,11 +67,15 @@ public class Main29 {
     }
 
     public static int gcd(int a, int b) {
-        return b==0 ? a : gcd (b, a%b);
+        return b == 0 ? a : gcd(b, a % b);
     }
 
-    public List<Integer> task8 (List <Object> list) {
-        return list.stream().filter(a->a instanceof Integer).map(a->(Integer)a).toList();
+    public List<Integer> task8(List<Object> list) {
+        return list.stream().filter(a -> a instanceof Integer).map(a -> (Integer) a).toList();
+    }
+
+    public List<String> task9(List<Object> list) {
+            return list.stream().filter(s->s instanceof String).map(s->(String)s).toList();
     }
 
 
