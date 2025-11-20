63502f07be1bc19a125a46e230cd01310e218750
itdep <morozvitali99@gmail.com>
2025-11-20 12:42:35 +0200

    Stream1 19 # 3-10

--- src/stream1/task19_sorted_10/Main25.java
+++ src/stream1/task19_sorted_10/Main25.java
@@ -2,6 +2,7 @@ package stream1.task19_sorted_10;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main25 {
@@ -14,7 +15,9 @@ public class Main25 {
         return Stream.of(3, 7, 1, 9, 2).sorted(Comparator.reverseOrder()).toList();
     }
 
-
+    public List <Integer> task3 () {
+        return List.of(3, 2, 5, 6, 1, 4).stream().sorted(Comparator.comparing((Integer a)->a%2).thenComparing(n->n)).collect(Collectors.toList());
+    }
 
 
 
