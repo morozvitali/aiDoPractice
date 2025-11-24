8bfcc7b7a64498d0daa44138407d643292b3994e
itdep <morozvitali99@gmail.com>
2025-11-24 10:04:33 +0200

    Stream1 19 # 9-10

--- src/stream1/task19_sorted_10/Main25.java
+++ src/stream1/task19_sorted_10/Main25.java
@@ -47,4 +47,10 @@ public class Main25 {
                 "apple", "pear", "banana");
         return words.stream().sorted(Comparator.comparing(String::length).thenComparing(Comparator.reverseOrder())).toList();
     }
+
+    public List <Integer> task9 () {
+        List<Integer> numbers = List.of(3, 2, 5, 6, 4, 1);
+        return numbers.stream().sorted(Comparator.comparing((Integer a)->a%2==0).thenComparing(Comparator.reverseOrder())).toList();
+    }
+
 }
