88353f32ef355c938f7eefef1cc88f3fdea04505
itdep <morozvitali99@gmail.com>
2025-11-26 09:46:14 +0200

    Stream1 10 # 1-10

---
+++ src/stream1/task10_list_to_map_to_entry/Main13.java
@@ -0,0 +1,17 @@
+package stream1.task10_list_to_map_to_entry;
+
+import java.util.List;
+import java.util.Map;
+
+public class Main13 {
+    public String practice1 () {
+        List<String> words = List.of("stream1", "code",
+                "developer", "Engineer");
+        return words.stream().map(w-> Map.entry(w, w.length()))
+                .reduce((a,b)->a.getValue() > b.getValue() ? a:b)
+                .map(Map.Entry::getKey).orElse("");
+    }
+
+
+
+}
