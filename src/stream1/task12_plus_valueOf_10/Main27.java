2c49f3b252b1fb7f10ee1cdb225d5293d0823fe7
itdep <morozvitali99@gmail.com>
2025-11-26 10:47:09 +0200

    Stream1 12 # 2-10

--- src/stream1/task12_plus_valueOf_10/Main27.java
+++ src/stream1/task12_plus_valueOf_10/Main27.java
@@ -1,7 +1,9 @@
 package stream1.task12_plus_valueOf_10;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -11,6 +13,17 @@ public class Main27 {
         return inputs.stream()
                 .map(Integer::parseInt)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
+    public List<Long> practice2 () {
+        List <Long> top10 = new Random().ints(100, -100, 101)
+                .mapToObj(String::valueOf)
+                .map(Long::valueOf)
+                .filter(n->n>0)
+                .sorted(Comparator.reverseOrder())
+                .limit(10)
+                .toList();
+        return top10;
     }
 
 
