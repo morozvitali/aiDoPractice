1b21f1a0b7ca98a43e0bd81eca9935e058366f8a
itdep <morozvitali99@gmail.com>
2025-11-27 12:50:18 +0200

    task14 2-10

--- src/stream1/task14_summarystatistics_10/Main38.java
+++ src/stream1/task14_summarystatistics_10/Main38.java
@@ -3,6 +3,8 @@ package stream1.task14_summarystatistics_10;
 import java.util.Arrays;
 import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main38 {
     public IntSummaryStatistics practice1 () {
@@ -19,5 +21,17 @@ public class Main38 {
                     .sum();
     }
 
+    public Map <String, Integer> practice3 () {
+        String[] words = {"sky", "apple",
+                "moon", "dry", "banana"};
+        return Arrays.stream(words).filter(w->w.length() > 3)
+                .filter(w->counter(w) > 1)
+                .collect(Collectors
+                        .toMap(w->w, w-> Math.toIntExact(w.chars().filter(c -> "aeiou".indexOf(c) >= 0).count())));
+    }
+
+    public static long counter (String s) {
+        return s.chars().filter(c->"aeiou".indexOf(c) >=0).count();
+    }
 
 }
