6ab9e6f608aca08500fe146bf7f41af4acc09ed9
itdep <morozvitali99@gmail.com>
2025-12-03 11:07:51 +0200

    task stream2 ### 2 of 10

--- src/stream2/task1_/Main3.java
+++ src/stream2/task1_/Main3.java
@@ -15,7 +15,15 @@ public class Main3 {
         ));
     }
 
-
+    public void practice2 () {
+        List<Integer> nums = List.of(1, 2, 3, 4);
+        int sum = nums.stream().collect(Collector.of(
+                () -> new int[1],
+                (a,n) -> a[0] += n,
+                (a1, a2) -> {a1[0] +=a2[0]; return a1;},
+                a->a[0]
+        ));
+    }
 
 
 }
