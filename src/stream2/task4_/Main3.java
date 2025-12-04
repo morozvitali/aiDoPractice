607d183cae5ef7fdbcbb05bcdd610dc0278f7a87
itdep <morozvitali99@gmail.com>
2025-12-04 16:12:25 +0200

    task stream2.4 ### 1 of 10

---
+++ src/stream2/task4_/Main3.java
@@ -0,0 +1,20 @@
+package stream2.task4_;
+
+import java.util.stream.Stream;
+
+public class Main3 {
+    public void practice1() {
+        Stream.iterate(0, n -> n + 2)
+                .limit(5)
+                .forEach(System.out::println);
+    }
+
+    public void practice2 () {
+
+    }
+
+
+
+
+
+}
