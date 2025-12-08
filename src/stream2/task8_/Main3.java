d590dab83c44ee1f426cb3707cda9feb7e63dbee
itdep <morozvitali99@gmail.com>
2025-12-08 13:54:02 +0200

    task stream 2.8 ### 1  of 10

---
+++ src/stream2/task8_/Main3.java
@@ -0,0 +1,14 @@
+package stream2.task8_;
+
+import java.util.Optional;
+
+public class Main3 {
+    public void practice1 () {
+        Optional<String> name = Optional.of("Java");
+        name.stream().forEach(System.out::println);
+     }
+
+
+
+
+}
