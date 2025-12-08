fa6be9ae098d7836559541e3012a6e4536a9225f
itdep <morozvitali99@gmail.com>
2025-12-08 14:17:27 +0200

    task stream 2.8 ### 6  of 10

--- src/stream2/task8_/Main3.java
+++ src/stream2/task8_/Main3.java
@@ -6,43 +6,49 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 public class Main3 {
-    public void practice1 () {
+    public void practice1() {
         Optional<String> name = Optional.of("Java");
         name.stream().forEach(System.out::println);
-     }
+    }
 
-    public void practice2 () {
-        Optional <String> empty = Optional.empty();
+    public void practice2() {
+        Optional<String> empty = Optional.empty();
         empty.stream().forEach(System.out::println);
     }
 
-    public void practice3 () {
-        Optional <List<Integer>> maybeList = Optional.of(List.of(1,2,3));
+    public void practice3() {
+        Optional<List<Integer>> maybeList = Optional.of(List.of(1, 2, 3));
         maybeList.stream()
                 .flatMap(List::stream)
                 .forEach(System.out::println);
     }
 
-    public void practice4 () {
+    public void practice4() {
         List<String> words = List.of("cat", "apple", "pear");
 
-        Optional <String> found = words.stream()
-                .filter(w->w.length() > 4)
+        Optional<String> found = words.stream()
+                .filter(w -> w.length() > 4)
                 .findFirst();
         found.stream().forEach(System.out::println);
     }
 
-    public void practice5 () {
-        Stream <Optional <String>> optionals = Stream.of(
+    public void practice5() {
+        Stream<Optional<String>> optionals = Stream.of(
                 Optional.of("A"),
                 Optional.empty(),
                 Optional.of("B")
         );
-        List <String> list = optionals.flatMap(Optional::stream).toList();
+        List<String> list = optionals.flatMap(Optional::stream).toList();
         System.out.println(list);
     }
 
+    public void practice6() {
+        Optional<Optional<String>> nested = Optional.of(Optional.of("Hello"));
+        nested.stream()
+                .flatMap(Optional::stream)
+                .forEach(System.out::println);
 
+    }
 
 
 }
