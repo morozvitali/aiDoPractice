c95a53af415322769c45f93bb7241323d54013b1
itdep <morozvitali99@gmail.com>
2025-12-08 11:18:58 +0200

    task stream 2.7 ### 6  of 10

--- src/stream2/task7_/Main3.java
+++ src/stream2/task7_/Main3.java
@@ -8,46 +8,67 @@ import java.util.stream.Stream;
 
 public class Main3 {
 
-    public void practice1 () {
+    public void practice1() {
         List<String> names = Stream.of("Bob", "Alice", "Tom")
                 .collect(Collectors
                         .toUnmodifiableList());
         System.out.println(names);
     }
 
-    public void practice2 () {
-        Set <Integer> set = Stream.of(1,2,3,2)
+    public void practice2() {
+        Set<Integer> set = Stream.of(1, 2, 3, 2)
                 .collect(Collectors.toUnmodifiableSet());
         System.out.println(set);
     }
 
-    public void practice3 () {
+    public void practice3() {
         Map<String, Integer> map = Stream.of("A", "B", "C")
                 .collect(Collectors.toUnmodifiableMap(
-                        s->s,
+                        s -> s,
                         String::length
                 ));
     }
 
-    public void ptactice4 () {
+    public void ptactice4() {
         List<String> list = List.of("apple", "apricot");
-        Map <Character, String> map = list.stream()
+        Map<Character, String> map = list.stream()
                 .collect(Collectors.toUnmodifiableMap(
-                        s-> s.charAt(0),
-                        s->s
+                        s -> s.charAt(0),
+                        s -> s
                 ));
         System.out.println(map);
     }
 
-    public void practice5 () {
+    public void practice5() {
         Map<Character, String> map = Stream.of("apple", "apricot")
-        .collect(Collectors.toUnmodifiableMap(
-                s->s.charAt(0),
-                s->s,
-                (a,b) -> a+ ", " + b
-        ));
+                .collect(Collectors.toUnmodifiableMap(
+                        s -> s.charAt(0),
+                        s -> s,
+                        (a, b) -> a + ", " + b
+                ));
         System.out.println(map);
     }
+
+    public void practice6() {
+        Map<Character, List<String>> map = Stream.of("cat", "cow", "dog")
+                .collect(Collectors.toUnmodifiableMap(
+                        w -> w.charAt(0),
+                        w -> List.of(w),
+                        (l1, l2) -> Stream.concat(l1.stream(), l2.stream()).toList()));
+        System.out.println(map);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
